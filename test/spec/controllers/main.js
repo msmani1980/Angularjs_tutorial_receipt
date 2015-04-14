e087748bb9f1d8caad85d5b0571e7f8f2262942e
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-14 14:29:20 -0500

    Rodrigo Curiel - Added scss and dashboard fixture

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,25 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('ts5App'));
+    // load the controller's module
+    beforeEach(module('ts5App'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should attach a list of dashboard items to the scope', function () {
+        expect(scope.dashboardItems.length).toBe(12);
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+    it('should have title and items', function () {
+        expect(scope.dashboardItems[0].title).toBe('Retail Item Management')
+    });
 });
