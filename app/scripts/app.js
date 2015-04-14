776e54d205160a87902f1a32ba627b6e1255850e
Ali Karbassi <ali@karbassi.com>
2015-04-14 11:47:05 -0500

    Yeoman generate angular. Beginning of project.

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ts5App
+ * @description
+ * # ts5App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ts5App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
