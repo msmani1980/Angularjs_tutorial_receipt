f5f950f5f302ccd0a2ffc8d6413800b961a3ecd0
Max Felker <max@bigroomstudios.com>
2015-04-14 12:13:32 -0500

    Created items route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/items', {
+        templateUrl: 'views/items.html',
+        controller: 'ItemsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
