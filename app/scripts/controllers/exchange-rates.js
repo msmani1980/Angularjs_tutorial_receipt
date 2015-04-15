44967a2d045d5c55199151a2c4f79cca19737d2a
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-15 16:52:32 -0500

    Rodrigo Curiel - added tests and http mocks

--- app/scripts/controllers/exchange-rates.js
+++ app/scripts/controllers/exchange-rates.js
@@ -8,6 +8,45 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-    .controller('ExchangeRatesCtrl', function ($scope) {
-        $scope.breadcrumb = 'Cash Management / Daily Exchange Rates';
-    });
+  .controller('ExchangeRatesCtrl', function ($scope, $http) {
+    var getCompany = function (companyId) {
+      return {
+        'id': companyId,
+        'companyName': 'Virgin Australia',
+        'legalName': 'Virgin Australia',
+        'baseCurrencyId': 1,
+        'exchangeRateVariance': '10.0000'
+      };
+    };
+
+    var getCompanyBaseCurrency = function (currencyList, baseCurrencyId) {
+      var companyCurrency = currencyList.filter(function(currencyItem) {
+        return currencyItem.id === baseCurrencyId; // filter out appropriate one
+      })[0];
+      $scope.companyCurrency = companyCurrency;
+    };
+
+    var getCurrencies = function () {
+      var url = 'https://54.87.153.42/api/currencies';
+      var request = {
+        method: 'GET',
+        url: url,
+        headers: {
+          'Content-Type': 'application/json',
+          'userId': 1,
+          'companyId': 326
+        }
+      };
+
+      $http(request).success(function (data) {
+        getCompanyBaseCurrency(data.response, getCompany(326).baseCurrencyId);
+      }).error(function (data, status) {
+        console.log('error on request', data, status);
+      });
+    };
+
+    getCurrencies();
+    $scope.breadcrumb = 'Cash Management / Daily Exchange Rates';
+    $scope.currentCompany = getCompany(326);
+
+  });
--- test/spec/controllers/exchange-rates.js
+++ test/spec/controllers/exchange-rates.js
@@ -1,22 +1,54 @@
 'use strict';
 
 describe('Controller: ExchangeRatesCtrl', function () {
-
   // load the controller's module
   beforeEach(module('ts5App'));
 
   var ExchangeRatesCtrl,
-    scope;
+    scope,
+    $httpBackend,
+    currenciesRequestHandler,
+    createController;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ExchangeRatesCtrl = $controller('ExchangeRatesCtrl', {
-      $scope: scope
-    });
-  }));
+  beforeEach(inject(function ($controller, $rootScope, $injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      currenciesRequestHandler = $httpBackend.when('GET', 'https://54.87.153.42/api/currencies')
+        .respond({
+          'response': [{
+            createdOn: '2014-08-19',
+            currencyCode: 'USD',
+            currencyId: 1,
+            currencyName: 'U.S. Dollar',
+            currencySymbol: '$',
+            decimalPrecision: 2,
+            id: 1
+          }]
+        });
+      scope = $rootScope.$new();
+      createController = function () {
+        ExchangeRatesCtrl = $controller('ExchangeRatesCtrl', {
+          $scope: scope
+        });
+      };
+    })
+  );
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should fetch currencies list from services', function () {
+    $httpBackend.expectGET('https://54.87.153.42/api/currencies');
+    ExchangeRatesCtrl = createController();
+    $httpBackend.flush();
+  });
 
   it('should have a breadcrumb property', function () {
+    ExchangeRatesCtrl = createController();
     expect(scope.breadcrumb).toBeDefined();
+    $httpBackend.flush();
   });
-});
+})
+;
