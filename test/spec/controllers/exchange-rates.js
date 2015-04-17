9990e58e50892ef7220e9b1bc4e7c7a4c39133fb
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-17 09:02:29 -0500

    Rodrigo Curiel - refactored exchange rate controller test to use spy and promises, added daily exchange rates service

--- test/spec/controllers/exchange-rates.js
+++ test/spec/controllers/exchange-rates.js
@@ -2,52 +2,43 @@
 
 describe('Controller: ExchangeRatesCtrl', function () {
   // load the controller's module
-  beforeEach(module('ts5App'));
 
   var ExchangeRatesCtrl,
     scope,
-    $httpBackend,
-    currenciesRequestHandler,
-    createController;
+    createController,
+    getCompanyBaseCurrency,
+    currencyFactory;
+
+  beforeEach(module('ts5App'));
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $injector) {
-      $httpBackend = $injector.get('$httpBackend');
-      currenciesRequestHandler = $httpBackend.when('GET', 'https://ec2-52-6-49-188.compute-1.amazonaws.com/api/currencies')
-        .respond({
-          'response': [{
-            createdOn: '2014-08-19',
-            currencyCode: 'USD',
-            currencyId: 1,
-            currencyName: 'U.S. Dollar',
-            currencySymbol: '$',
-            decimalPrecision: 2,
-            id: 1
-          }]
-        });
+  beforeEach(inject(function ($controller, $rootScope, $q, _currencyFactory_) {
+      var deferred = $q.defer();
+      currencyFactory = _currencyFactory_;
       scope = $rootScope.$new();
       createController = function () {
+        spyOn(currencyFactory, 'getCompanyBaseCurrency').and.returnValue(deferred.promise);
+        spyOn(currencyFactory, 'getCompanyCurrencies').and.returnValue(deferred.promise);
         ExchangeRatesCtrl = $controller('ExchangeRatesCtrl', {
           $scope: scope
         });
+        deferred.resolve({fake: 'data'});
       };
     })
   );
 
-  afterEach(function () {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-  });
-
-  it('should fetch currencies list from services', function () {
-    $httpBackend.expectGET('https://ec2-52-6-49-188.compute-1.amazonaws.com/api/currencies');
-    ExchangeRatesCtrl = createController();
-    $httpBackend.flush();
-  });
-
   it('should have a breadcrumb property', function () {
     ExchangeRatesCtrl = createController();
     expect(scope.breadcrumb).toBeDefined();
-    $httpBackend.flush();
+  });
+
+  it('should get the companyBaseCurrency from currencyFactory', function () {
+    ExchangeRatesCtrl = createController();
+    expect(currencyFactory.getCompanyBaseCurrency).toHaveBeenCalled();
+  });
+
+  it('should get the companyBaseCurrency from currencyFactory', function () {
+    ExchangeRatesCtrl = createController();
+    expect(currencyFactory.getCompanyCurrencies).toHaveBeenCalled();
   });
 });
