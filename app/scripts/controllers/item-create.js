d8b188b7af0c203dc47f15f91c8f1c2ab1964e51
Max Felker <max@bigroomstudios.com>
2015-04-21 00:46:53 -0400

    Added regular exp object (going to move to constant), updated validation to handle local and remote, updated success modal

--- app/scripts/controllers/item-create.js
+++ app/scripts/controllers/item-create.js
@@ -8,7 +8,25 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('ItemCreateCtrl', function ($scope,$http,baseUrl) {
+  .controller('ItemCreateCtrl', function ($scope,$http,baseUrl,$location,$anchorScroll) {
+
+  		$scope.scrollTo = function(id) {
+	      $location.hash(id);
+	      $anchorScroll();
+	    };
+
+	    $scope.regex = {
+			bit: /^(0|1)$/,
+			num: /^-?([0-9]*)$/,
+			alpha: /^[a-zA-z\s]+$/,
+			alphanum:  /^[a-zA-Z0-9]?[\s||\'||\"||\.||\?||\!||\-||a-zA-Z0-9]+$/,
+			email: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+			phone: /^([0-9]{3}( |-|.)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-|.)?([0-9]{3}( |-|.)?[0-9]{4}|[a-zA-Z0-9]{7})$/,
+			cc: /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/,
+			zip: /^(([0-9]{5})|([0-9]{5}[-][0-9]{4}))$/,
+			decimal: /^\d+\.\d{0,4}$/,
+			price: /^\$?\s?[0-9\,]+(\.\d{0,4})?$/
+		};
     
     	// View Name
   		$scope.viewName = 'Create Item';
@@ -18,6 +36,14 @@ angular.module('ts5App')
 
         // Submit form 
         $scope.submitForm = function(formData) {
+
+        	// If the local form is not valid
+        	if(!$scope.form.$valid) {
+
+  				$scope.displayError = true;
+
+  				return false;
+  			}
 
         	// set the formData models in the view to the scope.formData object
         	$scope.formData = angular.copy(formData);
