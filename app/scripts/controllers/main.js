846f678249e7d97edcbd0a4e8cd4c051d7420a0c
Max Felker <max@bigroomstudios.com>
2015-04-21 16:00:01 -0400

    Updated items list and items create MVC to match feature-view pattern, finished form validation and created input-text directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,7 +20,7 @@ angular.module('ts5App')
         menuItems: [
           {
             name: 'Manage Items',
-            route: 'items',
+            route: 'item-list',
             icon: 'icon-manage-retail-item',
             className: 'dashboard-managemenuItems'
           },
