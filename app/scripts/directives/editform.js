ddb9bd0804aeb25bc615676552da0b545c25170c
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-22 16:36:19 -0500

    Rodrigo Curiel - fix jshint

--- app/scripts/directives/editform.js
+++ app/scripts/directives/editform.js
@@ -7,59 +7,63 @@
  * # editForm
  */
 angular.module('ts5App')
-  .directive('editForm', ['$parse',function ($http,baseUrl) {
+  .directive('editForm', ['$parse', function ($http, baseUrl) {
 
     return {
       templateUrl: 'views/directives/edit-form.html',
       restrict: 'E',
       scope: {
-      	fields:	'=',
+        fields: '=',
         ngModel: '='
       },
 
-      controller: function($scope) {
+      controller: function ($scope) {
 
         // Submit form
-        $scope.submitForm = function() {
+        $scope.submitForm = function () {
 
           // loop through fields passed to the directive
-          angular.forEach($scope.fields, function(field) {
+          angular.forEach($scope.fields, function (field) {
 
-              // add data to the ngModel passed to the directive
-              $scope.ngModel[field.model] = field.value ;
+            // add data to the ngModel passed to the directive
+            $scope.ngModel[field.model] = field.value;
 
-           });
+          });
 
           // forcing price right now, FIX ME
-          $scope.ngModel.prices = [{startDate: '20150515', endDate: '20150715', typeId: '1', priceCurrencies: [], taxIs: 'Included',}];
+          $scope.ngModel.prices = [{
+            startDate: '20150515',
+            endDate: '20150715',
+            typeId: '1',
+            priceCurrencies: [],
+            taxIs: 'Included',
+          }];
 
           // Request Object
           var req = {
-              method: 'POST',
-              url: baseUrl + '/api/retail-items1',
-              headers: {
-                  'Content-Type': 'application/json',
-                  'userId': 1,
-                  'companyId': 326
-              },
-              data: {
-                retailItem: $scope.ngModel
-              }
+            method: 'POST',
+            url: baseUrl + '/api/retail-items1',
+            headers: {
+              'Content-Type': 'application/json',
+              'userId': 1,
+              'companyId': 326
+            },
+            data: {
+              retailItem: $scope.ngModel
+            }
           };
 
           // Post Data to server
-            $http(req).success(function(data) {
-            }).error(function(data) {
-            });
+          $http(req);/*.success(function (data) {
+          }).error(function (data) {
+          });*/
 
-          };
+        };
 
       }
 
 
-
-   };
-
+    };
 
 
   }]);
