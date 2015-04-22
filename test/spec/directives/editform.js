ab7cd0f801d1ef1de2bcf4b989fdadcfd7c96bf7
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-22 16:18:57 -0500

    Rodrigo Curiel - removed offending jshint errors

--- test/spec/directives/editform.js
+++ test/spec/directives/editform.js
@@ -5,8 +5,7 @@ describe('Directive: editForm', function () {
   // load the directive's module
   beforeEach(module('ts5App'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
