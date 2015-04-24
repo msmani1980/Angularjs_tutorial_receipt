32c93a6ad22df4f2e37ad7e1f0d08a08e3c7b4f7
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-04-24 11:13:20 -0500

    Rodrigo Curiel [TSVPORTAL-764] Fixed BOOTLINT errors

--- app/scripts/controllers/item-list.js
+++ app/scripts/controllers/item-list.js
@@ -9,52 +9,41 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-    .controller('ItemListCtrl', function ($scope,$http,itemsFactory) {
+  .controller('ItemListCtrl', function ($scope, $http, itemsFactory) {
 
-        $http.defaults.headers.common.userId = 1;
-        $http.defaults.headers.common.companyId = 326;
+    $http.defaults.headers.common.userId = 1;
+    $http.defaults.headers.common.companyId = 326;
 
-        // set the list size
-        $scope.listSize = 10;
+    // set the list size
+    $scope.listSize = 10;
 
-        // Get a list from the API
-        var itemList = itemsFactory.query(function () {
-
-            $scope.items = itemList.retailItems;
-
-            $scope.itemsCount = itemList.meta.count;
-
-            var pagination =  generatePagniation($scope.itemsCount,$scope.listSize);
-
-            $scope.pagination = pagination;
-
-        });
-
-        // Watch the listSize model change (items per page dropdown for pagination)
-        $scope.$watch('listSize', function() {
-
-            var pagination =  generatePagniation($scope.itemsCount, parseInt(this.last));
-
-            $scope.pagination = pagination;
-
-        });
-
-        //Generates pagination information
-        function generatePagniation(itemCount,itemsPerPage) {
-
-            var pageCount = Math.ceil( itemCount / itemsPerPage );
-
-            var pages = [0];
-
-            for(var i = 1; i < pageCount; i++) {
-                pages.push(i);
-            }
-
-            return {
-                pages: pages,
-                pageCount: pageCount
-            };
-
-        }
-
+    // Get a list from the API
+    var itemList = itemsFactory.query(function () {
+      $scope.items = itemList.retailItems;
+      $scope.itemsCount = itemList.meta.count;
+      var pagination = generatePagniation($scope.itemsCount, $scope.listSize);
+      $scope.pagination = pagination;
     });
+
+    // Watch the listSize model change (items per page dropdown for pagination)
+    $scope.$watch('listSize', function () {
+      var pagination = generatePagniation($scope.itemsCount, parseInt(this.last));
+      $scope.pagination = pagination;
+    });
+
+    //Generates pagination information
+    function generatePagniation(itemCount, itemsPerPage) {
+      var pageCount = Math.ceil(itemCount / itemsPerPage);
+      var pages = [0];
+      for (var i = 1; i < pageCount; i++) {
+        pages.push(i);
+      }
+
+      return {
+        pages: pages,
+        pageCount: pageCount
+      };
+
+    }
+
+  });
