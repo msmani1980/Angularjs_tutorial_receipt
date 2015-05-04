16b9d39e55c7d49ff1e42248501070766d9e6e4c
Jared Donisvitch <jdonisvitch@gmail.com>
2015-05-04 16:03:36 -0400

    move upload logic and template into directive image-upload

---
+++ app/scripts/directives/image-upload.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ts5App.directive:imageUpload
+ * @description Image upload directive for TS5 retail items 
+ * # imageUpload 
+ */
+
+angular.module('ts5App')
+  .directive('imageUpload', function () {
+
+	var imageUploadController = function ($scope, Upload, baseUrl, $http) {
+
+	    // set header param 'type' = item
+	    $http.defaults.headers.common.type = 'item';
+
+	    // Progress
+	    $scope.uploadProgress = [];
+
+	    //Watch for files
+	   	$scope.$watch('files', function (files) {
+	       $scope.files = files;
+	    });
+
+	    //Clear current images/files
+	    $scope.clearFiles = function() {
+	        $scope.files = [];
+	        $scope.uploadProgress = 0;
+	        $scope.uploadSuccess = false;
+	        $scope.uploadFail = false;
+	        $scope.formData.images = [];
+	    };
+
+	    //Upload Image function
+	    $scope.upload = function () {
+
+	        // grab files from scope
+	    	var files = $scope.files;
+
+	        //if a file exists and it's length is greater than 0
+	        if (files && files.length) {
+	            
+	            // Upload image
+	            Upload.upload({     
+	                url: baseUrl + '/api/images',
+	                fileFormDataName: 'image',
+	                file: files
+	            }).progress(function (evt) {
+
+	                //Upload Progress
+	                $scope.uploadProgress = parseInt(100.0 * evt.loaded / evt.total);
+
+	            // on a successful upload
+	            }).success(function (data) {
+
+	                // set the UI flag
+	                $scope.uploadSuccess = true; 
+
+	                // new image object
+	                var newImage = {
+	                    endDate: '20150515',
+	                    imageURL: data.url,
+	                    startDate: '20150715'
+	                };
+
+	                // pass new image object into formData.images array
+	                $scope.formData.images.push(newImage);
+
+	            // on a failed upload
+	            }).error(function (data) {
+
+	                //set the UI flag 
+	                $scope.uploadFail = true;
+
+	                console.log(data);
+
+	            });
+
+	        // no files found, exit function
+	        } else {
+	        	return false;
+	        }
+
+	    };
+
+    };
+
+    return {
+
+      	templateUrl: 'views/directives/image-upload.html',
+      	restrict: 'E',
+      	scope: false, // isolate scope to directive only
+      	controller: imageUploadController
+    };
+    
+  });
