f2e15e62dafe10652e3643290754f224aff6dbcf
Jared Donisvitch <jdonisvitch@gmail.com>
2015-05-05 17:53:09 -0400

    write tests for image upload, directive, response expected to be json.

--- test/spec/directives/image-upload.js
+++ test/spec/directives/image-upload.js
@@ -1,24 +1,78 @@
 'use strict';
 
-describe('Directive: imageUpload', function () {
+describe('Image Upload Directive |', function () {
 
+  var scope,
+      imageJSON,
+      Upload,
+      response,
+      $httpBackend;
+ 
   // load the directive's module
   beforeEach(module('ts5App'));
 
-  var element,
-    scope;
+  // load the expected json response and image
+  beforeEach(module('served/image-upload.json'));
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
 
-  it('should be inject into the DOM', inject(function ($compile) {
+  // Inject the service and responshandler
+  beforeEach(inject(function (_Upload_, $injector) {
 
-    element = angular.element('<image-upload></image-upload>');
+    // Inject the JSON fixtures
+    inject(function (_servedImageUpload_) {
+      imageJSON = _servedImageUpload_;    
+    }); 
 
-    element = $compile(element)(scope);
+    Upload = _Upload_;
 
-    expect(element).toBeDefined(); 
+    $httpBackend = $injector.get('$httpBackend');
 
   }));
+
+  /*describe('image upload template', function() {
+
+    beforeEach(inject(function ($compile) {
+      var element = ('<image-upload></image-upload>');
+      element = $compile(element)(scope);
+      console.log(element);
+      scope.$digest();
+    }));
+
+    it('should contain an element with the class drop-box', function() {
+      console.log(element.find('.drop-box'));
+        expect(element.find('.drop-box')).toBeDefined();
+    });
+
+  });
+  */
+  describe('the image uploader', function() {
+
+    beforeEach(inject(function () {
+      
+      spyOn(Upload, 'upload').and.callFake(function() {
+        return imageJSON;
+      });
+
+      // make the mock query call
+      response = Upload.upload();
+      
+    }));
+
+    it('should be defined', function(){
+      expect(Upload).toBeDefined();
+    });
+
+    it('should be able to call the upload function', function(){
+      expect(Upload.upload).toHaveBeenCalled();
+    });
+
+    it('should get a response', function () {
+      expect(response).toBeDefined();
+    });
+
+  });
+
 });
