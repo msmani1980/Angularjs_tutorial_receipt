aa88507a53718cae817098252fe43e48d69b137c
Max Felker <max@bigroomstudios.com>
2015-05-19 02:46:45 -0400

    TSVPORATL-893 maxfelker | Merged feature/items into feature/items-ui and updated station exception test issue

--- test/spec/directives/input-station-exception.js
+++ test/spec/directives/input-station-exception.js
@@ -5,8 +5,7 @@ describe('Directive: inputStationException', function () {
   // load the directive's module
   beforeEach(module('ts5App'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
