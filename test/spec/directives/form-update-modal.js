69fdc8d686be4e8e908ef2195ff09d9508a04a04
Max Felker <max@bigroomstudios.com>
2015-05-20 19:50:19 -0400

    TSVPORTAL-895 maxfelker | Fixed a broken test statement

--- test/spec/directives/form-update-modal.js
+++ test/spec/directives/form-update-modal.js
@@ -5,8 +5,7 @@ describe('Directive: formUpdateModal', function () {
   // load the directive's module
   beforeEach(module('ts5App'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
