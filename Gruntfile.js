bd6da3d05b4952d0f3d571a02df5f5dc924f947c
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-05-22 08:53:11 -0500

    JSHINT - removed extra comma in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,7 +188,7 @@ module.exports = function (grunt) {
       },
       server: {
         options: {
-          map: true,
+          map: true
         },
         files: [{
           expand: true,
