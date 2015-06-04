8746285a8e4b68b093b2c994de6eb70a8cba7cb6
Max Felker <max@bigroomstudios.com>
2015-06-04 18:06:34 -0400

    TSVPORTAL-1333 | Added httpbackend checks and started to to direct element generation

--- test/spec/directives/form-success-modal.js
+++ test/spec/directives/form-success-modal.js
@@ -1,14 +1,40 @@
 'use strict';
 
-describe('Directive: formSuccessModal', function () {
+describe('The Success Form Modal', function () {
 
   // load the directive's module
   beforeEach(module('ts5App'));
+  beforeEach(module('template-module'));
 
-  var scope;
+  var scope, element, httpBackend;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $injector) {
     scope = $rootScope.$new();
+    httpBackend = $injector.get('$httpBackend');
+    httpBackend.whenGET('views/directives/form-success-modal.html').respond(
+      200, '');
   }));
-  
+
+  describe('form-success-modal element', function () {
+
+    beforeEach(inject(function ($compile) {
+      element = angular.element(
+        '<form-success-modal list-path="item-list" create-path="item-create"></form-success-modal>'
+      );
+      element = $compile(element)(scope);
+      scope.$digest();
+      console.log(element);
+    }));
+
+    it('should inject the directive', function () {
+      expect(element).toBeDefined();
+    });
+
+    // TODO: Finish this - modal not actullay being found
+    it('should contain a div with the modal class', function () {
+      expect(element.find('.modal')).toBeDefined();
+    });
+
+  });
+
 });
