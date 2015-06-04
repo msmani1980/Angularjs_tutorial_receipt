fd8803d69f11ef4a1341f8e5669708a926a7bffa
Max Felker <max@bigroomstudios.com>
2015-06-04 11:09:33 -0400

    TSVPORTAL-1352 | Refactored controller, broke things out into functions

--- app/scripts/controllers/stock-owner-item-list.js
+++ app/scripts/controllers/stock-owner-item-list.js
@@ -12,26 +12,23 @@ angular.module('ts5App')
   .controller('StockOwnerItemListCtrl', function ($scope, $http, itemsFactory,
     companiesFactory) {
 
+    $scope.search = {
+      startDate: '',
+      endDate: ''
+    };
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 10;
+    $scope.startDateFilter = '';
+    $scope.endDateFilter = '';
+    $scope.itemsList = null;
+
+    var todaysDate = moment().format();
+
     // TODO: Move to global function
     function formatDate(dateString, formatFrom, formatTo) {
       return moment(dateString, formatFrom).format(formatTo).toString();
     }
 
-    // set search and start dates to nothing
-    $scope.search = {
-      startDate: '',
-      endDate: ''
-    };
-
-    $scope.startDateFilter = '';
-    $scope.endDateFilter = '';
-
-    var todaysDate = moment().format();
-
-    // TODO: Finish this watch
-    $scope.$watch('search', function () {
-      //  console.log('search changed');
-    });
 
     // TODO: Set a watch on this
     $scope.formatDateFilter = function () {
@@ -43,98 +40,71 @@ angular.module('ts5App')
 
     };
 
-    // display loading modal
-    //angular.element('#loading').modal('show').find('p').text('Getting a list of items for you');
+    this.setPaginatedList = function () {
+      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+      var end = begin + $scope.itemsPerPage;
+      $scope.paginatedItems = $scope.itemsList.slice(begin, end);
+    };
 
-    $scope.currentPage = 1;
-    $scope.itemsPerPage = 10;
+    this.getItemsList = function () {
+      var $this = this;
+      itemsFactory.getItemsList({}).then(function (response) {
+        $scope.itemsList = response.retailItems;
+        $scope.totalItems = response.meta.count;
+        $this.setPaginatedList();
+        $scope.$watch('currentPage + itemsPerPage', function () {
+          $this.setPaginatedList();
+        });
+      });
+    };
+
+    this.getItemTypesList = function () {
+      itemsFactory.getItemTypesList().then(function (itemTypes) {
+        $scope.itemTypes = itemTypes;
+      });
+    };
+
+    this.getSalesCategoriesList = function () {
+      companiesFactory.getSalesCategoriesList(function (data) {
+        $scope.salesCategories = data.salesCategories;
+      });
+    };
+
+    this.getItemsList();
+    this.getItemTypesList();
+    this.getSalesCategoriesList();
 
     $scope.pageCount = function () {
       return Math.ceil($scope.items.length / $scope.itemsPerPage);
     };
 
-    // Get a list of items
-    itemsFactory.getItemsList({}).then(function (response) {
-
-      var items = response.retailItems;
-
-      $scope.totalItems = response.meta.count;
-
-      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
-      var end = begin + $scope.itemsPerPage;
-
-      // update the paginated items to display
-      $scope.paginatedItems = items.slice(begin, end);
-
-      itemsFactory.getItemTypesList().then(function (itemTypes) {
-        $scope.itemTypes = itemTypes;
-      });
-
-      // get sales categories
-      companiesFactory.getSalesCategoriesList(function (data) {
-        $scope.salesCategories = data.salesCategories;
-      });
-
-      // hide loading modal
-      angular.element('#loading').modal('hide');
-
-      // when current page and items per page change
-      $scope.$watch('currentPage + itemsPerPage', function () {
-
-        var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
-        var end = begin + $scope.itemsPerPage;
-
-        // update the paginated items to display
-        $scope.paginatedItems = items.slice(begin, end);
-
-      });
-
-
-    });
-
     $scope.removeItem = function (id, itemKey) {
-
       if (window.confirm('Are you sure you would like to remove this item?')) {
-
         angular.element('#loading').modal('show').find('p').text(
           'Removing your item');
-
         itemsFactory.removeItem(id).then(function () {
-
           angular.element('#loading').modal('hide');
-
           $scope.paginatedItems.splice(itemKey, 1);
-
         });
 
       }
-
     };
 
     $scope.isItemActive = function (startDate) {
-
       startDate = formatDate(startDate, 'YYYYMMDD', 'L');
-
       return moment(startDate).isBefore(todaysDate);
-
     };
 
     $scope.isItemInactive = function (endDate) {
-
       endDate = formatDate(endDate, 'YYYYMMDD', 'L');
-
       return moment(endDate).isBefore(todaysDate);
-
     };
 
     $scope.clearSearchFilters = function () {
-
       var filters = $scope.search;
-
       for (var filterKey in filters) {
         $scope.search[filterKey] = '';
       }
-
     };
 
   });
