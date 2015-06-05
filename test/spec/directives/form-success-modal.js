f8d5c73f5481e92bc6f1ae7614f63eda7096208b
Max Felker <max@bigroomstudios.com>
2015-06-05 16:27:42 -0400

    TSVPORTAL-1333 | Added test cases for modal elements inside the directive

--- test/spec/directives/form-success-modal.js
+++ test/spec/directives/form-success-modal.js
@@ -14,15 +14,43 @@ describe('The form success modal', function () {
     scope = $rootScope.$new();
   }));
 
-  describe('form-success-modal element', function () {
+  describe('element', function () {
     beforeEach(inject(function ($compile) {
-      element = angular.element('<form-success-modal></form-success-modal>');
+      element = angular.element(
+        '<form-success-modal></form-success-modal>');
       element = $compile(element)(scope);
       scope.$digest();
     }));
 
     it('should have a modal element', function () {
-      expect(element).toBeDefined();
+      expect(element.find('.modal')[0]).toBeDefined();
+    });
+
+    it('should have a modal-body ', function () {
+      expect(element.find('.modal-body')[0]).toBeDefined();
+    });
+
+    it('should tell the user the item was saved', function () {
+      expect(element.find('.modal-body h2').text()).toEqual(
+        ' Your item was created successfully!');
+    });
+
+    it('should have a modal-footer', function () {
+      expect(element.find('.modal-footer')[0]).toBeDefined();
+    });
+
+    it('should have two butons in the footer', function () {
+      expect(element.find('.modal-footer .btn').length).toEqual(2);
+    });
+
+    it('should have a button to return to the list', function () {
+      expect(element.find('.modal-footer .btn-list').length).toEqual(
+        1);
+    });
+
+    it('should have a button to create another item', function () {
+      expect(element.find('.modal-footer .btn-create').length).toEqual(
+        1);
     });
 
   });
