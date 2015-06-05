c812cb5d2beecfc2f306cb38011b846a2dff277c
Max Felker <max@bigroomstudios.com>
2015-06-05 11:27:38 -0400

    TSVPORTAL-1333 | Updated test cases to work just like date picker

--- test/spec/directives/form-success-modal.js
+++ test/spec/directives/form-success-modal.js
@@ -1,41 +1,35 @@
 'use strict';
 
-describe('The Success Form Modal', function () {
+describe('The form success modal', function () {
 
   // load the directive's module
   beforeEach(module('ts5App'));
+
   beforeEach(module('template-module'));
 
-  var scope, element, httpBackend;
+  var element,
+    scope,
+    httpBackend;
 
   beforeEach(inject(function ($rootScope, $injector) {
     scope = $rootScope.$new();
     httpBackend = $injector.get('$httpBackend');
     httpBackend.whenGET('views/directives/form-success-modal.html').respond(
       200, '');
-    scope.$digest();
   }));
 
-  describe('form-success-modal element', function () {
-
+  describe('success modal template', function () {
     beforeEach(inject(function ($compile) {
       element = angular.element(
-        '<form-success-modal list-path="item-list" create-path="item-create"></form-success-modal>'
-      );
+        '<form-success-modal></form-success-modal');
       element = $compile(element)(scope);
       scope.$digest();
-      console.log(element);
     }));
 
-    it('should inject the directive', function () {
+    it('should have a modal element', function () {
       expect(element).toBeDefined();
-    });
-
-    // TODO: Finish this - modal not actullay being found
-    it('should contain a div with the modal class', function () {
-      expect(element.find('.modal')).toBeDefined();
+      console.log(element);
     });
 
   });
-
 });
