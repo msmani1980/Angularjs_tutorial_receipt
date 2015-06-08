9fca795d2534c55bac9828a0df1901befbec3c6e
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-08 15:47:48 -0500

    TSVPORTAL-867 - added mock response JSON and fetching from API

--- app/scripts/services/company-cc-types-service.js
+++ app/scripts/services/company-cc-types-service.js
@@ -8,11 +8,22 @@
  * Service in the ts5App.
  */
 angular.module('ts5App')
-  .service('companyCcTypesService', function () {
+  .service('companyCcTypesService', function ($resource, ENV) {
+    var requestURL = ENV.apiUrl + 'api/companies/403/company-credit-card-types';
+    var requestParameters = {
+      limit: 50
+    };
+
+    var actions = {
+      getCCtypes: {
+        method: 'GET'
+      }
+    };
+
+    var requestResource = $resource(requestURL, requestParameters, actions);
+
     function getCCtypes() {
-      return {
-        companyCCTypes: []
-      };
+      return requestResource.getCCtypes().$promise;
     }
 
     return {
