db47e97820e21ffece1c24ee429972131e0f52c8
Max Felker <max@bigroomstudios.com>
2015-06-09 11:34:17 -0400

    TSVPORTAL-1440 | Refactored stock owner items create controller to match new updates to retail items. Added controller test cases, ported from Retail items

--- app/scripts/controllers/stock-owner-item-create.js
+++ app/scripts/controllers/stock-owner-item-create.js
@@ -1,5 +1,3 @@
-// TODO: Write tests for this controller
-
 'use strict';
 /*global moment*/
 /**
@@ -16,7 +14,7 @@ angular.module('ts5App')
 
     // TODO: Refactor so the company object is returned, right now it's retruning a num so ember will play nice
     var companyId = GlobalMenuService.company.get();
-
+    var $this = this;
     $scope.formData = {
       startDate: '',
       endDate: '',
@@ -31,184 +29,164 @@ angular.module('ts5App')
       globalTradeNumbers: [],
       costPrices: []
     };
-
     $scope.viewName = 'Create Stock Owner Item';
-
     $scope.buttonText = 'Create';
-
     $scope.itemIsActive = false;
-
     $scope.itemIsInactive = false;
+    $scope.editingItem = false;
 
-    $scope.viewOnly = $location.path().includes('item-view');
+    this.checkIfViewOnly = function () {
+      var path = $location.path();
+      if (path.search('/stock-owner-item-view') !== -1) {
+        $scope.viewOnly = true;
+      }
+    };
 
-    var editingItem = false;
+    this.setFormAsViewOnly = function () {
+      $scope.viewName = 'Viewing Item ' + $routeParams.id;
+    };
 
-    function setFormAsViewOnly() {
-
-      $scope.viewName = 'Viewing Stock Owner Item ' + $routeParams.id;
-
-    }
-
-    function setFormAsEdit() {
-
-      editingItem = true;
-
-      $scope.viewName = 'Edit Stock Owner Item ' + $routeParams.id;
-
+    this.setFormAsEdit = function () {
+      $scope.editingItem = true;
+      $scope.viewName = 'Edit Item ' + $routeParams.id;
       $scope.buttonText = 'Save';
+    };
 
-    }
-
-    function validateItemCompany(data) {
+    this.validateItemCompany = function (data) {
       return data.retailItem.companyId === companyId;
-    }
+    };
 
-    // gets an item to editingItem
-    function getItem(id) {
+    this.getItem = function (id) {
 
-      // display loading modal
-      angular.element('#loading').modal('show').find('p').text(
-        'We are getting Item ' + id);
+      var $this = this;
+
+      // TODO: Make this use a loadingModal.show() method
+      angular.element('#loading').modal('show').find('p')
+        .text('We are getting Item ' + id);
 
       itemsFactory.getItem(id).then(function (data) {
-
-        // hide loading modal
-        angular.element('#loading').modal('hide');
-
-        if (validateItemCompany(data)) {
-
-          upateFormData(data.retailItem);
-
+        if ($this.validateItemCompany(data)) {
+          $this.upateFormData(data.retailItem);
         } else {
           $location.path('/');
           return false;
         }
-
-
+        angular.element('#loading').modal('hide');
       });
 
-    }
+    };
+
+    this.checkIfViewOnly();
 
     if ($scope.viewOnly) {
-      setFormAsViewOnly();
+      this.setFormAsViewOnly();
     }
 
     if ($routeParams.id && !$scope.viewOnly) {
-      setFormAsEdit();
+      this.setFormAsEdit();
     }
 
-    if (editingItem || $scope.viewOnly) {
-
-      getItem($routeParams.id);
-
+    if ($scope.editingItem || $scope.viewOnly) {
+      this.getItem($routeParams.id);
     }
 
     // deserialize tag object from api
     function deserializeTags(itemData) {
-
       for (var tagKey in itemData.tags) {
-
         var tag = itemData.tags[tagKey];
-
         itemData.tags[tagKey] = tag.tagId.toString();
-
       }
-
     }
 
     // deserialize characteristics object from api
     function deserializeCharacteristics(itemData) {
-
       for (var characteristicKey in itemData.characteristics) {
-
         var characteristic = itemData.characteristics[characteristicKey];
-
         itemData.characteristics[characteristicKey] = characteristic.characteristicId
           .toString();
-
       }
 
     }
 
     // deserialize allergens object from api
     function deserializeAllergens(itemData) {
-
       for (var allergenkey in itemData.allergens) {
-
         var allergen = itemData.allergens[allergenkey];
-
         itemData.allergens[allergenkey] = allergen.allergenId.toString();
-
       }
-
     }
 
     // checks to see if the item is active
     function checkIfItemIsActive(itemData) {
-
       var today = moment().format();
-
       $scope.itemIsActive = moment(itemData.startDate).isBefore(today);
-
     }
 
     // checks to see if the item is inactive
     function checkIfItemIsInactive(itemData) {
-
       var today = moment().format();
-
       $scope.itemIsInactive = moment(itemData.endDate).isBefore(today);
-
     }
 
     // updates the $scope.formData
-    function upateFormData(itemData) {
-
+    this.upateFormData = function (itemData) {
+      if (!itemData) {
+        return false;
+      }
       itemData.startDate = formatDate(itemData.startDate, 'YYYYMMDD', 'L');
       itemData.endDate = formatDate(itemData.endDate, 'YYYYMMDD', 'L');
-
       checkIfItemIsInactive(itemData);
-
       if (!$scope.itemIsInactive) {
-
         checkIfItemIsActive(itemData);
-
       }
-
       deserializeTags(itemData);
-
       deserializeCharacteristics(itemData);
-
       deserializeAllergens(itemData);
 
+
       // TODO: turn this into a function
-      // Loop through images
       for (var imageIndex in itemData.images) {
-
         var image = itemData.images[imageIndex];
-
-        // format start and end dates
         image.startDate = formatDate(image.startDate, false, 'L');
         image.endDate = formatDate(image.endDate, false, 'L');
-
       }
 
       // TODO: turn this into a function
       for (var priceIndex in itemData.costPrices) {
-
         var price = itemData.costPrices[priceIndex];
-
         price.startDate = formatDate(price.startDate, false, 'L');
         price.endDate = formatDate(price.endDate, false, 'L');
-
         updatePriceGroup(priceIndex);
 
       }
 
       $scope.formData = itemData;
 
-    }
+    };
+
+    // gets a list of all currencies for the item
+    this.getMasterCurrenciesList = function () {
+      currencyFactory.getCompanyCurrencies(function (data) {
+        var masterCurrenciesList = [];
+        for (var key in data.response) {
+          var currency = data.response[key];
+          masterCurrenciesList[currency.id] = currency.code;
+        }
+        $scope.masterCurrenciesList = masterCurrenciesList;
+      });
+    };
+
+    // gets a list of price types for price group
+    this.getPriceTypesList = function () {
+
+      itemsFactory.getPriceTypesList(function (data) {
+        $scope.priceTypes = data;
+      });
+
+    };
+
+    this.getMasterCurrenciesList();
+    this.getPriceTypesList();
 
     itemsFactory.getItemsList({}).then(function (data) {
       $scope.items = data.retailItems;
@@ -220,10 +198,6 @@ angular.module('ts5App')
 
     itemsFactory.getItemTypesList(function (data) {
       $scope.itemTypes = data;
-    });
-
-    itemsFactory.getPriceTypesList(function (data) {
-      $scope.priceTypes = data;
     });
 
     itemsFactory.getCharacteristicsList(function (data) {
@@ -260,27 +234,19 @@ angular.module('ts5App')
     }
 
     $scope.$watch('formData', function (newData, oldData) {
-
-      // check item dates and make sure all dates fall within the acceptable dates
-      checkItemDates(newData, oldData);
-
-      // if a price group date changes, update currencies list
-      refreshPriceGroups(newData, oldData);
-
-
+      //checkItemDates(newData,oldData);
+      $this.refreshPriceGroups(newData, oldData);
     }, true);
 
     $scope.$watch('form.$valid', function (validity) {
-
       if (validity) {
         $scope.displayError = false;
       }
-
     });
 
 
     // when a price date is change for a price groupd or station, need to update currencies
-    function refreshPriceGroups(newData, oldData) {
+    this.refreshPriceGroups = function (newData, oldData) {
 
       if (!oldData) {
         return false;
@@ -315,51 +281,49 @@ angular.module('ts5App')
 
       }
 
-    }
+    };
 
     // check date ranges on items, price groups
-    function checkItemDates(newData, oldData) {
+    /*  function checkItemDates(newData, oldData) {
 
-      if (newData.startDate !== oldData.startDate || newData.endDate !==
-        oldData.endDate) {
+        if (newData.startDate !== oldData.startDate || newData.endDate !==
+          oldData.endDate) {
 
-        // TODO: Move this to it's own function
-        if (newData.costPrices.length > 0) {
+          // TODO: Move this to it's own function
+          if (newData.costPrices.length > 0) {
 
-          // loop through all the price groups
-          for (var priceIndex in $scope.formData.costPrices) {
+            // loop through all the price groups
+            for (var priceIndex in $scope.formData.costPrices) {
 
-            var price = $scope.formData.costPrices[priceIndex];
+              var price = $scope.formData.costPrices[priceIndex];
 
-            // if new item end date is before price start date
-            if (moment(newData.endDate).isBefore(price.startDate)) {
+              // if new item end date is before price start date
+              if (moment(newData.endDate).isBefore(price.startDate)) {
 
-              // set price start date as new item end date
-              price.startDate = newData.endDate;
+                // set price start date as new item end date
+                price.startDate = newData.endDate;
 
-            }
+              }
 
-            // if new item start date is after price start date
-            if (moment(newData.startDate).isAfter(price.startDate)) {
+              // if new item start date is after price start date
+              if (moment(newData.startDate).isAfter(price.startDate)) {
 
-              // set price start date as new item start date
-              price.startDate = newData.startDate;
+                // set price start date as new item start date
+                price.startDate = newData.startDate;
 
-            }
+              }
 
-          } // end price for loop
+            } // end price for loop
 
-        } // if price length is greater than 0
+          } // if price length is greater than 0
 
-      } // end if newData.startDate is different
+        } // end if newData.startDate is different
 
-    } // end checkItemDates
+      } // end checkItemDates */
 
     $scope.removeQRCode = function () {
-
       $scope.formData.qrCodeImgUrl = '';
       $scope.formData.qrCodeValue = '';
-
     };
 
     $scope.removeImage = function (key) {
@@ -381,6 +345,11 @@ angular.module('ts5App')
     $scope.removeGTIN = function (key) {
       $scope.formData.globalTradeNumbers.splice(key, 1);
     };
+
+    /*
+     * Price Groups
+     *
+     */
 
     $scope.addPriceGroup = function () {
 
@@ -430,193 +399,112 @@ angular.module('ts5App')
 
     // Formats the dates when sending the payload to the API
     function formatPayloadDates(itemData) {
-
-      // format stary and end date
       itemData.startDate = formatDate(itemData.startDate, 'L', 'YYYYMMDD');
       itemData.endDate = formatDate(itemData.endDate, 'L', 'YYYYMMDD');
-
-      // Loop through images
+      // TODO: Turn this into a function
       for (var imageIndex in itemData.images) {
-
         var image = itemData.images[imageIndex];
-
-        // format start and end dates
         image.startDate = formatDate(image.startDate, 'L', 'YYYYMMDD');
         image.endDate = formatDate(image.endDate, 'L', 'YYYYMMDD');
-
       }
-
-      // Loop through costPrices
+      // TODO: Turn this into a function
       for (var priceIndex in itemData.costPrices) {
-
         var price = itemData.costPrices[priceIndex];
-
-        // format start and end dates
         price.startDate = formatDate(price.startDate, 'L', 'YYYYMMDD');
         price.endDate = formatDate(price.endDate, 'L', 'YYYYMMDD');
-
       }
-
     }
 
     // cleans up invalid properties of payload before submitting
     function cleanUpPayload(itemData) {
-
-      // Loop through costPrices
       for (var priceIndex in itemData.costPrices) {
-
         var price = itemData.costPrices[priceIndex];
-
         delete price.code;
-
       }
-
     }
 
     // formats the tags for payload
     function formatTags(itemData) {
-
-      // loop through tags in form data
       for (var tagKey in itemData.tags) {
-
         var tagId = itemData.tags[tagKey];
-
-        // set tag as object and set tagId property
         itemData.tags[tagKey] = {
           tagId: tagId
         };
-
       }
-
     }
 
     // formats the allergens for payload
     function formatAllergens(itemData) {
-
-      // loop through allergens in form data
       for (var allergenKey in itemData.allergens) {
-
         var allergenId = itemData.allergens[allergenKey];
-
-        // set tag as object and set allergenId property
         itemData.allergens[allergenKey] = {
           allergenId: allergenId
         };
-
       }
-
     }
 
     // formats the characteristics for payload
     function formatCharacteristics(itemData) {
-
-      // loop through characteristics in form data
       for (var characteristicKey in itemData.characteristics) {
-
         var characteristicId = itemData.characteristics[characteristicKey];
-
-        // set tag as object and set characteristicId property
         itemData.characteristics[characteristicKey] = {
           characteristicId: characteristicId
         };
-
       }
-
     }
 
-    function updateItem(itemData) {
-
-      // display loading modal
+    this.updateItem = function (itemData) {
+      var $this = this;
       angular.element('#loading').modal('show').find('p').text(
         'We are updating your item');
-
       var updateItemPayload = {
         retailItem: itemData
       };
-
-      // update itemData in API
       itemsFactory.updateItem($routeParams.id, updateItemPayload).then(
         function (response) {
-
-          upateFormData(response.retailItem);
-
+          $this.upateFormData(response.retailItem);
           angular.element('#loading').modal('hide');
-
           angular.element('#update-success').modal('show');
-
-          // error handler
         },
         function (response) {
-
           angular.element('#loading').modal('hide');
-
           $scope.displayError = true;
           $scope.formErrors = response.data;
-
         });
-
-    }
+    };
 
     function createItem(itemData) {
-
-      // display loading modal
       angular.element('#loading').modal('show').find('p').text(
         'We are creating your item');
-
       var newItemPayload = {
         retailItem: itemData
       };
-
       itemsFactory.createItem(newItemPayload).then(function () {
-
         angular.element('#loading').modal('hide');
-
         angular.element('#create-success').modal('show');
-
-        // error response
       }, function (error) {
-
         angular.element('#loading').modal('hide');
-
         $scope.displayError = true;
         $scope.formErrors = error.data;
-
       });
-
     }
 
-    // Submit function to proces form and hit the api
     $scope.submitForm = function (formData) {
 
       if (!$scope.form.$valid) {
-
         $scope.displayError = true;
-
         return false;
-
       }
-
-      // copy the form data to the itemData
       var itemData = angular.copy(formData);
-
       formatTags(itemData);
-
       formatCharacteristics(itemData);
-
       formatAllergens(itemData);
-
       formatPayloadDates(itemData);
-
       cleanUpPayload(itemData);
-
-      if (editingItem) {
-
-        updateItem(itemData);
-
+      if ($scope.editingItem) {
+        $this.updateItem(itemData);
       } else {
-
         createItem(itemData);
-
       }
 
     };
