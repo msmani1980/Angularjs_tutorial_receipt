a4ddb26855b12ae5bf7fbf0f3d0404534aa8193f
Kelly Meath <kmeath@egate-solutions.com>
2015-06-09 17:14:11 -0500

    TSVPORTAL-1811 - yo created cash bag service and test

---
+++ app/scripts/services/cash-bag-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ts5App.cashBagService
+ * @description
+ * # cashBagService
+ * Service in the ts5App.
+ */
+angular.module('ts5App')
+  .service('cashBagService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
