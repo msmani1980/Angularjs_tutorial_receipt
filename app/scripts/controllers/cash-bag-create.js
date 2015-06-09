6ea7cf7eacda699d1d013f96ebbe03b251f0de33
Kelly Meath <kmeath@egate-solutions.com>
2015-06-09 14:09:33 -0500

    TSVPORTAL-1809 finished initial markup.

--- app/scripts/controllers/cash-bag-create.js
+++ app/scripts/controllers/cash-bag-create.js
@@ -14,4 +14,17 @@ angular.module('ts5App')
       'AngularJS',
       'Karma'
     ];
+    var mochCashBag = {
+     flight: 'TODO: flight',
+    	date1: 'TODO: date1',
+    	baseCurrency: 'TODO: DATA',
+    	date2: 'TODO: date2',
+    	flightSchedules: [{id:1,name:'test'},{id:2,name:'test 2'}],
+    	exchanges: [
+    	  ['CHF',1400.75,10.00,13.75,0.1844,0.1844],
+    	  ['USD',6.66,6.00,0.66,0.1844,0.1844]
+    	]
+    };
+    
+    $scope.cashBag = mochCashBag;
   });
