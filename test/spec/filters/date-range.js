3eff950f63cf5057e5fad6fb2e9a0fca9765bd7b
Jared Donisvitch <jared@bigroomstudios.com>
2015-06-09 13:59:18 -0400

    TSVPORTAL-1232 | add tests, one specifically to test that date filters are not required, remove asterisks and change tests to reflect expectiations of date picker

--- test/spec/filters/date-range.js
+++ test/spec/filters/date-range.js
@@ -8,10 +8,11 @@ describe('Filter: dateRange |', function () {
 
   beforeEach(module('served/items-list.json'));
 
-  beforeEach(inject(function($filter) {
+  beforeEach(inject(function ($filter) {
     dateRange = $filter('daterange');
 
-    dateRange.filterItems = function(items, startDateFilter, endDateFilter){
+    dateRange.filterItems = function (items, startDateFilter,
+      endDateFilter) {
       return dateRange(items, startDateFilter, endDateFilter);
     };
 
@@ -25,15 +26,30 @@ describe('Filter: dateRange |', function () {
     expect(dateRange).toBeDefined();
   }));
 
-  it('should return an identical array of items, if no date filters are passed', inject(function () {
-    var items = dateRange(itemsJSON);
-    expect(items).toEqual(itemsJSON);
-  }));
+  it('should have itemsJSON injected and defined for testing', inject(
+    function () {
+      expect(itemsJSON).toBeDefined();
+    }));
+
+  it(
+    'should return an identical array of items, if no date filters are passed',
+    inject(function () {
+      var items = dateRange(itemsJSON);
+      expect(items).toEqual(itemsJSON);
+    }));
 
   it('should be called with a start and an end date', inject(function () {
     spyOn(dateRange, 'filterItems');
     dateRange.filterItems(itemsJSON, '2015-05-15', '2015-07-15');
-    expect(dateRange.filterItems).toHaveBeenCalledWith(itemsJSON, '2015-05-15', '2015-07-15');
+    expect(dateRange.filterItems).toHaveBeenCalledWith(itemsJSON,
+      '2015-05-15', '2015-07-15');
+  }));
+
+  it('should be called with a start date', inject(function () {
+    spyOn(dateRange, 'filterItems');
+    dateRange.filterItems(itemsJSON, '2015-05-15');
+    expect(dateRange.filterItems).toHaveBeenCalledWith(itemsJSON,
+      '2015-05-15');
   }));
 
   it('should return an array of filtered items', inject(function () {
@@ -41,9 +57,16 @@ describe('Filter: dateRange |', function () {
     expect(items.length).toBe(8);
   }));
 
-  it('should return a an empty array, if dates criteria are not met', inject(function () {
-    var items = dateRange(itemsJSON, '2015-05-15', '2015-05-15');
-    expect(items.length).toBe(0);
-  }));
+  it('should return a an empty array, if dates are entered but faulty',
+    inject(function () {
+      var items = dateRange(itemsJSON, '2015-05-15', '2015-05-15');
+      expect(items.length).toBe(0);
+    }));
+
+  it('should return all items, when no date filters passed', inject(
+    function () {
+      var items = dateRange(itemsJSON);
+      expect(items.length).toBe(17);
+    }));
 
 });
