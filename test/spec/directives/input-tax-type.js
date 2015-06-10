c84bff6ae3d4e53cb34588b5968953ab8ae989bc
Max Felker <max@bigroomstudios.com>
2015-06-10 10:51:51 -0400

    TSVPORTAL-1361 | Updated tax type directive to use BS container, updated directive template URL path, created test cases

--- test/spec/directives/input-tax-type.js
+++ test/spec/directives/input-tax-type.js
@@ -1,15 +1,67 @@
 'use strict';
 
-describe('Directive: inputTaxType', function () {
+describe('The tax type input directive', function () {
 
   // load the directive's module
   beforeEach(module('ts5App'));
 
-  var scope;
+  beforeEach(module('template-module'));
+
+  var scope,
+    element;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
 
+  describe('element', function () {
+
+    beforeEach(inject(function ($compile) {
+      element = angular.element(
+        '<input-tax-type></input-tax-type>');
+      element = $compile(element)(scope);
+      scope.$digest();
+    }));
+
+    it('should have a panel element', function () {
+      expect(element.find('.panel')[0]).toBeDefined();
+    });
+
+    describe('panel heading', function () {
+
+      it('should have a panel-heading', function () {
+        expect(element.find('.panel-title')[0]).toBeDefined();
+      });
+
+      it('should have a panel-title as a child of panel-heading',
+        function () {
+          expect(element.find('.panel-heading .panel-title')[0]).toBeDefined();
+        });
+
+      it('should have the correct heading label', function () {
+        expect(element.find('.panel-title').text().trim()).toEqual(
+          'Tax Type 1');
+      });
+
+    });
+
+    describe('panel body', function () {
+
+      it('should have a panel-body', function () {
+        expect(element.find('.panel-body')[0]).toBeDefined();
+      });
+
+      it('should have a row', function () {
+        expect(element.find('.panel-body .row')[0]).toBeDefined();
+      });
+
+      it('should have a two columns in the row', function () {
+        expect(element.find('.row .col-xs-12').length).toEqual(
+          2);
+      });
+
+    });
+
+  });
 
 });
