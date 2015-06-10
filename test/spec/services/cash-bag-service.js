0f2d1b343a67cd741ea15a5f9f5c9202d1aff716
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-10 16:53:56 -0500

    TSVPORTAL-1940 - fixed JSHint errors

--- test/spec/services/cash-bag-service.js
+++ test/spec/services/cash-bag-service.js
@@ -9,7 +9,11 @@ describe('Service: cashBagService', function () {
   var cashBagService,
     $httpBackend,
     responseJSON,
-    headers = {companyId:362,"Accept":"application/json, text/plain, */*","userId":1};
+    headers = {
+      companyId: 362,
+      'Accept': 'application/json, text/plain, */*',
+      'userId': 1
+    };
 
   beforeEach(inject(function (_cashBagService_, $injector) {
     inject(function (_servedCashBag_) {
@@ -40,7 +44,7 @@ describe('Service: cashBagService', function () {
     describe('getCashBagList', function () {
 
       beforeEach(function () {
-        $httpBackend.whenGET(/cash-bags/,headers).respond(responseJSON);
+        $httpBackend.whenGET(/cash-bags/, headers).respond(responseJSON);
 
         cashBagService.getCashBagList().then(function (dataFromAPI) {
           cashBagData = dataFromAPI;
@@ -52,20 +56,20 @@ describe('Service: cashBagService', function () {
         expect(Object.prototype.toString.call(cashBagData.cashBags)).toBe('[object Array]');
       });
 
-      it('should have isSubmitted property', function(){
+      it('should have isSubmitted property', function () {
         expect(cashBagData.cashBags[0].isSubmitted).not.toBe(null);
       });
 
-      it('should have retailCompanyId property', function(){
+      it('should have retailCompanyId property', function () {
         expect(cashBagData.cashBags[0].retailCompanyId).not.toBe(null);
       });
 
-      it('should have cashbagSubmittedBy property', function(){
+      it('should have cashbagSubmittedBy property', function () {
         expect(cashBagData.cashBags[0].cashbagSubmittedBy).not.toBe(undefined);
       });
     });
 
-    describe('api call parameters', function() {
+    describe('api call parameters', function () {
       it('should have a company id as payload', function () {
         var companyId = 413;
         // TODO: fix regex to not include limit=50
