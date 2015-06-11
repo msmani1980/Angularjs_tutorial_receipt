0b0f971887f2a305223046974daed1506a9d99fb
Jared Donisvitch <jared@bigroomstudios.com>
2015-06-11 16:15:38 -0400

    TSVPORTAL-1339 | add tests for modal in nav and also for the controller

--- test/spec/directives/left-navigation.js
+++ test/spec/directives/left-navigation.js
@@ -86,8 +86,45 @@ describe('Directive: leftNavigation', function () {
 
         });
 
-    });
+      it('should contain an element with a modal class', function () {
+        expect(element.find('.modal')).toBeDefined();
+      });
 
+      it('should contain an element with a fade class', function () {
+        expect(element.find('.fade')).toBeDefined();
+      });
+
+      it('should have a modal-content element', function () {
+        expect(element.find('.modal-content')).toBeDefined();
+      });
+
+      it('should have a modal-body element', function () {
+        expect(element.find('.modal-body')).toBeDefined();
+      });
+
+      it('should have a modal-footer element', function () {
+        expect(element.find('.modal-footer')).toBeDefined();
+      });
+
+      it('should have a leave button', function () {
+        expect(element.find('.btn-default')).toBeDefined();
+      });
+
+      it('should have a leave button that contains text', function () {
+        expect(element.find('.btn-default').text()).toContain(
+          'Discard Changes');
+      });
+
+      it('should have a save button', function () {
+        expect(element.find('.btn-default')).toBeDefined();
+      });
+
+      it('should have a save button that contains text', function () {
+        expect(element.find('.btn-primary').text()).toContain(
+          'Continue Editing');
+      });
+
+    });
 
   describe('directive isolated scope', function () {
 
@@ -97,14 +134,68 @@ describe('Directive: leftNavigation', function () {
       isolatedScope = element.isolateScope();
     }));
 
-    it('should have a navigateTo method attached to the scope',
+    it('should have a leaveViewNav method attached to the scope',
       function () {
         expect(isolatedScope.leaveViewNav).toBeDefined();
       });
 
+
+    it(
+      'should have a leaveViewClose function defined',
+      function () {
+        expect(isolatedScope.leaveViewClose).toBeDefined();
+      });
+
+
+    it('should be able to call leaveViewNav()', function () {
+      spyOn(isolatedScope, 'leaveViewNav').and.callThrough();
+      isolatedScope.leaveViewNav();
+      expect(isolatedScope.leaveViewNav).toHaveBeenCalled();
+    });
+
+    it('should contain a leavePathNav variable', function () {
+      isolatedScope.leaveViewNav('test-path');
+      expect(isolatedScope.leavePathNav).toBeDefined();
+    });
+
+    it(
+      'should return the leavePathNav variable, after being passed into leaveViewNav',
+      function () {
+        isolatedScope.leaveViewNav('test-path');
+        expect(isolatedScope.leavePathNav).toContain('test-path');
+      });
+
+
+    it('should contain the function leaveViewClose()', function () {
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leaveViewClose).toBeDefined();
+    });
+
+    it('should be able to call leaveViewClose()', function () {
+      spyOn(isolatedScope, 'leaveViewClose').and.callThrough();
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leaveViewClose).toHaveBeenCalled();
+    });
+
+    it(
+      'should contain a leavePathNav variable after leaveViewNav is called and closed',
+      function () {
+        isolatedScope.leaveViewNav('test-path');
+        isolatedScope.leaveViewClose();
+        expect(isolatedScope.leavePathNav).toBeDefined();
+      });
+
+    it(
+      'should return the leavePath variable, after being passed into leaveView and closed',
+      function () {
+        isolatedScope.leaveViewNav('test-path');
+        isolatedScope.leaveViewClose();
+        expect(isolatedScope.leavePathNav).toContain('test-path');
+      });
+
   });
 
-  describe('when the list-path attribute is passed', function () {
+  describe('when the base-path attribute is passed, it', function () {
 
     var isolatedScope;
 
@@ -127,6 +218,118 @@ describe('Directive: leftNavigation', function () {
         expect(element.attr('base-path')).toEqual(isolatedScope.basePath);
       });
 
+    it('should have a itemListPath variable',
+      function () {
+        expect(isolatedScope.itemListPath).toBeDefined();
+      });
+
+    it('should have a itemCreatePath variable',
+      function () {
+        expect(isolatedScope.itemCreatePath).toBeDefined();
+      });
+
+    it('should have a manageCategoriesPath variable',
+      function () {
+        expect(isolatedScope.manageCategoriesPath).toBeDefined();
+      });
+
+    it('should have a itemListPath variable that contains /item-list',
+      function () {
+        expect(isolatedScope.itemListPath).toContain('/item-list');
+      });
+
+    it(
+      'should have a itemCreatePath variable that contains /item-create',
+      function () {
+        expect(isolatedScope.itemCreatePath).toContain('/item-create');
+      });
+
+    it(
+      'should have a manageCategoriesPath variable that contains /item-create',
+      function () {
+        expect(isolatedScope.manageCategoriesPath).toContain(
+          'categories');
+      });
+
   });
+
+  describe(
+    'when the base-path attribute is passed with stock-owner-items, it',
+    function () {
+
+      var isolatedScope;
+
+      beforeEach(inject(function ($compile) {
+        element = angular.element(
+          '<left-navigation base-path="stock-owner-items"></left-navigation>'
+        );
+        element = $compile(element)(scope);
+        scope.$digest();
+        isolatedScope = element.isolateScope();
+      }));
+
+      it(
+        'should have a itemListPath variable that contains /stock-owner-item-list',
+        function () {
+          expect(isolatedScope.itemListPath).toContain(
+            '/stock-owner-item-list');
+        });
+
+      it(
+        'should have a itemCreatePath variable that contains /stock-owner-item-create',
+        function () {
+          expect(isolatedScope.itemCreatePath).toContain(
+            '/stock-owner-item-create');
+        });
+
+      it(
+        'should have a manageCategoriesPath variable that contains /stock-owner-item',
+        function () {
+          expect(isolatedScope.manageCategoriesPath).toContain(
+            '/stock-owner-item');
+        });
+
+    });
+
+  describe('when the controller is accessed, it',
+    function () {
+
+      it(
+        'should have a setModalElement function defined',
+        function () {
+          expect(controller.setModalElement).toBeDefined();
+        });
+      it(
+        'should have a setModalElement function set modelElement',
+        function () {
+          controller.setModalElement();
+          expect(controller.modalElement).toBeDefined();
+        });
+
+      it(
+        'should have a showModal function defined',
+        function () {
+          expect(controller.showModal).toBeDefined();
+        });
+
+      it(
+        'should have a hideModal function defined',
+        function () {
+          expect(controller.hideModal).toBeDefined();
+        });
+
+      it(
+        'should have a navigateTo function defined',
+        function () {
+          expect(controller.navigateTo).toBeDefined();
+        });
+
+      it(
+        'should have a checkIfEditing function defined',
+        function () {
+          expect(controller.checkIfEditing).toBeDefined();
+        });
+
+    });
 
 });
