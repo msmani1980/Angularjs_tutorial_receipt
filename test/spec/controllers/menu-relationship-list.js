1d7cce57ad124b66242fa6a79147571e4e6966d9
Jared Donisvitch <jared@bigroomstudios.com>
2015-06-12 11:55:53 -0400

    TSVPORTAL-1532 | Added test cases for menu api response

--- test/spec/controllers/menu-relationship-list.js
+++ test/spec/controllers/menu-relationship-list.js
@@ -7,17 +7,17 @@ describe('Controller: MenuRelationshipListCtrl', function () {
     $controller,
     $location,
     MenuRelationshipListCtrl,
-    $httpBackend;
+    menuAPIResponse;
 
   beforeEach(module('ts5App'));
   //beforeEach(module('ts5App', 'template-module'));
-  /*beforeEach(module(
-    'served/caterer-stations-list.json'
-  ));*/
+  beforeEach(module(
+    'served/menus.json'
+  ));
 
 
-  beforeEach(inject(function (_$rootScope_, _$controller_, $injector) {
-    $httpBackend = $injector.get('$httpBackend');
+  beforeEach(inject(function (_$rootScope_, _$controller_, $injector,
+    _servedMenus_) {
     $location = $injector.get('$location');
     $location.path('/menu-relationship-list');
     $rootScope = _$rootScope_;
@@ -27,6 +27,7 @@ describe('Controller: MenuRelationshipListCtrl', function () {
       '$rootScope': $rootScope,
       '$scope': $scope
     });
+    menuAPIResponse = _servedMenus_;
   }));
 
   describe('The MenuRelationshipListCtrl', function () {
@@ -41,5 +42,56 @@ describe('Controller: MenuRelationshipListCtrl', function () {
 
   });
 
+  describe('Menu API response', function () {
+
+    it('should be defined', function () {
+      expect(menuAPIResponse).toBeDefined();
+    });
+
+    it('should be contain a menus array', function () {
+      expect(menuAPIResponse.menus).toBeDefined();
+    });
+
+    it('should be contain at least one object in the menus array',
+      function () {
+        expect(menuAPIResponse.menus.length).toBeGreaterThan(0);
+      });
+
+    describe('menu object', function () {
+
+      var menuObject;
+
+      beforeEach(function () {
+        menuObject = menuAPIResponse.menus[0];
+      });
+
+      it('should be defined', function () {
+        expect(menuObject).toBeDefined();
+      });
+
+      it('should have an id and it is a number', function () {
+        expect(menuObject.id).toBeDefined();
+        expect(menuObject.id).toEqual(jasmine.any(Number));
+      });
+
+      it('should have a companyId and it is a number', function () {
+        expect(menuObject.companyId).toBeDefined();
+        expect(menuObject.companyId).toEqual(jasmine.any(Number));
+      });
+
+      it('should have a menuCode and it is a number', function () {
+        expect(menuObject.menuCode).toBeDefined();
+        expect(menuObject.menuCode).toEqual(jasmine.any(String));
+      });
+
+      it('should have a menuName and it is a string', function () {
+        expect(menuObject.menuName).toBeDefined();
+        expect(menuObject.menuName).toEqual(jasmine.any(String));
+      });
+
+    });
+
+
+  });
 
 });
