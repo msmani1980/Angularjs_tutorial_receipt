9dc52b9c71540b713b2853437e5c9b328012c434
Jared Donisvitch <jared@bigroomstudios.com>
2015-06-12 11:43:35 -0400

    TSVPORTAL-1532 | Added tests for controller

--- app/scripts/controllers/menu-relationship-list.js
+++ app/scripts/controllers/menu-relationship-list.js
@@ -9,9 +9,5 @@
  */
 angular.module('ts5App')
   .controller('MenuRelationshipListCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.helloWorld = 'scott';
   });
--- test/spec/controllers/menu-relationship-list.js
+++ test/spec/controllers/menu-relationship-list.js
@@ -2,21 +2,44 @@
 
 describe('Controller: MenuRelationshipListCtrl', function () {
 
-  // load the controller's module
+  var $rootScope,
+    $scope,
+    $controller,
+    $location,
+    MenuRelationshipListCtrl,
+    $httpBackend;
+
   beforeEach(module('ts5App'));
+  //beforeEach(module('ts5App', 'template-module'));
+  /*beforeEach(module(
+    'served/caterer-stations-list.json'
+  ));*/
 
-  var MenuRelationshipListCtrl,
-    scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function (_$rootScope_, _$controller_, $injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    $location = $injector.get('$location');
+    $location.path('/menu-relationship-list');
+    $rootScope = _$rootScope_;
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
     MenuRelationshipListCtrl = $controller('MenuRelationshipListCtrl', {
-      $scope: scope
+      '$rootScope': $rootScope,
+      '$scope': $scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  describe('The MenuRelationshipListCtrl', function () {
+
+    it('should be defined', function () {
+      expect(MenuRelationshipListCtrl).toBeDefined();
+    });
+
+    it('should have a the route /menu-relationship-list', function () {
+      expect($location.path()).toBe('/menu-relationship-list');
+    });
+
   });
+
+
 });
