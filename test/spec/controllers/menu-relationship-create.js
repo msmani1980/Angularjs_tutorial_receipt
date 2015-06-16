2ac01374467d44f295096b287d6be0564b6ea20d
Jared Donisvitch <jared@bigroomstudios.com>
2015-06-16 16:35:49 -0400

    TSVPORTAL-1532 | Add view and container test cases to menu create view

--- test/spec/controllers/menu-relationship-create.js
+++ test/spec/controllers/menu-relationship-create.js
@@ -2,21 +2,81 @@
 
 describe('Controller: MenuRelationshipCreateCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('ts5App'));
+  var $rootScope,
+    $scope,
+    $controller,
+    $location,
+    MenuRelationshipCreateCtrl,
+    menuAPIResponse,
+    stationAPIResponse;
 
-  var MenuRelationshipCreateCtrl,
-    scope;
+  beforeEach(module('ts5App', 'template-module'));
+  beforeEach(module('served/menus.json', 'served/caterer-stations.json'));
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MenuRelationshipCreateCtrl = $controller('MenuRelationshipCreateCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function (_$rootScope_, _$controller_, $injector,
+    _servedMenus_, _servedCatererStations_) {
+    $location = $injector.get('$location');
+    $location.path('/menu-relationship-create');
+    $rootScope = _$rootScope_;
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
+    MenuRelationshipCreateCtrl = $controller(
+      'MenuRelationshipCreateCtrl', {
+        '$rootScope': $rootScope,
+        '$scope': $scope
+      });
+    menuAPIResponse = _servedMenus_;
+    stationAPIResponse = _servedCatererStations_;
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  describe('The MenuRelationshipCreateCtrl', function () {
+
+    it('should be defined', function () {
+      expect(MenuRelationshipCreateCtrl).toBeDefined();
+    });
+
+    it('should have a the route /menu-relationship-create', function () {
+      expect($location.path()).toBe('/menu-relationship-create');
+    });
+
   });
+
+  describe('view', function () {
+
+    var $templateCache,
+      $compile,
+      html,
+      view;
+
+    beforeEach(inject(function (_$templateCache_, _$compile_) {
+      $templateCache = _$templateCache_;
+      $compile = _$compile_;
+      html = $templateCache.get(
+        '/views/menu-relationship-create.html');
+      var compiled = $compile(angular.element(html))($scope);
+      view = angular.element(compiled[0]);
+      $scope.$digest();
+    }));
+
+    it('should be defined', function () {
+      expect(view).toBeDefined();
+    });
+
+    describe('container', function () {
+
+      var container;
+
+      beforeEach(function () {
+        container = angular.element(view.find(
+          '.fluid-container')[0]);
+      });
+
+      it('should be defined', function () {
+        expect(container).toBeDefined();
+      });
+
+    });
+
+  });
+
 });
