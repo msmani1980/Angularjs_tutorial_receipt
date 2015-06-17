2f8513a413d1c56396fd0acc48d7877dc1f9ac45
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-17 12:46:39 -0500

    TSVPORTAL-660 - added files

---
+++ test/spec/controllers/item-import.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: ItemImportCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('ts5App'));
+
+  var ItemImportCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    ItemImportCtrl = $controller('ItemImportCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
