18fca67139018e28ebdc2f232c013c6dcee01256
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-19 10:57:37 -0500

    TSVPORTAL-660 | hotfix | fixed JSHint errors

--- app/scripts/controllers/post-trip-data.js
+++ app/scripts/controllers/post-trip-data.js
@@ -10,5 +10,5 @@
 angular.module('ts5App')
   .controller('PostFlightDataCtrl', function ($scope) {
     $scope.viewName = 'Post Trip Data';
-    $(".employeeID-multiple-select").select2();
+    $('.employeeID-multiple-select').select2();
   });
