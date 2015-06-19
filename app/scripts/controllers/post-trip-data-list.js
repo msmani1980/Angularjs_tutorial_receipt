73e7e8457a1325fed7d32fcfd28661755bef8663
Tim Gunderson <TGunderson@egate-solutions.com>
2015-06-19 10:51:58 -0500

    TSVPORTAL-1379 tgunderson | remove TODO for download template modal

--- app/scripts/controllers/post-trip-data-list.js
+++ app/scripts/controllers/post-trip-data-list.js
@@ -4,31 +4,10 @@
  * @ngdoc function
  * @name ts5App.controller:PostFlightDataCtrl
  * @description
- * # PostFlightDataCtrl
+ * # PostFlightDataListCtrl
  * Controller of the ts5App
  */
 angular.module('ts5App')
   .controller('PostFlightDataListCtrl', function ($scope) {
     $scope.viewName = 'Post Trip Data Management';
-    
-// TODO: DOWNLOAD TEMPLATE MODAL
-//    $scope.checkVarianceAndSave = function(shouldSubmit) {
-//        if (!$scope.dailyExchangeRatesForm.$valid){
-//          return false;
-//        }
-//        serializePreviousExchangeRates();
-//        createPayload(shouldSubmit);
-//
-//        $scope.varianceObject = calculateVariance();
-//        if (Object.keys($scope.varianceObject).length > 0) {
-//          angular.element('.variance-warning-modal').modal('show');
-//          return;
-//        }
-//        $scope.saveDailyExchangeRates();
-//      };
-//
-//      $scope.saveDailyExchangeRates = function () {
-//        angular.element('.variance-warning-modal').modal('hide');
-//        currencyFactory.saveDailyExchangeRates($scope.payload).then(successRequestHandler, showErrors);
-//      };
-  });
+});
