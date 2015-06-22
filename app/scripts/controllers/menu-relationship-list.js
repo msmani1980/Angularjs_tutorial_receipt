489c51b4e94f3af1cf6cc6ec97a859dfb4392139
Max Felker <max@bigroomstudios.com>
2015-06-22 18:21:38 -0400

    TSVPORTAL-1535 | Added controller functionality, menu service calls and added pagination. Tests are super broken, refactoring

--- app/scripts/controllers/menu-relationship-list.js
+++ app/scripts/controllers/menu-relationship-list.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global moment */
+
 /**
  * @ngdoc function
  * @name ts5App.controller:MenuRelationshipListCtrl
@@ -8,106 +8,70 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('MenuRelationshipListCtrl', function ($scope) {
-    var menuAPIResponse,
-      stationAPIResponse;
+  .controller('MenuRelationshipListCtrl', function ($scope, dateUtility,
+    $filter, menuService) {
 
-    var todaysDate = Date.parse(new Date());
-
-    // TODO: Move to global function
-    function formatDate(dateString, formatFrom, formatTo) {
-      var dateToReturn = moment(dateString, formatFrom).format(formatTo).toString();
-      return dateToReturn;
-    }
-
-    // set search and start dates to nothing
-    $scope.search = {
+    var $this = this;
+    $scope.currentPage = 1;
+    $scope.recordsPerPage = 10;
+    $scope.recordList = [];
+    $scope.dateRange = {
       startDate: '',
       endDate: ''
     };
-    $scope.startDateFilter = '';
-    $scope.endDateFilter = '';
+    var stationAPIResponse;
 
-    $scope.$watch('search.startDate + search.endDate', function () {
-      $scope.formatDateFilter();
+    $scope.$watch('dateRange.startDate + dateRange.endDate', function () {
+      $scope.updateRecordList();
     });
 
-    $scope.formatDateFilter = function () {
-      if ($scope.search.startDate && $scope.search.endDate) {
-        $scope.startDateFilter = formatDate($scope.search.startDate, 'L',
-          'YYYY-MM-DD');
-        $scope.endDateFilter = formatDate($scope.search.endDate, 'L',
-          'YYYY-MM-DD');
-      }
+    this.updateRecordList = function () {
+      var filteredRecords = $this.filterItems();
+      $scope.recordListCount = filteredRecords.length;
+      $this.setPaginatedRecords(filteredRecords);
+    };
+
+    this.filterItems = function () {
+      var dateFiltered = $filter('daterange')($scope.recordList, $scope.dateRange
+        .startDate,
+        $scope.dateRange.endDate);
+      return $filter('filter')(dateFiltered, $scope.search);
+    };
+
+    this.setPaginatedRecords = function (filteredRecords) {
+      var begin = (($scope.currentPage - 1) * $scope.recordsPerPage);
+      var end = begin + $scope.recordsPerPage;
+      $scope.paginatedRecords = filteredRecords.slice(begin, end);
+    };
+
+    this.getRecordList = function () {
+      var $this = this;
+      menuService.getMenuList().then(function (response) {
+        $scope.recordList = response.menus;
+        $scope.recordListCount = $scope.recordList.length;
+        $this.updateRecordList();
+      });
     };
 
     $scope.isItemActive = function (startDate) {
-      return Date.parse(startDate) <= todaysDate;
+      return Date.parse(startDate) <= dateUtility.now();
     };
 
     $scope.isItemInactive = function (endDate) {
-      return Date.parse(endDate) <= todaysDate;
+      return Date.parse(endDate) <= dateUtility.now();
     };
 
     $scope.clearSearchFilters = function () {
+      $scope.dateRange.endDate = '';
+      $scope.dateRange.endDate = '';
       var filters = $scope.search;
-      $scope.startDate = '';
-      $scope.endDate = '';
-      $scope.startDateFilter = '';
-      $scope.endDateFilter = '';
       for (var filterKey in filters) {
         $scope.search[filterKey] = '';
       }
+      $scope.recordListCount = $scope.recordList.length;
     };
 
-    menuAPIResponse = {
-      'menus': [{
-        'id': 121,
-        'companyId': 374,
-        'menuCode': 'Test01',
-        'menuName': 'Test Menu',
-        'description': 'Test Menu',
-        'startDate': '2015-04-15',
-        'endDate': '2015-04-15',
-        'createdBy': 1,
-        'createdOn': '2015-04-14 02:49:35.715873',
-        'updatedBy': null,
-        'updatedOn': null,
-        'menuItems': [{
-          'id': 248,
-          'itemId': 331,
-          'itemName': 'Test Item',
-          'menuId': 121,
-          'itemQty': 1
-        }]
-      }, {
-        'id': 122,
-        'companyId': 374,
-        'menuCode': 'MNCD001',
-        'menuName': 'Menu Name',
-        'description': 'Description for menu, testing 2',
-        'startDate': '2015-04-29',
-        'endDate': '2015-05-30',
-        'createdBy': 1,
-        'createdOn': '2015-04-27 22:04:00.266856',
-        'updatedBy': 1,
-        'updatedOn': '2015-04-28 21:56:11.001683',
-        'menuItems': [{
-          'id': 249,
-          'itemId': 331,
-          'itemName': 'Test Item',
-          'menuId': 122,
-          'itemQty': 4
-        }]
-      }],
-      'meta': {
-        'count': 2,
-        'limit': 2,
-        'start': 0
-      }
-    };
-
-    $scope.menuList = menuAPIResponse.menus;
+    this.getRecordList();
 
     stationAPIResponse = {
 
