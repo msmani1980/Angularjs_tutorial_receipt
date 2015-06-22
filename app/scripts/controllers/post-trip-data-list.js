b9a7c1f8713973901984800dde4218ad9007c3a0
Alyssa Reyes <areyes@egate-solutions.com>
2015-06-22 16:11:01 -0500

    TSVPORTAL-158 | stubbed controller tests

--- app/scripts/controllers/post-trip-data-list.js
+++ app/scripts/controllers/post-trip-data-list.js
@@ -10,4 +10,16 @@
 angular.module('ts5App')
   .controller('PostFlightDataListCtrl', function ($scope) {
     $scope.viewName = 'Post Trip Data Management';
+    $scope.search = {};
+
+    $scope.searchPostTripData = function() {
+      // TODO: call API with search object
+    };
+
+    $scope.clearSearchForm = function() {
+      $scope.search = {};
+      // TODO: call API with empty search object
+    }
+
+
 });
