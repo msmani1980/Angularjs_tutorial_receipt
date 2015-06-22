6357debd94c9ba1818c8382f212cb8e6d3571ef1
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-22 13:58:47 -0500

    TSVPORTAL-660 - filters working

--- app/scripts/controllers/item-import.js
+++ app/scripts/controllers/item-import.js
@@ -12,11 +12,23 @@ angular.module('ts5App')
     $scope.viewName = 'Import Stock Owner Items';
 
     // TODO: pull data from API
-    $scope.stockOwnerList = [{
+    $scope.stockOwnerList = [
+      {name: 'GRO'},
+      {name: 'Easy Jet'},
+      {name: 'Delta'}
+    ];
+
+    // TODO: pull data from API
+    $scope.stockOwnerItemsList = [{
       colorCode: '#00FF00',
       companyName: 'GRO',
       reference: 'CHP0524',
       itemName: 'Bananas'
+    }, {
+      colorCode: '#FFF',
+      companyName: 'Another one',
+      reference: 'NTHR1',
+      itemName: 'Apple'
     }];
 
     // TODO: pull data from API
@@ -25,6 +37,11 @@ angular.module('ts5App')
       companyName: 'GRO',
       reference: 'CHP0524',
       itemName: 'Bananas'
+    },{
+      colorCode: '#CCC',
+      companyName: 'One more here too',
+      reference: 'NMHRT2',
+      itemName: 'iPad Mini Retina'
     }];
 
   });
