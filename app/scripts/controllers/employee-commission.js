87f0fc386a77bb4a12fc00a10216c3e699a78655
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-06-23 14:23:48 -0500

    TSVPORTAL-1972 - added method to dateUtility, making call to get items on date change

--- app/scripts/controllers/employee-commission.js
+++ app/scripts/controllers/employee-commission.js
@@ -9,12 +9,25 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('EmployeeCommissionCtrl', function ($scope, employeeCommissionFactory) {
+  .controller('EmployeeCommissionCtrl', function ($scope, employeeCommissionFactory, dateUtility) {
 
     $scope.viewName = 'Employee Commission';
     $scope.startDate = moment().add(1, 'days').format('L').toString();
 
-    employeeCommissionFactory.getItemsList({}).then(function(dataFromAPI) {
-      $scope.itemsList = dataFromAPI.retailItems;
+    $scope.$watchGroup(['startDate', 'endDate'], function () {
+      var payload = {};
+
+      if (!angular.isUndefined($scope.startDate)) {
+        payload.startDate = dateUtility.formatDateForAPI($scope.startDate);
+      }
+
+      if (!angular.isUndefined($scope.endDate)) {
+        payload.endDate = dateUtility.formatDateForAPI($scope.endDate);
+      }
+
+      employeeCommissionFactory.getItemsList(payload).then(function (dataFromAPI) {
+        $scope.itemsList = dataFromAPI.retailItems;
+      });
     });
+
   });
--- test/spec/controllers/employee-commission.js
+++ test/spec/controllers/employee-commission.js
@@ -3,11 +3,26 @@
 describe('Controller: EmployeeCommissionCtrl', function () {
 
   beforeEach(module('ts5App'));
+  beforeEach(module('served/items-list.json'));
+
 
   var EmployeeCommissionCtrl,
+    employeeCommissionFactory,
+    getItemsListDeferred,
+    itemsListJSON,
     scope;
 
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($q, $controller, $rootScope, _employeeCommissionFactory_) {
+    inject(function (_servedItemsList_) {
+      itemsListJSON = _servedItemsList_;
+    });
+
+    getItemsListDeferred = $q.defer();
+    getItemsListDeferred.resolve(itemsListJSON);
+
+    employeeCommissionFactory = _employeeCommissionFactory_;
+    spyOn(employeeCommissionFactory, 'getItemsList').and.returnValue(getItemsListDeferred.promise);
+
     scope = $rootScope.$new();
     EmployeeCommissionCtrl = $controller('EmployeeCommissionCtrl', {
       $scope: scope
