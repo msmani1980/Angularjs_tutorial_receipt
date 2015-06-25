1c1b447c3054c81936b9b01147463dc1a6b48daa
Kelly Meath <kmeath@egate-solutions.com>
2015-06-25 12:00:16 -0500

    TSVPORTAL-2028 - Added random color for each company

--- app/scripts/controllers/item-import.js
+++ app/scripts/controllers/item-import.js
@@ -11,50 +11,130 @@ angular.module('ts5App')
   .controller('ItemImportCtrl', function ($scope, $q, ItemImportFactory ) {
 
     // controller global properties
-    var _companyId = null;
+    var _companyId    = null;
 
     // scope properties
     $scope.viewName = 'Import Stock Owner Items';
     $scope.selectedStockownerCompany = null;
     $scope.companiesLoaded = false;
-
+     var f = false;
+    $scope.hexColor = function(item, useCompanyId){
+      if(!f){
+        console.log(item);
+        f = true;
+      }
+      if(item.hasOwnProperty(item)){
+        return item.hexColor;
+      }
+      var companyId = _companyId;
+      if(undefined === useCompanyId){
+        companyId = $scope.selectedStockownerCompany.id;
+      }
+      item.hexColor = helpers.randomHexColor.get(companyId);
+      return item.hexColor;
+    };
 
     $scope.changeSelectedStockownerCompany = function(){
       $scope.stockOwnerItemsList = [];
+      if(undefined === $scope.selectedStockownerCompany){
+        return;
+      }
       ItemImportFactory.getItemsList({companyId:$scope.selectedStockownerCompany.id}).then(function(response){
-        $scope.stockOwnerItemsList = response.retailItems;
+        console.log($scope.selectedStockownerCompany.id);
+        f= false;
+        angular.forEach(response.retailItems, function(retailItem){
+          if(!helpers.retailItemSkus.has(retailItem)) {
+            this.push(retailItem);
+          }
+        }, $scope.stockOwnerItemsList);
       });
     };
 
     // scope methods
     $scope.importAll = function(value){
-      if(!angular.isDefined(value)){
-        value = document.getElementById('stockOwnerId').value;
+
+    };
+
+    // helpers
+    var helpers = {
+      retailItemSkus: {
+        existing: [],
+        set: function (retailItem) {
+          this.existing.push(retailItem.itemCode);
+        },
+        has: function (retailItem) {
+          return false;
+          return -1 !== this.existing.indexOf(retailItem.itemCode);
+        }
+      },
+      randomHexColor: {
+        predefined: ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#FF1493', '#FFA500', '#FFD700', '#008080', '#9400D3'],
+        storage: {
+          companies: [],
+          companyColor: [],
+        },
+        get: function (companyId) {
+          // if the company has a color saved, return it
+          if(-1 !== this.storage.companies.indexOf(companyId)) {
+            // get com
+            return this.getFromStorage(companyId);
+          }
+
+          var randColor;
+          // if there are no more predefined colors, generate one
+          if (!this.predefined.length) {
+            randColor = this.generate();
+          }
+          // else choose predefined color
+          else{
+            var randomIndex = Math.floor(Math.random() * this.predefined.length);
+            randColor = this.predefined[randomIndex];
+            this.predefined.splice(randomIndex, 1);
+          }
+
+          // push company and color to storage
+          this.storage.companies.push(companyId);
+          this.storage.companyColor.push(randColor);
+          return this.getFromStorage(companyId);
+        },
+        getFromStorage: function(companyId){
+          return this.storage.companyColor[this.storage.companies.indexOf(companyId)];
+        },
+        generate: function () {
+          var hexPieces = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+          var colorString = '#';
+          for (var i = 0; i < 6; i++) {
+            colorString += hexPieces[Math.floor(Math.random() * hexPieces.length)];
+          }
+          return colorString;
+        }
       }
-      ItemImportFactory.getItemsList({companyId:value}).then(function(response){
-        $scope.stockOwnerItemsList = response.retailItems;
-      });
     };
 
     // Constructor
     (function constructor(){
       _companyId = ItemImportFactory.getCompanyId();
-      ItemImportFactory.getCompaniesList({companyTypeId:2,limit:null}).then(function(response){
+      var promises = [ItemImportFactory.getCompaniesList({companyTypeId:2,limit:null}).then(function(response){
         // TODO - assign colors to each company
         var companies = response.companies;
 
         // TODO - Can only mock until https://jira.egate-solutions.com/browse/TSVPORTAL-2038 is done
-        var mockResponse = {'companies':[{'id':419,'companyName':'TEST_009','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'TEST_009','ediName':'TEST_009','dbaName':'TEST_009','countRelation':'1','baseCurrencyCode':'USD','companyCode':'TEST_009','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':413,'companyName':'GRO 555','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'Gro test Company ','ediName':'EDI ','dbaName':'Test Dba ','countRelation':'1','baseCurrencyCode':'USD','companyCode':'5555','isActive':'false','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':407,'companyName':'GRO 2','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'GRO 2','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'GBP','companyCode':'223423','isActive':'true','baseCurrencyId':8,'companyLanguages':'','exchangeRateVariance':null},{'id':404,'companyName':'StockOwner1','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'StockOwner1','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'USD','companyCode':'12345','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':396,'companyName':'stockCom12','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'stockCom12','ediName':'stockCom1','dbaName':'stockCom12','countRelation':'0','baseCurrencyCode':'USD','companyCode':'st21','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':392,'companyName':'stockCom','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'stockCom','ediName':'stockCom','dbaName':'stockCom','countRelation':'0','baseCurrencyCode':'USD','companyCode':'st2','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':378,'companyName':'Test Training ','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'Test Training ','ediName':null,'dbaName':'Test DBA','countRelation':'0','baseCurrencyCode':'USD','companyCode':'555','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':375,'companyName':'delarche test','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':369,'legalName':'dddd','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'TTS','companyCode':'dsf','isActive':'false','baseCurrencyId':13,'companyLanguages':'French (Standard)','exchangeRateVariance':null}],'meta':{'count':8,'limit':8,'start':0}};
+        var mockResponse = {'companies':[{'id':413,'companyName':'Company 413','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'TEST_009','ediName':'TEST_009','dbaName':'TEST_009','countRelation':'1','baseCurrencyCode':'USD','companyCode':'TEST_009','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':413,'companyName':'GRO 555','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'Gro test Company ','ediName':'EDI ','dbaName':'Test Dba ','countRelation':'1','baseCurrencyCode':'USD','companyCode':'5555','isActive':'false','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':407,'companyName':'GRO 2','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'GRO 2','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'GBP','companyCode':'223423','isActive':'true','baseCurrencyId':8,'companyLanguages':'','exchangeRateVariance':null},{'id':404,'companyName':'StockOwner1','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'StockOwner1','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'USD','companyCode':'12345','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':396,'companyName':'stockCom12','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'stockCom12','ediName':'stockCom1','dbaName':'stockCom12','countRelation':'0','baseCurrencyCode':'USD','companyCode':'st21','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':392,'companyName':'stockCom','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'stockCom','ediName':'stockCom','dbaName':'stockCom','countRelation':'0','baseCurrencyCode':'USD','companyCode':'st2','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':378,'companyName':'Test Training ','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':null,'legalName':'Test Training ','ediName':null,'dbaName':'Test DBA','countRelation':'0','baseCurrencyCode':'USD','companyCode':'555','isActive':'true','baseCurrencyId':1,'companyLanguages':'','exchangeRateVariance':null},{'id':375,'companyName':'delarche test','companyTypeName':'Stockowner','companyTypeId':2,'parentCompanyId':369,'legalName':'dddd','ediName':null,'dbaName':null,'countRelation':'0','baseCurrencyCode':'TTS','companyCode':'dsf','isActive':'false','baseCurrencyId':13,'companyLanguages':'French (Standard)','exchangeRateVariance':null}],'meta':{'count':8,'limit':8,'start':0}};
 
         companies = mockResponse.companies;
         // companies.unshift({companyName:"Select Stock Owner",id:""});
         $scope.stockOwnerList = companies;
+      }),
+      ItemImportFactory.getItemsList({companyId:_companyId}).then(function(response){
+        $scope.itemList = [];
+        angular.forEach(response.retailItems, function(retailItem){
+          helpers.retailItemSkus.set(retailItem);
+          this.push(retailItem);
+        }, $scope.itemList)
+      })];
+      $q.all(promises).then(function(){
         $scope.companiesLoaded = true;
       });
-      ItemImportFactory.getItemsList({companyId:_companyId}).then(function(response){
-        $scope.itemList = response.retailItems;
-      });
-
     })();
 
     // TODO: documentation here:
@@ -68,39 +148,4 @@ angular.module('ts5App')
     };
 
     // TODO: change BACK button to back/save when models change
-/*
-    // TODO: pull data from API
-    $scope.stockOwnerList = [
-      {name: 'GRO'},
-      {name: 'Easy Jet'},
-      {name: 'Delta'}
-    ];
-
-    // TODO: pull data from API
-    $scope.stockOwnerItemsList = [{
-      colorCode: '#00FF00',
-      companyName: 'GRO',
-      reference: 'CHP0524',
-      itemName: 'Bananas'
-    }, {
-      colorCode: '#FFF',
-      companyName: 'Another one',
-      reference: 'NTHR1',
-      itemName: 'Apple'
-    }];
-
-    // TODO: pull data from API
-    /*
-    $scope.itemList = [{
-      colorCode: '#00FF00',
-      companyName: 'GRO',
-      reference: 'PHN4',
-      itemName: 'iPhone 5s'
-    }, {
-      colorCode: '#CCC',
-      companyName: 'One more here too',
-      reference: 'NMHRT2',
-      itemName: 'iPad Mini Retina'
-    }];
-    */
   });
