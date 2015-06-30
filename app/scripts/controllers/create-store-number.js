a9053e28ec3f584ee7217badcdf2ed794b26baac
Kelly Meath <kmeath@egate-solutions.com>
2015-06-30 12:13:36 -0500

    TSVPORTAL-2029 adding controller

---
+++ app/scripts/controllers/create-store-number.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ts5App.controller:CreatestorenumberCtrl
+ * @description
+ * # CreatestorenumberCtrl
+ * Controller of the ts5App
+ */
+angular.module('ts5App')
+  .controller('createStoreNumberCtrl', function ($scope) {
+
+  });
