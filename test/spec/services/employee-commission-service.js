2c79b73145f0eaa5990aff6b20e633663840dd2a
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-07-13 12:09:46 -0500

    TSVPORTAL-1598 - adding GET, PUT, POST and DELETE to service

--- test/spec/services/employee-commission-service.js
+++ test/spec/services/employee-commission-service.js
@@ -8,17 +8,10 @@ describe('Service: employeeCommissionService', function () {
 
   // instantiate service
   var employeeCommissionService,
-    $httpBackend,
-    menuResponseJSON;
+    $httpBackend;
 
   beforeEach(inject(function (_employeeCommissionService_, $injector) {
-    inject(function (_servedMenus_) {
-      menuResponseJSON = _servedMenus_;
-    });
-
     $httpBackend = $injector.get('$httpBackend');
-    $httpBackend.whenGET(/employee-commissions\/\d+/).respond(menuResponseJSON);
-
     employeeCommissionService = _employeeCommissionService_;
   }));
 
@@ -33,6 +26,53 @@ describe('Service: employeeCommissionService', function () {
 
   describe('API requests', function () {
 
+    it('should GET commission list', function () {
+      $httpBackend.expectGET(/employee-commissions/).respond(200, {fakeResponseKey: 'fakeResponseValue'});
+
+      employeeCommissionService.getCommissionList().then(function (response) {
+        expect(response.fakeResponseKey).toBe('fakeResponseValue');
+      });
+      $httpBackend.flush();
+    });
+
+    it('should GET a single commission', function () {
+      var commissionId = 166;
+      $httpBackend.expectGET(/employee-commissions\/\d+/).respond(200, {fakeResponseKey: commissionId});
+
+      employeeCommissionService.getCommission(commissionId).then(function (response) {
+        expect(response.fakeResponseKey).toBe(commissionId);
+      });
+      $httpBackend.flush();
+    });
+
+    it('should POST a commission', function () {
+      $httpBackend.expectPOST(/employee-commissions/).respond(200, {fakeResponseKey: 'POST'});
+
+      employeeCommissionService.createCommission().then(function (response) {
+        expect(response.fakeResponseKey).toBe('POST');
+      });
+      $httpBackend.flush();
+    });
+
+    it('should PUT a commission', function () {
+      var commissionId = 166;
+      $httpBackend.expectPUT(/employee-commissions/).respond(200, {fakeResponseKey: commissionId});
+
+      employeeCommissionService.updateCommission().then(function (response) {
+        expect(response.fakeResponseKey).toBe(commissionId);
+      });
+      $httpBackend.flush();
+    });
+
+    it('should deleteCommission a commission', function () {
+      var commissionId = 166;
+      $httpBackend.expectDELETE(/employee-commissions/).respond(200, {fakeResponseKey: commissionId});
+
+      employeeCommissionService.deleteCommission(commissionId).then(function (response) {
+        expect(response.fakeResponseKey).toBe(commissionId);
+      });
+      $httpBackend.flush();
+    });
 
   });
 
