8df37f00d0abb12e991f7c023dd4508b1f311c4e
Max Felker <max@bigroomstudios.com>
2015-07-15 11:58:29 -0400

    TSVPORTAL-2363 TSVPORTAL-2360 | Added tests for the date picker field directive and began tests on date utility

--- test/spec/directives/date-picker-field.js
+++ test/spec/directives/date-picker-field.js
@@ -5,11 +5,101 @@ describe('Directive: datePickerField', function () {
   // load the directive's module
   beforeEach(module('ts5App'));
 
+  beforeEach(module('template-module'));
+
   var element,
     scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
+
+  describe('date picker template', function () {
+    var formGroup;
+    beforeEach(inject(function ($compile) {
+      element = angular.element(
+        '<date-picker-field label="labelFrom" required></date-picker-field>'
+      );
+      element = $compile(element)(scope);
+      scope.$digest();
+      formGroup = angular.element(element.find('.form-group')[0]);
+    }));
+
+    it('should be wrapped in a form-group', function () {
+      expect(formGroup).toBeDefined();
+    });
+
+    describe('label element', function () {
+      var label;
+      beforeEach(function () {
+        label = angular.element(formGroup.find('label')[0]);
+      });
+
+      it('should be present in the DOM', function () {
+        expect(label).toBeDefined();
+      });
+
+      describe('required span label', function () {
+        var span;
+        beforeEach(function () {
+          span = angular.element(label.find('span')[0]);
+        });
+
+        it('should be present in the DOM', function () {
+          expect(span).toBeDefined();
+        });
+
+        it('should have a .required class', function () {
+          expect(span.hasClass('required')).toBeTruthy();
+        });
+
+        it(
+          'should have a ng-show attribute that shows the span if the field is required',
+          function () {
+            expect(span.attr('ng-show')).toEqual('required');
+          });
+
+        it('should have a * display in the span', function () {
+          expect(span.text()).toEqual('*');
+        });
+
+      });
+
+    });
+
+    describe('input group', function () {
+      var inputGroup;
+      beforeEach(function () {
+        inputGroup = angular.element(formGroup.find(
+          '.input-group')[0]);
+      });
+
+      it('should be present in the DOM', function () {
+        expect(inputGroup).toBeDefined();
+      });
+
+      describe('input group button element', function () {
+        var inputGroupBtn,
+          button;
+        beforeEach(function () {
+          inputGroupBtn = angular.element(inputGroup.find(
+            '.input-group-btn')[0]);
+          button = angular.element(inputGroupBtn.find(
+            'button')[0]);
+        });
+
+        it('should be present in the DOM', function () {
+          expect(inputGroupBtn).toBeDefined();
+        });
+
+      });
+
+    });
+
+    it('should have 1 input field', function () {
+      expect(element.find('input').length).toBe(1);
+    });
+
+  });
 
 });
--- test/spec/services/date-utility.js
+++ test/spec/services/date-utility.js
@@ -31,11 +31,13 @@ describe('Date Utility service', function () {
       expect(dateUtility.formatDate).toBeDefined();
     });
 
-    it('should format a date string from one format to another', function () {
-      var expectedDateString = '20150619';
-      var formattedString = dateUtility.formatDate('06/19/2015', 'L', 'YYYYMMDD');
-      expect(formattedString).toEqual(expectedDateString);
-    });
+    it('should format a date string from one format to another',
+      function () {
+        var expectedDateString = '20150619';
+        var formattedString = dateUtility.formatDate('06/19/2015',
+          'L', 'YYYYMMDD');
+        expect(formattedString).toEqual(expectedDateString);
+      });
 
   });
 
@@ -45,15 +47,17 @@ describe('Date Utility service', function () {
       expect(dateUtility.formatDateForAPI).toBeDefined();
     });
 
-    it('should format a date string from one format to another', function () {
-      var expectedDateString = '20150619';
-      var formattedString = dateUtility.formatDateForAPI('06/19/2015');
-      expect(formattedString).toEqual(expectedDateString);
-    });
+    it('should format a date string from one format to another',
+      function () {
+        var expectedDateString = '20150619';
+        var formattedString = dateUtility.formatDateForAPI(
+          '06/19/2015');
+        expect(formattedString).toEqual(expectedDateString);
+      });
 
   });
 
-  describe('isValid() method', function(){
+  describe('isValid() method', function () {
 
     it('should be defined for APP', function () {
       expect(dateUtility.isDateValidForApp).toBeDefined();
@@ -76,19 +80,35 @@ describe('Date Utility service', function () {
     });
 
     it('should return false for API', function () {
-      expect(dateUtility.isDateValidForAPI('05/10/1979')).toBe(false);
+      expect(dateUtility.isDateValidForAPI('05/10/1979')).toBe(
+        false);
     });
 
   });
 
-  describe('date comparison', function(){
-    it('should return true is date is after today', function(){
+  describe('date comparison', function () {
+    it('should return true is date is after today', function () {
       expect(dateUtility.isAfterToday('05/10/2080')).toBe(true);
     });
 
-    it('should return false is date is not after today', function(){
+    it('should return false is date is not after today', function () {
       expect(dateUtility.isAfterToday('05/10/2000')).toBe(false);
     });
+  });
+
+  describe('nowFormatted method', function () {
+    it('should be defined', function () {
+      expect(dateUtility.nowFormatted).toBeDefined();
+    });
+
+    /*it(
+      'should return a formatted date in MM/DD/YYYY when no format is passed',
+      function () {
+        var testTimestamp = Date.parse(new Date());
+        var formattedTimeStamp = dateUtility.nowFormatted();
+        console.log(testTimestamp, formattedTimeStamp);
+        //expect(dateUtility.nowFormatted()).toEqual('');
+      });*/
   });
 
 });
