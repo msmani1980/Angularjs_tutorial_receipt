61abf5287c63e26fd82a12e08a1d0a6a5904c13b
Kelly Meath <kmeath@egate-solutions.com>
2015-07-28 16:20:44 -0500

    TSVPORTAL-2474 - Added tests for main menu service

--- test/spec/services/main-menu-service.js
+++ test/spec/services/main-menu-service.js
@@ -14,5 +14,22 @@ describe('Service: mainMenuService', function () {
   it('should do something', function () {
     expect(!!mainMenuService).toBe(true);
   });
-
+  it('should have a getMenu function defined', function(){
+    expect(mainMenuService.getMenu).toBeDefined();
+    expect(Object.prototype.toString.call(mainMenuService.getMenu)).toBe('[object Function]');
+  });
+  it('should have a getStockOwnerMenu function defined', function(){
+    expect(mainMenuService.getStockOwnerMenu).toBeDefined();
+    expect(Object.prototype.toString.call(mainMenuService.getStockOwnerMenu)).toBe('[object Function]');
+  });
+  it('should have a getRetailMenu function defined', function(){
+    expect(mainMenuService.getRetailMenu).toBeDefined();
+    expect(Object.prototype.toString.call(mainMenuService.getRetailMenu)).toBe('[object Function]');
+  });
+  it('should return the same array as getRetailMenu when no companyTypeId is passed to getMenu', function(){
+    expect(mainMenuService.getMenu().length).toBe(mainMenuService.getRetailMenu().length);
+  });
+  it('should return the same array as getRetailMenu when 2 is passed to getMenu', function(){
+    expect(mainMenuService.getMenu(2).length).toBe(mainMenuService.getStockOwnerMenu().length);
+  });
 });
