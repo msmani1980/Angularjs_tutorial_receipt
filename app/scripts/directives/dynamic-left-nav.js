ceef0ec79d86056424801b7c97d3a031713f3a30
Kelly Meath <kmeath@egate-solutions.com>
2015-07-28 15:42:48 -0500

    TSVPORTAL-2646 - FInished tests and feature

--- app/scripts/directives/dynamic-left-nav.js
+++ app/scripts/directives/dynamic-left-nav.js
@@ -9,18 +9,97 @@
 angular.module('ts5App')
   .directive('dynamicLeftNav', function () {
 
-    var dynamicLeftNavController = function ($scope, $location, mainMenuService) {
-      $scope.locationPath = $location.path();
+    var dynamicLeftNavController = function ($scope, $location, $injector, $filter) {
+      var $this = this;
+      var mainMenuService = $injector.get('mainMenuService');
       var menu = mainMenuService.getMenu();
-      console.log($scope.locationPath);
-      console.log($filter('filter')(menu, {route: $scope.locationPath})[0]);
+      var menuItems = [];
+      if($scope.title) {
+        menuItems = $filter('filter')(menu, {title: $scope.title}, true);
+      }
+      //
+      else{
+        menuItems = $filter('filter')(menu, {menuItems: {route: $location.path()}});
+      }
+      if(menuItems.length){
+        $scope.menuItems = menuItems[0].menuItems;
+      }
+
+      $scope.leaveViewNav = function (path) {
+        $scope.leavePathNav = path;
+        var currentPath = $location.path();
+        var onEditView = $this.checkIfEditing();
+
+        $this.setModalElement();
+        $this.hideModal();
+
+        if (onEditView && currentPath !== $scope.leavePathNav) {
+          $this.showModal();
+        } else {
+          $this.hideModal();
+          $this.navigateTo($scope.leavePathNav);
+        }
+
+      };
+
+      $scope.itemClass = function(path) {
+        var itemClass = '';
+        if('/#'+$location.path() === path){
+          itemClass += ' active';
+        }
+        return itemClass;
+      };
+
+      this.setModalElement = function () {
+        this.modalElement = angular.element('#leave-view-modal-nav');
+      };
+
+      this.showModal = function () {
+        this.modalElement.modal('show');
+      };
+
+      this.hideModal = function () {
+        this.modalElement.modal('hide');
+      };
+
+      this.navigateTo = function (path) {
+        if (path.substring(0, 2) === '/#') {
+          path = path.substring(2);
+        }
+        $location.path(path);
+      };
+
+      this.checkIfEditing = function () {
+        if($scope.isEditing){
+          return true;
+        }
+        var path = $location.path();
+        if (path.search('create') !== -1) {
+          return true;
+        }
+        if (path.search('edit') !== -1) {
+          return true;
+        }
+        return false;
+      };
+
+      $scope.leaveViewClose = function () {
+        $this.setModalElement();
+        $this.hideModal();
+        $this.modalElement.on('hidden.bs.modal', function () {
+          $this.navigateTo($scope.leavePathNav);
+          $scope.$apply();
+        });
+
+      };
     };
 
     return {
       templateUrl: '/views/directives/dynamic-left-nav.html',
       restrict: 'E',
       scope: {
-        basePath: '@'
+        title: '@',
+        isEditing: '@'
       },
       controller: dynamicLeftNavController
     };
--- test/spec/directives/dynamic-left-nav.js
+++ test/spec/directives/dynamic-left-nav.js
@@ -3,18 +3,144 @@
 describe('Directive: dynamicLeftNav', function () {
 
   // load the directive's module
-  beforeEach(module('ts5App'));
+  beforeEach(module('ts5App', 'template-module'));
 
   var element,
-    scope;
+    scope,
+    controller,
+    location;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function (_$rootScope_, _$location_) {
+    scope = _$rootScope_;
+    location = _$location_;
   }));
-
-  it('should make hidden element visible', inject(function ($compile) {
+  beforeEach(inject(function ($compile) {
     element = angular.element('<dynamic-left-nav></dynamic-left-nav>');
     element = $compile(element)(scope);
-    //  expect(element.text()).toBe('this is the dynamicLeftNav directive');
+    scope.$digest();
+    controller = element.controller('dynamicLeftNav');
   }));
+
+  describe('directive isolated scope', function () {
+    var isolatedScope;
+    beforeEach(inject(function () {
+      isolatedScope = element.isolateScope();
+    }));
+    it('should have a leaveViewNav method attached to the scope', function () {
+      expect(isolatedScope.leaveViewNav).toBeDefined();
+    });
+    it('should have a leaveViewClose function defined', function () {
+      expect(isolatedScope.leaveViewClose).toBeDefined();
+    });
+    it('should be able to call leaveViewNav()', function () {
+      spyOn(isolatedScope, 'leaveViewNav').and.callThrough();
+      isolatedScope.leaveViewNav('some-path');
+      expect(isolatedScope.leaveViewNav).toHaveBeenCalled();
+    });
+    it('should contain a leavePathNav variable', function () {
+      isolatedScope.leaveViewNav('test-path');
+      expect(isolatedScope.leavePathNav).toBeDefined();
+    });
+    it('should return the leavePathNav variable, after being passed into leaveViewNav', function () {
+      isolatedScope.leaveViewNav('test-path');
+      expect(isolatedScope.leavePathNav).toContain('test-path');
+    });
+    it('should contain the function leaveViewClose()', function () {
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leaveViewClose).toBeDefined();
+    });
+    it('should be able to call leaveViewClose()', function () {
+      spyOn(isolatedScope, 'leaveViewClose').and.callThrough();
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leaveViewClose).toHaveBeenCalled();
+    });
+    it('should contain a leavePathNav variable after leaveViewNav is called and closed', function () {
+      isolatedScope.leaveViewNav('test-path');
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leavePathNav).toBeDefined();
+    });
+    it('should return the leavePath variable, after being passed into leaveView and closed', function () {
+      isolatedScope.leaveViewNav('test-path');
+      isolatedScope.leaveViewClose();
+      expect(isolatedScope.leavePathNav).toContain('test-path');
+    });
+  });
+
+  describe('when the controller is accessed, it', function () {
+    it('should have a setModalElement function defined', function () {
+      expect(controller.setModalElement).toBeDefined();
+    });
+    it('should have a setModalElement function set modelElement', function () {
+      controller.setModalElement();
+      expect(controller.modalElement).toBeDefined();
+    });
+    it('should have a showModal function defined', function () {
+      expect(controller.showModal).toBeDefined();
+    });
+    it('should have a hideModal function defined', function () {
+      expect(controller.hideModal).toBeDefined();
+    });
+    it('should have a navigateTo function defined', function () {
+      expect(controller.navigateTo).toBeDefined();
+    });
+    it('should have a checkIfEditing function defined', function () {
+      expect(controller.checkIfEditing).toBeDefined();
+    });
+  });
+
+  describe('when the title attribute is set', function () {
+    var isolatedScope;
+    beforeEach(inject(function ($compile) {
+      element = angular.element('<dynamic-left-nav title="Cash Management"></dynamic-left-nav>');
+      element = $compile(element)(scope);
+      scope.$digest();
+      isolatedScope = element.isolateScope();
+    }));
+    it('should have have a title attribute', function () {
+      expect(element.attr('title')).toBeDefined();
+    });
+    it('should match the title variable passed to the directive', function () {
+      expect(element.attr('title')).toEqual(isolatedScope.title);
+    });
+    it('should have a menuItems variable in scope', function () {
+      expect(isolatedScope.menuItems).toBeDefined();
+    });
+    it('should have 3 items in the menuItems array scope variable', function () {
+      expect(isolatedScope.menuItems.length).toEqual(3);
+    });
+  });
+
+  describe('when no title attribute is set', function () {
+    var isolatedScope;
+    beforeEach(inject(function ($compile) {
+      element = angular.element('<dynamic-left-nav></dynamic-left-nav>');
+      element = $compile(element)(scope);
+      spyOn(location, 'path').and.returnValue('/cash-bag-list');
+      scope.$digest();
+      isolatedScope = element.isolateScope();
+    }));
+    it('should have a menuItems variable in scope', function () {
+      expect(isolatedScope.menuItems).toBeDefined();
+    });
+    it('should have 3 items in the menuItems array scope variable', function () {
+      expect(isolatedScope.menuItems.length).toEqual(3);
+    });
+  });
+
+  describe('when the is-editing attribute is set', function () {
+    var isolatedScope;
+    beforeEach(inject(function ($compile) {
+      element = angular.element('<dynamic-left-nav is-editing="1"></dynamic-left-nav>');
+      element = $compile(element)(scope);
+      scope.$digest();
+      isolatedScope = element.isolateScope();
+    }));
+    it('should have have a isEditing attribute', function () {
+      expect(element.attr('is-editing')).toBeDefined();
+    });
+    it('should match the title variable passed to the directive', function () {
+      expect(element.attr('is-editing')).toEqual(isolatedScope.isEditing);
+    });
+  });
+
 });
