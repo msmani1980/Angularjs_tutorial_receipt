4229b400281163f2569760fa0c29e8415013e6c3
Max Felker <max@bigroomstudios.com>
2015-07-30 13:30:36 -0400

    TSVPORTAL-2114 | Updated labels and found tests for both input price types directive are no done. Committing then updating these tests

--- test/spec/directives/input-price-type-stock-owner.js
+++ test/spec/directives/input-price-type-stock-owner.js
@@ -1,15 +1,34 @@
 'use strict';
 
-describe('Directive: inputPriceTypeStockOwner', function () {
+describe('The Stock Owner Item Price Type directive', function () {
 
-  // load the directive's module
   beforeEach(module('ts5App'));
+  beforeEach(module('template-module'));
 
-  var scope;
+  var element,
+    $scope,
+    panel;
 
   beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+    $scope = $rootScope.$new();
   }));
 
+  function renderDirective($compile) {
+    element = angular.element('<item-price-type-stock-owner></item-price-type-stock-owner>');
+    element = $compile(element)($scope);
+    $scope.$digest();
+    panel = angular.element(element.find('.panel')[0]);
+  }
+
+  describe('when the directive is rendered', function () {
+    beforeEach(inject(function ($compile) {
+      renderDirective($compile);
+    }));
+
+    it('should render a .panel component', function () {
+      expect(panel).toBeDefined();
+    });
+
+  });
 
 });
--- test/spec/directives/item-price-type.js
+++ test/spec/directives/item-price-type.js
@@ -1,25 +1,34 @@
 'use strict';
 
-describe('Directive: itemPriceType', function () {
+describe('The Item Price Type directive', function () {
 
-  // load the directive's module
   beforeEach(module('ts5App'));
+  beforeEach(module('template-module'));
 
   var element,
-    scope;
+    $scope,
+    panel;
 
   beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+    $scope = $rootScope.$new();
   }));
 
-  it('should be inject into the DOM', inject(function ($compile) {
-
+  function renderDirective($compile) {
     element = angular.element('<item-price-type></item-price-type>');
+    element = $compile(element)($scope);
+    $scope.$digest();
+    panel = angular.element(element.find('.panel')[0]);
+  }
 
-    element = $compile(element)(scope);
+  describe('when the directive is rendered', function () {
+    beforeEach(inject(function ($compile) {
+      renderDirective($compile);
+    }));
 
-    expect(element).toBeDefined(); 
+    it('should render a .panel component', function () {
+      expect(panel).toBeDefined();
+    });
 
-  }));
+  });
 
 });
