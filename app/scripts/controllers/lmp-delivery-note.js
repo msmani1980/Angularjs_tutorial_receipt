8e0f57b0dcac8e2627fc6f30b6bc4d918999e2ff
Kelly Meath <kmeath@egate-solutions.com>
2015-08-03 16:46:59 -0500

    TSVPORTAL-2275 - Adding stubbed out test and controller.

---
+++ app/scripts/controllers/lmp-delivery-note.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ts5App.controller:LmpDeliveryNoteCtrl
+ * @description
+ * # LmpDeliveryNoteCtrl
+ * Controller of the ts5App
+ */
+angular.module('ts5App')
+  .controller('LmpDeliveryNoteCtrl', function ($scope) {
+
+    // Scope vars
+    $scope.state = null;
+    $scope.id = null;
+  });
