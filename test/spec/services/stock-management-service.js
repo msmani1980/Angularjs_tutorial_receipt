6ed10420ed398673e15544026e877f78335b7f55
Kelly Meath <kmeath@egate-solutions.com>
2015-08-04 11:04:19 -0500

    TSVPORTAL-2275 - Checking in newly added files, gotta cherry pick the rest from another branch

---
+++ test/spec/services/stock-management-service.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('Service: stockManagementService', function () {
+
+  // load the service's module
+  beforeEach(module('ts5App'));
+
+  // instantiate service
+  var stockManagementService,
+    httpBackend;
+
+  beforeEach(inject(function (_stockManagementService_, $httpBackend) {
+    stockManagementService = _stockManagementService_;
+    httpBackend = $httpBackend;
+  }));
+
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should exist', function () {
+    expect(!!stockManagementService).toBe(true);
+  });
+
+
+  describe('API calls', function () {
+    describe('getDeliveryNote', function () {
+      it('should be accessible in the service', function () {
+        expect(stockManagementService.getDeliveryNote).toBeDefined();
+      });
+      beforeEach(function () {
+        httpBackend.whenGET(/stock-management\/delivery-notes/).respond({done: true});
+      });
+      it('should make GET request to API', function () {
+        stockManagementService.getDeliveryNote(38);
+        httpBackend.expectGET(/stock-management\/delivery-notes/);
+        httpBackend.flush();
+      });
+    });
+  });
+
+});
