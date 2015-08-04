edbb514776ef53023a5f5ca9657066f3b9cbe8a8
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-08-04 10:48:09 -0500

    TSVPORTAL-2631 - fix for name

--- app/scripts/controllers/item-create.js
+++ app/scripts/controllers/item-create.js
@@ -831,6 +831,9 @@ angular.module('ts5App')
     };
 
     $scope.submitForm = function(formData) {
+      //if (!$scope.form.$valid) {
+      //  return false;
+      //}
       $scope.form.$setSubmitted(true);
       if (formData && $this.validateForm()) {
         var itemData = angular.copy(formData);
