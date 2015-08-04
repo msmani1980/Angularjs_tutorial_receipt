fec39095e51e91c1c0506b64a132cf3b0ff1e7a0
Kelly Meath <kmeath@egate-solutions.com>
2015-08-04 14:48:51 -0500

    TSVPORTAL-2275 - Started view state with get delivery note api call

--- app/scripts/controllers/lmp-delivery-note.js
+++ app/scripts/controllers/lmp-delivery-note.js
@@ -8,14 +8,55 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('LmpDeliveryNoteCtrl', function ($scope, stockManagementService) {
+  .controller('LmpDeliveryNoteCtrl', function ($scope, stockManagementService, $routeParams, $location) {
 
-    // Scope vars
-    $scope.state = null;
-    $scope.id = null;
+    // static scope vars
+    $scope.viewName = 'Delivery note';
 
+    function getDeliveryNote(){
+      displayLoadingModal();
+      stockManagementService.getDeliveryNote($routeParams.id).then(setDeliveryNote, showResponseErrors);
+    }
+
+    function setDeliveryNote(response){
+      $scope.deliveryNote = response;
+      hideLoadingModal();
+    }
+
+    function displayLoadingModal(loadingText) {
+      angular.element('#loading').modal('show').find('p').text(loadingText);
+    }
+
+    function hideLoadingModal() {
+      angular.element('#loading').modal('hide');
+    }
+
+    function showResponseErrors(response){
+      if ('data' in response) {
+        angular.forEach(response.data,function(error){
+          this.push(error);
+        }, $scope.formErrors);
+      }
+      $scope.displayError = true;
+      hideLoadingModal();
+    }
+
+    // constructor
     function init(){
-      stockManagementService.getDeliveryNote().then(function(){});
+      // scope vars
+      $scope.state = $routeParams.state;
+      $scope.viewName += ' ' + $scope.state;
+      $scope.displayError = false;
+      $scope.formErrors = [];
+
+      switch($scope.state){
+        case 'view':
+          getDeliveryNote();
+          break;
+        default:
+          $location.path('/');
+          break;
+      }
     }
     init();
   });
--- test/spec/controllers/lmp-delivery-note.js
+++ test/spec/controllers/lmp-delivery-note.js
@@ -3,43 +3,65 @@
 describe('Controller: LmpDeliveryNoteCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('ts5App'));
-  beforeEach(module('served/lmp-delivery-note.json'));
+  beforeEach(module('ts5App', 'served/lmp-delivery-note.json'));
 
   var LmpDeliveryNoteCtrl;
   var scope;
   var stockManagementService;
   var lmpDeliveryNoteResponseJSON;
   var getDeliveryNoteDeffered;
+  var routeParams;
+  var location;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $q, _stockManagementService_, _servedLmpDeliveryNote_) {
+  beforeEach(inject(function ($controller, $rootScope, $q, _stockManagementService_, _servedLmpDeliveryNote_, $location) {
 
     scope = $rootScope.$new();
+    location = $location;
     stockManagementService = _stockManagementService_;
     lmpDeliveryNoteResponseJSON = _servedLmpDeliveryNote_;
 
     getDeliveryNoteDeffered = $q.defer();
     getDeliveryNoteDeffered.resolve(lmpDeliveryNoteResponseJSON);
     spyOn(stockManagementService, 'getDeliveryNote').and.returnValue(getDeliveryNoteDeffered.promise);
-
-    LmpDeliveryNoteCtrl = $controller('LmpDeliveryNoteCtrl', {
-      $scope: scope
-    });
   }));
 
-  describe('scope vars', function(){
-    it('should have a state scope var defined', function(){
-      expect(scope.state).toBeDefined();
-    });
-    it('should have an id scope var defined', function(){
-      expect(scope.id).toBeDefined();
+  describe('invalid state passed to route', function(){
+    beforeEach(inject(function($controller){
+      routeParams = {
+        state: 'invalid'
+      };
+      LmpDeliveryNoteCtrl = $controller('LmpDeliveryNoteCtrl', {
+        $scope: scope,
+        $routeParams: routeParams
+      });
+      scope.$digest();
+    }));
+    it('should redirect to /', function(){
+      expect(location.path()).toBe('/');
     });
   });
 
-  describe('controller init', function(){
-    it('should call stock management get delivery note api', function(){
-      expect(stockManagementService.getDeliveryNote).toHaveBeenCalled();
+  describe('Read controller action, aka view', function(){
+    beforeEach(inject(function($controller){
+      routeParams = {
+        state: 'view',
+        id: 38
+      };
+      LmpDeliveryNoteCtrl = $controller('LmpDeliveryNoteCtrl', {
+        $scope: scope,
+        $routeParams: routeParams
+      });
+      scope.$digest();
+    }));
+    it('should have a state scope var set to view', function(){
+      expect(scope.state).toBe('view');
+    });
+    it('should call stock management get delivery note api with id', function(){
+      expect(stockManagementService.getDeliveryNote).toHaveBeenCalledWith(routeParams.id);
+    });
+    it('should set deliveryNote scope var', function(){
+      expect(scope.deliveryNote).toBeDefined();
     });
   });
 });
