edbe8c1f3ecded857e861126b31ba57dd39de2e3
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-08-10 13:32:01 -0500

    TSVPORTAL-2666 - added angular-animate back, don't know where it's been required

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -71,6 +71,7 @@ module.exports = function (config) {
       'bower_components/ng-file-upload/ng-file-upload.js',
       'bower_components/qrcode/lib/qrcode.js',
       'bower_components/angular-qr/src/angular-qr.js',
+      'bower_components/angular-animate/angular-animate.js',
       'bower_components/ngtoast/dist/ngToast.js',
       'bower_components/angular-ui-select/dist/select.js',
       'bower_components/select2/select2.js',
