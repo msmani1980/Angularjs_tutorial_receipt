ba45521488548f74ad7608b72723f089283d246e
Kelly Meath <kmeath@egate-solutions.com>
2015-08-13 15:40:55 -0500

    TSVPORTAL-2642 - Service with tests

---
+++ test/spec/services/stock-adjustments-service.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('Service: stockAdjustmentsService', function () {
+
+  // load the service's module
+  beforeEach(module('ts5App'));
+
+  // instantiate service
+  var stockAdjustmentsService;
+  var $httpBackend;
+  beforeEach(inject(function ($injector) {
+    stockAdjustmentsService = $injector.get('stockAdjustmentsService');
+    $httpBackend = $injector.get('$httpBackend');
+  }));
+
+  it('should do something', function () {
+    expect(!!stockAdjustmentsService).toBe(true);
+  });
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('API calls', function () {
+    describe('adjustStock', function () {
+      it('should be accessible in the service', function () {
+        expect(stockAdjustmentsService.adjustStock).toBeDefined();
+      });
+
+      beforeEach(function () {
+        $httpBackend.whenPOST(/stock-management\/stock-adjustments/).respond({done: true});
+      });
+      it('should POST data to item import API', function () {
+        stockAdjustmentsService.adjustStock({});
+        $httpBackend.expectPOST(/stock-management\/stock-adjustments/);
+        $httpBackend.flush();
+      });
+    });
+  });
+
+});
