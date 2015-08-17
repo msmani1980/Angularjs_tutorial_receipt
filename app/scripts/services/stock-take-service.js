96142c84879cfa39241d5f90a0ab775355dcf841
Max Felker <mfelker@egate-solutions.com>
2015-08-17 17:20:16 -0500

    TSVPORTAL-2695 | Added stock take service and tests

---
+++ app/scripts/services/stock-take-service.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ts5App.stockTakeService
+ * @description
+ * # stockTakeService
+ * Service in the ts5App.
+ */
+angular.module('ts5App')
+  .service('stockTakeService', function ($resource,ENV) {
+
+    var requestURL = ENV.apiUrl + '/api/stock-management/stock-takes/:id';
+    var requestParameters = {
+      id: '@id'
+    };
+
+    var actions = {
+      getStockTakeList: {
+        method: 'GET'
+      },
+      getStockTake: {
+        method: 'GET'
+      },
+      createStockTake: {
+        method: 'POST'
+      },
+      updateStockTake: {
+        method: 'PUT'
+      },
+      removeStockTake: {
+        method: 'DELETE'
+      }
+    };
+
+    var requestResource = $resource(requestURL, requestParameters, actions);
+
+    var getStockTakeList = function () {
+      return requestResource.getStockTakeList().$promise;
+    };
+
+    var getStockTake = function (id) {
+      return requestResource.getStockTake({id: id}).$promise;
+    };
+
+    var createStockTake = function (payload) {
+      return requestResource.createStockTake(payload).$promise;
+    };
+
+    var updateStockTake = function (id, payload) {
+      return requestResource.updateStockTake({id: id}, payload).$promise;
+    };
+
+    var removeStockTake = function (id) {
+      return requestResource.removeStockTake({id: id}).$promise;
+    };
+
+    return {
+      getStockTakeList: getStockTakeList,
+      getStockTake: getStockTake,
+      createStockTake: createStockTake,
+      updateStockTake: updateStockTake,
+      removeStockTake: removeStockTake
+    };
+
+
+  });
