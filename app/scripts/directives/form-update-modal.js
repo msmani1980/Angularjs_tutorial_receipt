d3b1ffd95179ac572581ff437de012766e632c6e
Max Felker <mfelker@egate-solutions.com>
2015-08-21 11:18:57 -0500

    TSVPORTAL-2116 | Upated form update modal to have a return to list button and added test coverage

--- app/scripts/directives/form-update-modal.js
+++ app/scripts/directives/form-update-modal.js
@@ -8,9 +8,30 @@
  */
 angular.module('ts5App')
   .directive('formUpdateModal', function () {
+
+    var controller = function ($scope, $location, $route) {
+      $scope.navigateTo = function (path) {
+        var currentPath = $location.path();
+        var modalElement = angular.element('#update-success');
+        modalElement.modal('hide');
+        modalElement.on('hidden.bs.modal', function () {
+          if (currentPath === path) {
+            $route.reload();
+          } else {
+            $location.path(path);
+            $scope.$apply();
+          }
+        });
+      };
+    };
+
     return {
       templateUrl: '/views/directives/form-update-modal.html',
       restrict: 'E',
-      scope: false
+      scope: {
+        listPath: '@'
+      },
+      controller: controller
     };
+
   });
