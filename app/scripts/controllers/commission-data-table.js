4a5caa3f8a334cec2f0176dbe27e9b810d0e2571
Alyssa Reyes <areyes@egate-solutions.com>
2015-08-21 12:09:51 -0500

    TSVPORTAL-2775 | mock up data table values and search

--- app/scripts/controllers/commission-data-table.js
+++ app/scripts/controllers/commission-data-table.js
@@ -12,6 +12,56 @@
  */
 angular.module('ts5App')
   .controller('CommissionDataTableCtrl', function ($scope) {
+    $scope.viewName = 'Commission Data Table';
+
+    $scope.commissionData = [{
+      crewBase: 'CREW',
+      startDate: '08/20/2015',
+      endDate: '09/20/2015',
+      product: false,
+      eposSales: true,
+      eposPercent: 1.00,
+      cashBanked: false,
+      cashBankedPercent: 0.00,
+      manualBars: 10.00,
+      cash: 20.00,
+      stock: 5.00,
+      incentiveIncrement: 12.00
+    }, {
+      crewBase: 'CREW',
+      startDate: '08/20/2015',
+      endDate: '09/20/2015',
+      product: true,
+      eposSales: false,
+      eposPercent: 0.00,
+      cashBanked: false,
+      cashBankedPercent: 0.00,
+      manualBars: 100.00,
+      cash: 20.00,
+      stock: 5.00,
+      incentiveIncrement: 12.00
+    }, {
+      crewBase: 'CREW',
+      startDate: '08/20/2015',
+      endDate: '09/20/2015',
+      product: false,
+      eposSales: false,
+      eposPercent: 0.00,
+      cashBanked: true,
+      cashBankedPercent: 2.00,
+      manualBars: 15.00,
+      cash: 20.00,
+      stock: 5.00,
+      incentiveIncrement: 12.00
+    }];
+
+    $scope.searchCommissionData = function () {
+
+    };
+
+    $scope.canDelete = function (data) {
+      return true;
+    };
 
 
   });
