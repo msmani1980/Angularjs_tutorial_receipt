46980252bad2aae4a866c061bc2056836f27f891
Max Felker <mfelker@egate-solutions.com>
2015-08-31 10:48:49 -0400

    TSVPORTAL-2989 | Added services stubs for stations and menus, added test coverage for this

--- app/scripts/controllers/store-instance-step-1.js
+++ app/scripts/controllers/store-instance-step-1.js
@@ -8,10 +8,34 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('StoreInstanceStep1Ctrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('StoreInstanceStep1Ctrl', function ($scope,catererStationService,menuService) {
+
+    $scope.cateringStationList = [];
+    $scope.menuList = [];
+
+    this.init = function() {
+      this.getCatererStationList();
+      this.getMenuList();
+    };
+
+    this.getCatererStationList = function() {
+      // TODO: Use store instance factory
+      catererStationService.getCatererStationList().then(this.setCatererStationList);
+    };
+
+    this.setCatererStationList = function(dataFromAPI) {
+      $scope.cateringStationList = dataFromAPI.response;
+    };
+
+    this.getMenuList = function() {
+      // TODO: Use store instance factory
+      menuService.getMenuList().then(this.setMenuList);
+    };
+
+    this.setMenuList = function(dataFromAPI) {
+      $scope.menuList = dataFromAPI.menus;
+    };
+
+    this.init();
+
   });
