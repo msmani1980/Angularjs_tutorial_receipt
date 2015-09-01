958dfca670d221546f6be9429f331f8ca816a1de
Alyssa Reyes <areyes@egate-solutions.com>
2015-09-01 10:57:58 -0500

    TSVPORTAL-2775 | fix readonly states, some code cleanup

--- app/scripts/controllers/commission-data-create.js
+++ app/scripts/controllers/commission-data-create.js
@@ -10,9 +10,7 @@
 angular.module('ts5App')
   .controller('CommissionDataCtrl', function ($scope, $routeParams) {
     var $this = this;
-    //var state = $routeParams.state;
-    //var recordId = $routeParams.id;
-    $scope.viewName = 'Create Commission Data';
+    $scope.viewName = 'Creating Commission Data';
     $scope.commissionData = {};
     $scope.baseCurrency = 'GBP'; // TODO: get from API
     $scope.readOnly = true;
@@ -26,20 +24,11 @@ angular.module('ts5App')
       }
     };
 
-    $scope.getMaxLength = function (value) {
-      if(value === 'Percentage') {
-        return 5;
-      } else {
-        return 10;
-      }
-    };
-
     $scope.updateManualBars = function () {
       if($scope.commissionData.manualBarsType === 'Percentage') {
         $scope.manualBarsUnit = '%';
         $scope.manualBarsCharLimit = 5;
       } else {
-        // TODO: set to company's base currency
         $scope.manualBarsUnit = $scope.baseCurrency;
         $scope.manualBarsCharLimit = 10;
       }
@@ -50,7 +39,6 @@ angular.module('ts5App')
         $scope.incentiveIncrementUnit = '%';
         $scope.incentiveIncrementCharLimit = 5;
       } else {
-        // TODO: set to company's base currency
         $scope.incentiveIncrementUnit = $scope.baseCurrency;
         $scope.incentiveIncrementCharLimit = 10;
       }
@@ -76,17 +64,22 @@ angular.module('ts5App')
       $scope.updateManualBars();
       $scope.updateIncentiveIncrement();
     };
-    this.setCrewBase = function(data) {
-      $scope.crewBaseList = data;
-    };
 
     this.init = function () {
+      $scope.readOnly = $routeParams.state === 'view';
       $this.setCrewBase([{crewName:'CREW'}, {crewName:'CREW2'}]);
+      if($routeParams.state === 'view') {
+        $scope.viewName = 'Viewing Commission Data';
+      } else if($routeParams.state === 'edit') {
+        $scope.viewName = 'Editing Commission Data';
+      }
+
+
       // TODO: API calls to make:
       // commissionFactory.getBaseCurrency();
       // commissionFactory.getCrewBaseList().then(setCrewBase, showError);
+      // commissionFactory.getCommissionData(recordId).then(setCommissionData, showError);
 
-      $scope.readOnly = $routeParams.state === 'view';
       if($routeParams.id) {
         $this.setCommissionData({
           crewBase: 'CREW',
@@ -101,7 +94,6 @@ angular.module('ts5App')
           commissionType: 'Percentage',
           commissionValue: 100
         });
-        // commissionFactory.getCommissionData(recordId).then(setCommissionData, showError);
       }
     };
     this.init();
