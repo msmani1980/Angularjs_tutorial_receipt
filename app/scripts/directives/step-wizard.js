8663b585f127145c58407f6cb9671b510c78cc4e
Kelly Meath <kmeath@egate-solutions.com>
2015-09-01 14:26:39 -0500

    TSVPORTAL-2989 - Checking in wizard to merge master into branch

--- app/scripts/directives/step-wizard.js
+++ app/scripts/directives/step-wizard.js
@@ -15,12 +15,52 @@ angular.module('ts5App')
       steps: '='
     },
     controller: function($scope, $location) {
-      console.log($scope.steps);
-      $scope.stepClass = function(step){
 
-      };
-      $scope.goToStep = function(step, $index){
-        console.log(step);
+      var currentStepIndex = 0;
+
+      function setStepClasses(){
+        for(var i in $scope.steps){
+          if(i < currentStepIndex || i === currentStepIndex) {
+            $scope.steps[i].class = 'active';
+          }
+        }
+      }
+
+      function setCurrentStepIndex(){
+        for(var i in $scope.steps){
+          if($location.path() === $scope.steps[i].uri){
+            currentStepIndex = i;
+          }
+        }
+      }
+
+      function trailingSlashOnStepURI(step){
+        // Add trailing slash to step URI if one exists in address bar
+        if ($location.path().match(/\/$/) && !step.uri.match(/\/$/)) {
+          step.uri += '/';
+        }
+        // Remove trailing slash to step URI if not present in address bar
+        if (!$location.path().match(/\/$/) && step.uri.match(/\/$/)) {
+          step.uri = step.uri.substr(0, step.uri.length - 1);
+        }
+        return step;
+      }
+      function trailingSlashes(){
+        $scope.steps.map(trailingSlashOnStepURI)
+      }
+      function init(){
+        trailingSlashes();
+        setCurrentStepIndex();
+        setStepClasses();
+      }
+      init();
+
+      $scope.goToStepURI = function($index, uri){
+        // Only allow previous steps to be clicked
+        if($index >= currentStepIndex){
+          return false;
+        }
+        $location.url(uri);
       };
     }
   };
