7bdb19c4b5d9da703050fc27077f6dbb172717c6
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-09-03 13:51:14 -0500

    TSVPORTAL-1960 - refactored tests, adding store instace items, code cleanup

--- app/scripts/services/store-instance-service.js
+++ app/scripts/services/store-instance-service.js
@@ -7,52 +7,79 @@
  * # storeInstanceService
  * Service in the ts5App.
  */
-angular.module('ts5App')
-  .service('storeInstanceService', function($resource, ENV) {
-    var requestURL = ENV.apiUrl + '/api/dispatch/store-instances/:id';
-    var requestParameters = {
-      id: '@id'
-    };
-    var actions = {
-      getStoreInstancesList: {
-        method: 'GET'
-      },
-      getStoreInstance: {
-        method: 'GET'
-      },
-      createStoreInstance: {
-        method: 'POST'
-      },
-      updateStoreInstance: {
-        method: 'PUT'
-      },
-      deleteStoreInstance: {
-        method: 'DELETE'
-      }
-    };
-    var requestResource = $resource(requestURL, requestParameters, actions);
+angular.module('ts5App').service('storeInstanceService', function ($resource, ENV) {
+  var requestURL = ENV.apiUrl + '/api/dispatch/store-instances/:id/:api';
+  var requestParameters = {
+    id: '@id',
+    api: '@api'
+  };
+  var actions = {
+    getStoreInstancesList: {
+      method: 'GET'
+    },
+    getStoreInstance: {
+      method: 'GET'
+    },
+    createStoreInstance: {
+      method: 'POST'
+    },
+    updateStoreInstance: {
+      method: 'PUT'
+    },
+    deleteStoreInstance: {
+      method: 'DELETE'
+    },
+    getStoreInstanceItems: {
+      method: 'GET'
+    },
+    getStoreInstancesMenuItems: {
+      method: 'GET'
+    }
+  };
 
-    function getStoreInstancesList(query) {
-      return requestResource.getStoreInstancesList(query).$promise;
-    }
-    function getStoreInstance(id) {
-      return requestResource.getStoreInstance({id: id}).$promise;
-    }
-    function createStoreInstance(payload) {
-      return requestResource.createStoreInstance(payload).$promise;
-    }
-    function updateStoreInstance(id, payload) {
-      return requestResource.updateStoreInstance({id: id}, payload).$promise;
-    }
-    function deleteStoreInstance(id) {
-      return requestResource.deleteStoreInstance({id: id}).$promise;
-    }
+  var requestResource = $resource(requestURL, requestParameters, actions);
 
-    return {
-      getStoreInstancesList: getStoreInstancesList,
-      getStoreInstance: getStoreInstance,
-      createStoreInstance: createStoreInstance,
-      updateStoreInstance: updateStoreInstance,
-      deleteStoreInstance: deleteStoreInstance
-    };
-  });
+  function getStoreInstancesList(payload) {
+    return requestResource.getStoreInstancesList(payload).$promise;
+  }
+
+  function getStoreInstance(id) {
+    return requestResource.getStoreInstance({id: id}).$promise;
+  }
+
+  function createStoreInstance(payload) {
+    return requestResource.createStoreInstance(payload).$promise;
+  }
+
+  function updateStoreInstance(id, payload) {
+    return requestResource.updateStoreInstance({id: id}, payload).$promise;
+  }
+
+  function deleteStoreInstance(id) {
+    return requestResource.deleteStoreInstance({id: id}).$promise;
+  }
+
+  function getStoreInstanceItems(id) {
+    return requestResource.getStoreInstanceItems({
+      id: id,
+      api: 'items'
+    }).$promise;
+  }
+
+  function getStoreInstanceMenuItems(id) {
+    return requestResource.getStoreInstanceMenuItems({
+      id: id,
+      api: 'menu-items'
+    }).$promise;
+  }
+
+  return {
+    getStoreInstancesList: getStoreInstancesList,
+    getStoreInstance: getStoreInstance,
+    createStoreInstance: createStoreInstance,
+    updateStoreInstance: updateStoreInstance,
+    deleteStoreInstance: deleteStoreInstance,
+    getStoreInstanceItems: getStoreInstanceItems,
+    getStoreInstanceMenuItems: getStoreInstanceMenuItems
+  };
+});
--- test/spec/services/store-instance-service.js
+++ test/spec/services/store-instance-service.js
@@ -15,41 +15,82 @@ describe('Service: storeInstanceService', function () {
 
   describe('getStoreInstancesList', function () {
     it('should make GET request to API', function () {
-      httpBackend.whenGET(/dispatch\/store-instances/).respond({done: true});
-      storeInstanceService.getStoreInstancesList();
-      httpBackend.expectGET(/dispatch\/store-instances/);
+      var expectedURL = /dispatch\/store-instances$/;
+      httpBackend.expectGET(expectedURL).respond(200, {success: true});
+      storeInstanceService.getStoreInstancesList().then(function (response) {
+        expect(response.success).toBe(true);
+      });
       httpBackend.flush();
     });
   });
+
   describe('getStoreInstance', function () {
     it('should make GET request to API', function () {
-      httpBackend.whenGET(/dispatch\/store-instances/).respond({done: true});
-      storeInstanceService.getStoreInstance(38);
-      httpBackend.expectGET(/dispatch\/store-instances/);
+      var expectedURL = /dispatch\/store-instances\/\d+$/;
+      var fakeId = 38;
+      httpBackend.expectGET(expectedURL).respond(200, {id: fakeId});
+      storeInstanceService.getStoreInstance(fakeId).then(function (response) {
+        expect(response.id).toBe(fakeId);
+      });
       httpBackend.flush();
     });
   });
-  describe('createStoreInstance', function(){
+
+  describe('createStoreInstance', function () {
     it('should make POST request to API', function () {
-      httpBackend.whenPOST(/dispatch\/store-instances/).respond({done: true});
-      storeInstanceService.createStoreInstance({});
-      httpBackend.expectPOST(/dispatch\/store-instances/);
+      var expectedURL = /dispatch\/store-instances$/;
+      var fakePayload = {
+        key: 'value'
+      };
+      httpBackend.expectPOST(expectedURL).respond(200, angular.extend({}, fakePayload, {id: 'newFakeId'}));
+
+      storeInstanceService.createStoreInstance(fakePayload).then(function (response) {
+        expect(response.id).toBe('newFakeId');
+        expect(response.key).toBe(fakePayload.key);
+      });
+
       httpBackend.flush();
     });
   });
-  describe('updateStoreInstance', function(){
+
+  describe('updateStoreInstance', function () {
     it('should make PUT request to API', function () {
-      httpBackend.whenPUT(/dispatch\/store-instances/).respond({done: true});
-      storeInstanceService.updateStoreInstance({});
-      httpBackend.expectPUT(/dispatch\/store-instances/);
+      var expectedURL = /dispatch\/store-instances\/\d+$/;
+      var fakeId = 38;
+      var fakePayload = {
+        id: fakeId,
+        key: 'value'
+      };
+      httpBackend.expectPUT(expectedURL).respond(200, fakePayload);
+
+      storeInstanceService.updateStoreInstance(fakeId, fakePayload).then(function (response) {
+        expect(response.id).toBe(fakePayload.id);
+        expect(response.key).toBe(fakePayload.key);
+      });
       httpBackend.flush();
     });
   });
-  describe('deleteStoreInstance', function(){
+
+  describe('deleteStoreInstance', function () {
     it('should make DELETE request to API', function () {
-      httpBackend.whenDELETE(/dispatch\/store-instances/).respond({done: true});
-      storeInstanceService.deleteStoreInstance(12);
-      httpBackend.expectDELETE(/dispatch\/store-instances/);
+      var expectedURL = /dispatch\/store-instances\/\d+$/;
+      var fakeId = 12;
+      httpBackend.expectDELETE(expectedURL).respond(200, {id: fakeId});
+      storeInstanceService.deleteStoreInstance(fakeId).then(function (response) {
+        expect(response.id).toBe(fakeId);
+      });
+      httpBackend.flush();
+    });
+  });
+
+  describe('getStoreInstanceItems', function () {
+    it('should make GET request to API', function () {
+      var expectedURL = /dispatch\/store-instances\/\d+\/items$/;
+      var fakeId = 12;
+      httpBackend.expectGET(expectedURL).respond(200, {id: fakeId});
+      storeInstanceService.getStoreInstanceItems(fakeId).then(function (response) {
+        expect(response.id).toBe(fakeId);
+      });
       httpBackend.flush();
     });
   });
