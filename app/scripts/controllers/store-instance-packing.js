453cd83474ed0028c53d675a0b4952779d186959
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-09-04 16:01:53 -0500

    TSVPORTAL-1960 - markup cleanup, integration of store details to html

--- app/scripts/controllers/store-instance-packing.js
+++ app/scripts/controllers/store-instance-packing.js
@@ -8,7 +8,9 @@
  * Controller of the ts5App
  */
 angular.module('ts5App').controller('StoreInstancePackingCtrl', function ($scope, storeInstanceFactory) {
-    storeInstanceFactory.getStoreDetails(5).then(function (data) {
-      console.log(data);
+    storeInstanceFactory.getStoreDetails(5).then(function (storeDetailsJSON) {
+      angular.forEach(storeDetailsJSON, function(value, key){
+        $scope[key] = value;
+      });
     });
   });
--- test/spec/controllers/store-instance-packing.js
+++ test/spec/controllers/store-instance-packing.js
@@ -5,15 +5,51 @@ describe('Controller: StoreInstancePackingCtrl', function () {
   // load the controller's module
   beforeEach(module('ts5App'));
 
-  var StoreInstancePackingCtrl,
-    scope;
+  var StoreInstancePackingCtrl;
+  var scope;
+  var storeInstanceFactory;
+  var storeDetailsJSON;
+  var getStoreDetailsDeferred;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector, $q) {
     scope = $rootScope.$new();
+
+    storeInstanceFactory = $injector.get('storeInstanceFactory');
+
+    getStoreDetailsDeferred = $q.defer();
+    spyOn(storeInstanceFactory, 'getStoreDetails').and.returnValue(getStoreDetailsDeferred.promise);
+
     StoreInstancePackingCtrl = $controller('StoreInstancePackingCtrl', {
       $scope: scope
     });
   }));
+
+  describe('Init', function () {
+    describe('API calls', function () {
+
+      beforeEach(function () {
+        storeDetailsJSON = {
+          LMPStation: "ORD",
+          storeNumber: "180485",
+          scheduleDate: "2015-08-13",
+          scheduleNumber: "SCHED123",
+          storeInstanceNumber: 5
+        };
+        getStoreDetailsDeferred.resolve(storeDetailsJSON);
+        scope.$digest();
+      });
+
+      it('should get the store details', function () {
+        expect(storeInstanceFactory.getStoreDetails).toHaveBeenCalled();
+      });
+
+      it('should attach all properties of JSON to scope', function () {
+        angular.forEach(storeDetailsJSON, function (value, key) {
+          expect(scope[key]).toBe(storeDetailsJSON[key]);
+        });
+      });
+    });
+  });
 
 });
