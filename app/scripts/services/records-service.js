e1bab70dab961b10ac540bc26c319ebd355cb3e0
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-09-10 10:26:16 -0500

    TSVPORTAL-1960 - add store status to records service, cleanup tests

--- app/scripts/services/records-service.js
+++ app/scripts/services/records-service.js
@@ -27,6 +27,10 @@ angular.module('ts5App')
       getDiscountTypes: {
         method: 'GET',
         isArray: true
+      },
+      getStoreStatus: {
+        method: 'GET',
+        isArray: true
       }
     };
     var requestResource = $resource(requestURL, requestParameters, actions);
@@ -46,9 +50,15 @@ angular.module('ts5App')
       return requestResource.getDiscountTypes().$promise;
     }
 
+    function getStoreStatus() {
+      requestParameters.api = 'store-status';
+      return requestResource.getStoreStatus().$promise;
+    }
+
     return {
       getCrewBaseTypes: getCrewBaseTypes,
       getCommissionPayableTypes: getCommissionPayableTypes,
-      getDiscountTypes: getDiscountTypes
+      getDiscountTypes: getDiscountTypes,
+      getStoreStatus: getStoreStatus
     };
   });
