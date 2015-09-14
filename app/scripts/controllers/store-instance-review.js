4b1bf3be06a479c8278c227076e39b2cceba85a6
Alyssa Reyes <areyes@egate-solutions.com>
2015-09-14 17:16:22 -0500

    TSVPORTAL-1960 - fix jshint error

--- app/scripts/controllers/store-instance-review.js
+++ app/scripts/controllers/store-instance-review.js
@@ -132,6 +132,14 @@ angular.module('ts5App')
       $scope.storeDetails.currentStatus = $filter('filter')($scope.storeDetails.statusList, {id: response.statusId}, true)[0];
       showUserCurrentStatus('updated');
     }
+    
+    function getStatusNameIntByName(name){
+      var status = $filter('filter')($scope.storeDetails.statusList, {statusName: name}, true);
+      if(!status || !status.length){
+        return false;
+      }
+      return status[0].name;
+    }
 
     function getSetStoreStatusByNamePromise(name){
       $scope.formErrors = [];
@@ -171,14 +179,6 @@ angular.module('ts5App')
     function updatedStoreStatusSubmitted(){
       showMessage('Now what? Redirect user where?', 'info');
       // TODO redirect user somewhere?
-    }
-
-    function getStatusNameIntByName(name){
-      var status = $filter('filter')($scope.storeDetails.statusList, {statusName: name}, true);
-      if(!status || !status.length){
-        return false;
-      }
-      return status[0].name;
     }
 
     function init() {
