d222503aca500fa32676d11101a3659d419417fd
Max Felker <mfelker@egate-solutions.com>
2015-09-15 15:28:12 -0400

    TSVPORTAL-3154 | Mocked sealTypesList in controller, updated scope managment and actions for directive, mocked tests

--- app/scripts/controllers/store-instance-seals.js
+++ app/scripts/controllers/store-instance-seals.js
@@ -15,6 +15,7 @@ angular.module('ts5App')
 
     this.setSealColors = function(sealColorsJSON) {
       $scope.sealColorsList = sealColorsJSON;
+      console.log($scope.sealColorsList);
     };
 
     this.setStoreDetails = function(storeDetailsJSON) {
@@ -22,7 +23,7 @@ angular.module('ts5App')
     };
 
     this.setSealTypes = function(sealTypesJSON) {
-      $scope.sealTypesList = sealTypesJSON;
+      $scope.sealTypes = sealTypesJSON;
     };
 
     this.setWizardSteps = function(storeId) {
@@ -44,6 +45,7 @@ angular.module('ts5App')
     this.init = function() {
       $scope.storeId = $routeParams.storeId;
       if ($scope.storeId) {
+        // TODO: Make a $q
         this.getStoreDetails();
         this.getSealColors();
         this.getSealTypes();
@@ -52,5 +54,69 @@ angular.module('ts5App')
     };
 
     this.init();
+
+    $scope.copySeals = function(copyFrom,copyTo) {
+       var sealTypeFrom = $scope.sealTypesList.filter(function(sealType) { return sealType.name === copyFrom; })[0];
+       var sealTypeTo = $scope.sealTypesList.filter(function(sealType) { return sealType.name === copyTo; })[0];
+       sealTypeTo.seals.numbers = angular.copy(sealTypeFrom.seals.numbers);
+    };
+
+    // TODO: Set with controller method after APIs all resolve
+    $scope.sealTypesList = [
+      {
+        name:'Outbound',
+        code: 'OB',
+        color:'#00B200',
+        seals: {
+            numbers:[]
+        }
+      },
+      {
+        name:'Handover',
+        code: 'HO',
+        color:'#E5E500',
+        seals: {
+            numbers:[]
+        },
+        actions: [
+          {
+            label: 'Copy From Outbound',
+            trigger: function() {
+              return $scope.copySeals('Outbound','Handover');
+            }
+          }
+        ]
+      },
+      {
+        name:'Inbound',
+        code: 'IB',
+        color:'#0000FF',
+        seals: {
+            numbers:[]
+        },
+        actions: [
+          {
+            label: 'Copy From Outbound',
+            trigger: function() {
+              return $scope.copySeals('Outbound','Inbound');
+            }
+          },
+          {
+            label: 'Copy From Handover',
+            trigger: function() {
+              return $scope.copySeals('Handover','Inbound');
+            }
+          },
+        ]
+      },
+      {
+        name:'High Security',
+        code: 'HS',
+        color:'#B70024',
+        seals: {
+            numbers:[]
+        }
+      },
+    ];
 
   });
