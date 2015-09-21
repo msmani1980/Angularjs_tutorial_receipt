5823c4b7a8a70b9096e924138814c6c47c190e36
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-09-21 14:09:56 -0500

    TSVPORTAL-3090 - Formatting date, code cleanup

--- app/scripts/controllers/cash-bag-list.js
+++ app/scripts/controllers/cash-bag-list.js
@@ -11,16 +11,16 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('CashBagListCtrl', function ($scope, cashBagFactory, $location, $routeParams, $q, ngToast) {
+  .controller('CashBagListCtrl', function ($scope, cashBagFactory, $location, $routeParams, $q, ngToast, dateUtility) {
 
-    var _companyId = null,
-      _services = null;
+    var _companyId = null
+    var _services  = null;
 
-    $scope.viewName = 'Manage Cash Bag';
-    $scope.displayModalError = false;
-    $scope.displayError = false;
+    $scope.viewName           = 'Manage Cash Bag';
+    $scope.displayModalError  = false;
+    $scope.displayError       = false;
     $scope.createCashBagError = 'temp error message';
-    $scope.search = {};
+    $scope.search             = {};
 
     function showSuccessMessage(error) {
       ngToast.create({
@@ -41,11 +41,30 @@ angular.module('ts5App')
       return bankRefList;
     }
 
+    function formatDateForApp() {
+      $scope.cashBagList.map(function (cashBag) {
+        if (cashBag.scheduleDate) {
+          cashBag.scheduleDate = dateUtility.formatDateForApp(cashBag.scheduleDate);
+        }
+      });
+    }
+
+    function getCashBagResponseHandler (response) {
+      $scope.cashBagList = angular.copy(response.cashBags);
+      formatDateForApp();
+      angular.forEach($scope.cashBagList, function (cashBag) {
+        if ($scope.isNew(cashBag.id)) {
+          showSuccessMessage('successfully created');
+        }
+      });
+      $scope.bankRefList = getSortedBankRefList(response.cashBags);
+    }
+
     // Constructor
     (function constructor() {
       // set global controller properties
       _companyId = cashBagFactory.getCompanyId();
-      _services = {
+      _services  = {
         promises: [],
         call: function (servicesArray) {
           angular.forEach(servicesArray, function (_service) {
@@ -53,23 +72,13 @@ angular.module('ts5App')
           });
         },
         getCashBagList: function () {
-          return cashBagFactory.getCashBagList(_companyId,{isDelete:'false'}).then(
-            function (response) {
-              $scope.cashBagList = response.cashBags;
-              angular.forEach($scope.cashBagList, function(_cb){
-                if($scope.isNew(_cb.id)){
-                  showSuccessMessage('successfully created');
-                }
-              });
-              $scope.bankRefList = getSortedBankRefList(response.cashBags);
-            }
-          );
+          return cashBagFactory.getCashBagList(_companyId, {isDelete: 'false'}).then(getCashBagResponseHandler);
         },
         getStationList: function () {
           return cashBagFactory.getStationList(_companyId).then(
             function (response) {
-              $scope.stationList = response.response;
-              $('.stations-multi-select').select2({ width: '100%' });
+              $scope.stationList = angular.copy(response.response);
+              $('.stations-multi-select').select2({width: '100%'});
 
             }
           );
@@ -77,7 +86,7 @@ angular.module('ts5App')
         getSchedulesList: function () {
           return cashBagFactory.getSchedulesList(_companyId).then(
             function (response) {
-              $scope.schedulesList = response.distinctSchedules;
+              $scope.schedulesList = angular.copy(response.distinctSchedules);
             }
           );
         }
@@ -87,7 +96,7 @@ angular.module('ts5App')
 
     // helpers
     function showModalErrors(errorMessage) {
-      $scope.displayModalError = true;
+      $scope.displayModalError  = true;
       $scope.createCashBagError = errorMessage;
     }
 
@@ -103,13 +112,13 @@ angular.module('ts5App')
     $scope.searchCashBag = function () {
       if ($scope.search.startDate) {
         $scope.search.startDate = moment($scope.search.startDate, 'MM/DD/YYYY').format('YYYYMMDD').toString();
-        $scope.search.endDate = $scope.search.startDate;
+        $scope.search.endDate   = $scope.search.startDate;
       }
       cashBagFactory.getCashBagList(_companyId, $scope.search).then(function (response) {
         $scope.cashBagList = response.cashBags;
-        if($scope.search.startDate) {
+        if ($scope.search.startDate) {
           $scope.search.startDate = moment($scope.search.startDate, 'YYYYMMDD').format('MM/DD/YYYY').toString();
-          $scope.search.endDate = $scope.search.endDate;
+          $scope.search.endDate   = $scope.search.endDate;
         }
       });
     };
@@ -120,7 +129,7 @@ angular.module('ts5App')
       $scope.searchCashBag();
     };
 
-    $scope.isNew = function(cashBagId){
+    $scope.isNew = function (cashBagId) {
       return ($routeParams.newId === cashBagId);
     };
 
