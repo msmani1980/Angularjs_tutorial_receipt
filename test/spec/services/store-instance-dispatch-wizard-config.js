0cd02eb9f69ef9616d67d10a86bb0ca19e58855d
Vinko Buble <vbuble@egate-solutions.com>
2015-09-23 05:58:47 -0400

    TSVPORTAL-1133 fixed failing test

--- test/spec/services/store-instance-dispatch-wizard-config.js
+++ test/spec/services/store-instance-dispatch-wizard-config.js
@@ -1,39 +1,60 @@
 'use strict';
 
-describe('Service: storeInstanceDispatchWizardConfig', function () {
+fdescribe('Service: storeInstanceDispatchWizardConfig', function () {
 
   // load the service's module
   beforeEach(module('ts5App'));
 
   // instantiate service
   var storeInstanceDispatchWizardConfig;
-  var mockedConfig;
+  var mockedConfigDispatch;
+  var mockedConfigReplenish;
   var mockId;
   beforeEach(inject(function (_storeInstanceDispatchWizardConfig_) {
     storeInstanceDispatchWizardConfig = _storeInstanceDispatchWizardConfig_;
     mockId = 7;
-    mockedConfig = [
+    mockedConfigDispatch = [
       {
         label: 'Create Store Instance',
-        uri: '/store-instance-create'
+        uri: '/store-instance-create/dispatch/' + mockId
       },
       {
         label: 'Packing',
-        uri: '/store-instance-packing/' + mockId
+        uri: '/store-instance-packing/dispatch/' + mockId
       },
       {
         label: 'Assign Seals',
-        uri: '/store-instance-seals/' + mockId
+        uri: '/store-instance-seals/dispatch/' + mockId
       },
       {
         label: 'Review & Dispatch',
-        uri: '/store-instance-review/' + mockId + '/dispatch'
+        uri: '/store-instance-review/dispatch/' + mockId
+      }
+    ];
+    mockedConfigReplenish = [
+      {
+        label: 'Create Store Replenish',
+        uri: '/store-instance-create/replenish/' + mockId
+      },
+      {
+        label: 'Replenish Packing',
+        uri: '/store-instance-packing/replenish/' + mockId
+      },
+      {
+        label: 'Assign Replenish Seals',
+        uri: '/store-instance-seals/replenish/' + mockId
+      },
+      {
+        label: 'Review & Dispatch Replenish',
+        uri: '/store-instance-review/replenish/' + mockId
       }
     ];
   }));
 
   it('should match mocked dispatch config', function(){
-    var config = storeInstanceDispatchWizardConfig.getSteps(mockId);
-    expect(config).toEqual(mockedConfig);
+    var configDispatch = storeInstanceDispatchWizardConfig.getSteps('dispatch', mockId);
+    expect(configDispatch).toEqual(mockedConfigDispatch);
+    var configReplenish = storeInstanceDispatchWizardConfig.getSteps('replenish', mockId);
+    expect(configReplenish).toEqual(mockedConfigReplenish);
   });
 });
