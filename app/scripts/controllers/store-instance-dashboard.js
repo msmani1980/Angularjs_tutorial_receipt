fefbe0225e252eecbae50c67867ba4a5588e02a1
Vinko Buble <vbuble@egate-solutions.com>
2015-10-14 11:56:44 -0400

    TSVPORTAL-3800 export pdf

--- app/scripts/controllers/store-instance-dashboard.js
+++ app/scripts/controllers/store-instance-dashboard.js
@@ -11,7 +11,9 @@
  */
 angular.module('ts5App').controller('StoreInstanceDashboardCtrl',
   function($scope, storeInstanceDashboardFactory, storeTimeConfig, lodash, dateUtility, $q,
-    $route, ngToast, $location, $filter) {
+    $route, ngToast, $location, $filter, ENV) {
+
+    var $this = this;
 
     $scope.viewName = 'Store Instance Dashboard';
     $scope.catererStationList = [];
@@ -23,6 +25,8 @@ angular.module('ts5App').controller('StoreInstanceDashboardCtrl',
     $scope.allCheckboxesSelected = false;
     $scope.allScheduleDetailsExpanded = false;
     $scope.openStoreInstanceId = -1;
+    $scope.hasSelectedStore = false;
+    $scope.exportURL = '';
 
     function showLoadingModal(text) {
       angular.element('#loading').modal('show').find('p').text(text);
@@ -94,7 +98,24 @@ angular.module('ts5App').controller('StoreInstanceDashboardCtrl',
           store.selected = $scope.allCheckboxesSelected;
         }
       });
+      $this.storeSelectionToglled();
     };
+
+    $scope.storeSelectionToglled = function() {
+      $scope.hasSelectedStore = lodash.reduce($scope.storeInstanceList, function(total, item) {
+        return total || item.selected;
+      }, false);
+      if ($scope.hasSelectedStore) {
+        $scope.exportURL = ENV.apiUrl + '/api/dispatch/store-instances/documents/C208?sessionToken=' + '9e85ffbb3b92134fbf39a0c366bd3f12f0f5' +
+          lodash.reduce($scope.storeInstanceList, function(total, item) {
+            return (item.selected ? total + '&storeInstanceId=' + item.id : total);
+          }, '')
+        ;//$http.defaults.headers.common.sessionToken;
+      } else {
+        $scope.exportURL = '';
+      }
+    };
+
 
     $scope.isScheduleDetailOpen = function(id) {
       return !(angular.element('.scheduleDetails-' + id).hasClass('accordion-cell-closed'));
