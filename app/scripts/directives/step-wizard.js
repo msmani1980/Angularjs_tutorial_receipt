4543e46c22d01b7bbeb474433f80dada9be9751e
Jared Donisvitch <jared@bigroomstudios.com>
2015-10-27 10:28:55 -0400

    TSVPORTAL-4127 | add flag and tests for when a user goes to the prevStep and it's step 1

--- app/scripts/directives/step-wizard.js
+++ app/scripts/directives/step-wizard.js
@@ -7,156 +7,161 @@
  * # stepWizard
  */
 angular.module('ts5App')
-.directive('stepWizard', function () {
-  return {
-    templateUrl: '/views/directives/step-wizard.html',
-    restrict: 'E',
-    scope: {
-      steps: '=',
-      disabled: '=',
-      showNextPrevButton: '=',
-      nextTrigger: '&',
-      prevTrigger: '&',
-      saveTrigger: '&',
-      saveButtonText: '='
-    },
-    controller: function($scope, $location) {
+  .directive('stepWizard', function() {
+    return {
+      templateUrl: '/views/directives/step-wizard.html',
+      restrict: 'E',
+      scope: {
+        steps: '=',
+        disabled: '=',
+        showNextPrevButton: '=',
+        nextTrigger: '&',
+        prevTrigger: '&',
+        saveTrigger: '&',
+        saveButtonText: '='
+      },
+      controller: function($scope, $location) {
 
-      var currentStepIndex = null;
+        var currentStepIndex = null;
 
-      // Maliformed uri cleanup
-      function trailingSlashOnStepURI(step){
-        // Add trailing slash to step URI if one exists in address bar
-        if ($location.path().match(/\/$/) && !step.uri.match(/\/$/)) {
-          step.uri += '/';
+        // Maliformed uri cleanup
+        function trailingSlashOnStepURI(step) {
+          // Add trailing slash to step URI if one exists in address bar
+          if ($location.path().match(/\/$/) && !step.uri.match(/\/$/)) {
+            step.uri += '/';
+          }
+          // Remove trailing slash to step URI if not present in address bar
+          if (!$location.path().match(/\/$/) && step.uri.match(/\/$/)) {
+            step.uri = step.uri.substr(0, step.uri.length - 1);
+          }
+          return step;
         }
-        // Remove trailing slash to step URI if not present in address bar
-        if (!$location.path().match(/\/$/) && step.uri.match(/\/$/)) {
-          step.uri = step.uri.substr(0, step.uri.length - 1);
-        }
-        return step;
-      }
-      function trailingSlashes(){
-        $scope.steps.map(trailingSlashOnStepURI);
-      }
-      function init(){
-        if(angular.isUndefined($scope.saveButtonText)) {
-          $scope.saveButtonText = 'Save & Exit';
-        }
-        trailingSlashes();
-      }
-      init();
 
-      function callTrigger(trigger, toIndex){
-        $scope.$parent.wizardStepToIndex = toIndex;
-        // If controller does not return true, do not step the user
-        if(angular.isUndefined($scope[trigger])) {
-          return false;
+        function trailingSlashes() {
+          $scope.steps.map(trailingSlashOnStepURI);
         }
-        var triggerReturn = $scope[trigger]();
-        if(typeof triggerReturn !== 'boolean') {
-          return false;
-        }
-        return triggerReturn;
-      }
 
-      function resolveTrigger(autoStep, toIndex){
-        if(!autoStep) {
-          return false;
+        function init() {
+          if (angular.isUndefined($scope.saveButtonText)) {
+            $scope.saveButtonText = 'Save & Exit';
+          }
+          trailingSlashes();
         }
-        $location.url($scope.steps[toIndex].uri);
-        return true;
-      }
+        init();
 
-      $scope.goToStepURI = function($index){
-        if($scope.disabled){
-          return false;
+        function callTrigger(trigger, toIndex) {
+          $scope.$parent.wizardStepToIndex = toIndex;
+          // If controller does not return true, do not step the user
+          if (angular.isUndefined($scope[trigger])) {
+            return false;
+          }
+          var triggerReturn = $scope[trigger]();
+          if (typeof triggerReturn !== 'boolean') {
+            return false;
+          }
+          return triggerReturn;
         }
-        if($index < 0){
-          return false;
-        }
-        // Only allow previous steps to be clicked
-        if($index >= currentStepIndex){
-          return false;
-        }
-        // if controller's prev-trigger scope function returns false
-        // the wizard will NOT step backwards
-        var autoStepBackwards = callTrigger('prevTrigger', $index);
-        return resolveTrigger(autoStepBackwards, $index);
-      };
 
-      $scope.wizardSave = function(){
-        if($scope.disabled){
-          return false;
-        }
-        callTrigger('saveTrigger', currentStepIndex);
-      };
-
-      $scope.wizardPrev = function(){
-        if($scope.disabled){
-          return false;
-        }
-        var prevIndex = currentStepIndex - 1;
-        if(prevIndex < 0){
-          return false;
-        }
-        return $scope.goToStepURI(prevIndex);
-      };
-
-      $scope.wizardNext = function(){
-        if($scope.disabled){
-          return false;
-        }
-        if(!$scope.showNextPrevButton){
-          return false;
-        }
-        var nextIndex = currentStepIndex + 1;
-        // No more steps, dont do anything
-        if(nextIndex > $scope.steps.length){
-          return false;
-        }
-        // If the controller's next-trigger scope function returns false
-        // the wizard will NOT step forward
-        var stepForward = callTrigger('nextTrigger', nextIndex);
-        return resolveTrigger(stepForward, nextIndex);
-      };
-
-      $scope.disablePrev = function(){
-        if($scope.disabled){
+        function resolveTrigger(autoStep, toIndex) {
+          if (!autoStep) {
+            return false;
+          }
+          $location.url($scope.steps[toIndex].uri);
           return true;
         }
-        return !currentStepIndex;
-      };
 
-      $scope.disableNext = function(){
-        if($scope.disabled){
-          return true;
-        }
-        return (currentStepIndex === ($scope.steps.length - 1));
-      };
+        $scope.goToStepURI = function($index) {
+          if ($scope.disabled) {
+            return false;
+          }
+          if ($index < 0) {
+            return false;
+          }
+          // Only allow previous steps to be clicked
+          if ($index >= currentStepIndex) {
+            return false;
+          }
+          // if controller's prev-trigger scope function returns false
+          // the wizard will NOT step backwards
+          var autoStepBackwards = callTrigger('prevTrigger', $index);
+          return resolveTrigger(autoStepBackwards, $index);
+        };
 
-      $scope.disableStep = function($index){
-        return $scope.disabled || ($scope.steps[$index].disabled);
-      };
+        $scope.wizardSave = function() {
+          if ($scope.disabled) {
+            return false;
+          }
+          callTrigger('saveTrigger', currentStepIndex);
+        };
 
-      $scope.stepInit = function($index){
-        if($location.path() === $scope.steps[$index].uri){
-          currentStepIndex = $index;
-        }
-        if(null === currentStepIndex){
-          $scope.steps[$index].class = 'complete';
-          return;
-        }
-        $scope.steps[$index].disabled = true;
-        if($index === currentStepIndex) {
-          $scope.steps[$index].class = 'active';
-          return;
-        }
-        if($index > currentStepIndex) {
-          $scope.steps[$index].class = 'future';
-        }
-      };
-    }
-  };
+        $scope.wizardPrev = function() {
+          if ($scope.disabled) {
+            return false;
+          }
+          var prevIndex = currentStepIndex - 1;
+          if (prevIndex < 0) {
+            return false;
+          }
+          if (prevIndex === 0) {
+            $scope.stepOneFromStepTwo = true;
+          }
+          return $scope.goToStepURI(prevIndex);
+        };
 
-});
+        $scope.wizardNext = function() {
+          if ($scope.disabled) {
+            return false;
+          }
+          if (!$scope.showNextPrevButton) {
+            return false;
+          }
+          var nextIndex = currentStepIndex + 1;
+          // No more steps, dont do anything
+          if (nextIndex > $scope.steps.length) {
+            return false;
+          }
+          // If the controller's next-trigger scope function returns false
+          // the wizard will NOT step forward
+          var stepForward = callTrigger('nextTrigger', nextIndex);
+          return resolveTrigger(stepForward, nextIndex);
+        };
+
+        $scope.disablePrev = function() {
+          if ($scope.disabled) {
+            return true;
+          }
+          return !currentStepIndex;
+        };
+
+        $scope.disableNext = function() {
+          if ($scope.disabled) {
+            return true;
+          }
+          return (currentStepIndex === ($scope.steps.length - 1));
+        };
+
+        $scope.disableStep = function($index) {
+          return $scope.disabled || ($scope.steps[$index].disabled);
+        };
+
+        $scope.stepInit = function($index) {
+          if ($location.path() === $scope.steps[$index].uri) {
+            currentStepIndex = $index;
+          }
+          if (null === currentStepIndex) {
+            $scope.steps[$index].class = 'complete';
+            return;
+          }
+          $scope.steps[$index].disabled = true;
+          if ($index === currentStepIndex) {
+            $scope.steps[$index].class = 'active';
+            return;
+          }
+          if ($index > currentStepIndex) {
+            $scope.steps[$index].class = 'future';
+          }
+        };
+      }
+    };
+
+  });
--- test/spec/directives/step-wizard.js
+++ test/spec/directives/step-wizard.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('The Step Wizard directive', function () {
+describe('The Step Wizard directive', function() {
 
   // load the directive's module
   beforeEach(module('ts5App', 'template-module'));
@@ -12,13 +12,13 @@ describe('The Step Wizard directive', function () {
   var template;
   var compile;
 
-  beforeEach(inject(function ($rootScope, $location, $compile) {
+  beforeEach(inject(function($rootScope, $location, $compile) {
     location = $location;
     scope = $rootScope.$new();
     compile = $compile;
   }));
 
-  function compileDirective(){
+  function compileDirective() {
     element = angular.element(template);
     element = compile(element)(scope);
     scope.$apply();
@@ -27,43 +27,38 @@ describe('The Step Wizard directive', function () {
   }
 
   describe('location without trailing slash', function() {
-    beforeEach(inject(function(){
-
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2/'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2/'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
       scope.param1 = 1;
       scope.param2 = 2;
       scope.param3 = 3;
       scope.param4 = 4;
-      scope.mockNextTrigger1 = function(param1){
-        if(param1 === 2){
+      scope.mockNextTrigger1 = function(param1) {
+        if (param1 === 2) {
           return;
         }
         return true;
       };
-      scope.mockPrevTrigger1 = function(param2, param3){
-        if(param2 === 3){
+      scope.mockPrevTrigger1 = function(param2, param3) {
+        if (param2 === 3) {
           return;
         }
-        if(param3 === 4){
+        if (param3 === 4) {
           return;
         }
         return true;
       };
-      scope.mockSaveTrigger1 = function(param4){
-        if(param4 === 5){
+      scope.mockSaveTrigger1 = function(param4) {
+        if (param4 === 5) {
           return;
         }
       };
@@ -77,116 +72,112 @@ describe('The Step Wizard directive', function () {
       spyOn(location, 'url').and.callThrough();
       spyOn(location, 'path').and.returnValue('/test-uri-2');
 
-      spyOn(scope,'mockNextTrigger1').and.callThrough();
-      spyOn(scope,'mockPrevTrigger1').and.callThrough();
-      spyOn(scope,'mockSaveTrigger1').and.callThrough();
+      spyOn(scope, 'mockNextTrigger1').and.callThrough();
+      spyOn(scope, 'mockPrevTrigger1').and.callThrough();
+      spyOn(scope, 'mockSaveTrigger1').and.callThrough();
 
       compileDirective();
     }));
 
-    it('should not disable step 2 since on step 3', function(){
+    it('should not disable step 2 since on step 3', function() {
       expect(directiveScope.steps[1].disabled).toBe(true);
     });
 
-    it('should not disable the 1st step button', function(){
+    it('should not disable the 1st step button', function() {
       expect(directiveScope.disableStep(0)).toBeFalsy();
     });
 
-    it('should disable the 2nd step button', function(){
+    it('should disable the 2nd step button', function() {
       expect(directiveScope.disableStep(1)).toBe(true);
     });
 
-    it('should disable the 3rd step button', function(){
+    it('should disable the 3rd step button', function() {
       expect(directiveScope.disableStep(2)).toBe(true);
     });
 
-    it('should disable step 3 since on step 2', function(){
+    it('should disable step 3 since on step 2', function() {
       expect(directiveScope.steps[2].disabled).toBe(true);
     });
 
-    it('should have a steps var in the elements scopes that matches wizardSteps', function () {
+    it('should have a steps var in the elements scopes that matches wizardSteps', function() {
       expect(directiveScope.steps).toEqual(scope.wizardSteps);
     });
 
-    it('should set the 1st step\'s class equal to active', function () {
+    it('should set the 1st step\'s class equal to active', function() {
       expect(directiveScope.steps[0].class).toBe('complete');
     });
 
-    it('should set the 2nd steps\'s class equal to active', function () {
+    it('should set the 2nd steps\'s class equal to active', function() {
       expect(directiveScope.steps[1].class).toBe('active');
     });
 
-    it('should not set the 3rd steps\'s class', function () {
+    it('should not set the 3rd steps\'s class', function() {
       expect(directiveScope.steps[2].class).toBe('future');
     });
 
-    it('should strip the trailing slash from Test label 2\'s URI', function () {
+    it('should strip the trailing slash from Test label 2\'s URI', function() {
       expect(directiveScope.steps[1].uri).toBe('/test-uri-2');
     });
 
-    it('should step back to /test-uri-1 when wizardPrev is triggered', function(){
+    it('should step back to /test-uri-1 when wizardPrev is triggered', function() {
       expect(directiveScope.wizardPrev()).toBe(true);
       expect(directiveScope.goToStepURI).toHaveBeenCalledWith(0);
       expect(scope.mockPrevTrigger1).toHaveBeenCalledWith(scope.param1);
       expect(location.url).toHaveBeenCalledWith('/test-uri-1');
+      expect(directiveScope.stepOneFromStepTwo).toBeTruthy();
     });
 
-    it('should step forward to /test-uri-3 when wizardNext is triggered', function(){
+    it('should step forward to /test-uri-3 when wizardNext is triggered', function() {
       expect(directiveScope.wizardNext()).toBe(false);
       expect(scope.mockNextTrigger1).toHaveBeenCalledWith(scope.param2, scope.param3);
       expect(scope.wizardStepToIndex).toBe(2);
     });
 
-    it('should return false if index is less than 0', function(){
+    it('should return false if index is less than 0', function() {
       expect(directiveScope.goToStepURI(-1)).toBe(false);
     });
 
-    it('should return false if index is equal to currentStepIndex', function(){
+    it('should return false if index is equal to currentStepIndex', function() {
       expect(directiveScope.goToStepURI(1)).toBe(false);
     });
 
-    it('should return false if index is greater than currentStepIndex', function(){
+    it('should return false if index is greater than currentStepIndex', function() {
       expect(directiveScope.goToStepURI(5)).toBe(false);
     });
 
-    it('should call the parent scopes save trigger when save and exit is clicked', function(){
+    it('should call the parent scopes save trigger when save and exit is clicked', function() {
       directiveScope.wizardSave();
       expect(scope.mockSaveTrigger1).toHaveBeenCalledWith(scope.param4);
     });
 
   });
 
-  describe('wizardPrev directive scope function', function(){
-
-    beforeEach(inject(function(){
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2/'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+  describe('wizardPrev directive scope function', function() {
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2/'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
       scope.param5 = 5;
       scope.param6 = 6;
       scope.param7 = 7;
-      scope.mockPrevTrigger2 = function(param5){
-        if(param5 === 6){
+      scope.mockPrevTrigger2 = function(param5) {
+        if (param5 === 6) {
           return true;
         }
         return false;
       };
-      scope.mockNextTrigger2 = function(param6, param7){
-        if(param6 === 7){
+      scope.mockNextTrigger2 = function(param6, param7) {
+        if (param6 === 7) {
           return true;
         }
-        if(param7 === 8){
+        if (param7 === 8) {
           return true;
         }
         return false;
@@ -200,25 +191,25 @@ describe('The Step Wizard directive', function () {
       spyOn(location, 'url').and.callThrough();
       spyOn(location, 'path').and.returnValue('/test-uri-3');
 
-      spyOn(scope,'mockNextTrigger2').and.callThrough();
-      spyOn(scope,'mockPrevTrigger2').and.callThrough();
+      spyOn(scope, 'mockNextTrigger2').and.callThrough();
+      spyOn(scope, 'mockPrevTrigger2').and.callThrough();
 
       compileDirective();
     }));
 
-    it('should disable next button since on the last step', function(){
+    it('should disable next button since on the last step', function() {
       expect(directiveScope.disableNext()).toBe(true);
     });
 
-    it('should not disable prev button since on the last step', function(){
+    it('should not disable prev button since on the last step', function() {
       expect(directiveScope.disablePrev()).toBe(false);
     });
 
-    it('should not go anymore forward since it is on the last step', function(){
+    it('should not go anymore forward since it is on the last step', function() {
       expect(directiveScope.wizardNext()).toBe(false);
     });
 
-    it('should only trigger the prevTrigger function, and not step the user back', function(){
+    it('should only trigger the prevTrigger function, and not step the user back', function() {
       expect(directiveScope.wizardPrev()).toBe(false);
       expect(directiveScope.goToStepURI).toHaveBeenCalledWith(1);
       expect(scope.wizardStepToIndex).toBe(1);
@@ -229,21 +220,17 @@ describe('The Step Wizard directive', function () {
 
   describe('location with trailing slash', function() {
 
-    beforeEach(inject(function(){
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
 
       template = '<step-wizard steps="wizardSteps"></step-wizard>';
 
@@ -251,40 +238,36 @@ describe('The Step Wizard directive', function () {
       compileDirective();
     }));
 
-    it('should disable the prev button since on first step', function(){
+    it('should disable the prev button since on first step', function() {
       expect(directiveScope.disablePrev()).toBe(true);
     });
 
-    it('should not disable the next button since on first step', function(){
+    it('should not disable the next button since on first step', function() {
       expect(directiveScope.disableNext()).toBe(false);
     });
 
-    it('should append a trailing slash to Test label 1\' URI', function () {
+    it('should append a trailing slash to Test label 1\' URI', function() {
       expect(directiveScope.steps[0].uri).toBe('/test-uri-1/');
     });
 
-    it('should not go back since it is at the first step', function(){
+    it('should not go back since it is at the first step', function() {
       expect(directiveScope.wizardPrev()).toBe(false);
     });
 
   });
 
   describe('wizard disabled', function() {
-    beforeEach(inject(function(){
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
       scope.mockDisabled = true;
 
       template = '<step-wizard steps="wizardSteps" disabled="mockDisabled"></step-wizard>';
@@ -293,60 +276,56 @@ describe('The Step Wizard directive', function () {
       compileDirective();
     }));
 
-    it('should not step forward', function(){
+    it('should not step forward', function() {
       expect(directiveScope.wizardNext()).toBe(false);
     });
 
-    it('should not step backwards', function(){
+    it('should not step backwards', function() {
       expect(directiveScope.wizardPrev()).toBe(false);
     });
 
-    it('should not save and exit', function(){
+    it('should not save and exit', function() {
       expect(directiveScope.wizardSave()).toBe(false);
     });
 
-    it('should not step anywhere', function(){
+    it('should not step anywhere', function() {
       expect(directiveScope.goToStepURI(2)).toBe(false);
     });
 
-    it('should disable the next button', function(){
+    it('should disable the next button', function() {
       expect(directiveScope.disableNext()).toBe(true);
     });
 
-    it('should disable the prev button', function(){
+    it('should disable the prev button', function() {
       expect(directiveScope.disablePrev()).toBe(true);
     });
 
-    it('should disable the 1st step button', function(){
+    it('should disable the 1st step button', function() {
       expect(directiveScope.disableStep(0)).toBe(true);
     });
 
-    it('should disable the 2nd step button', function(){
+    it('should disable the 2nd step button', function() {
       expect(directiveScope.disableStep(1)).toBe(true);
     });
 
-    it('should disable the 3rd step button', function(){
+    it('should disable the 3rd step button', function() {
       expect(directiveScope.disableStep(2)).toBe(true);
     });
 
   });
 
   describe('no triggers set', function() {
-    beforeEach(inject(function () {
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
 
       template = '<step-wizard steps="wizardSteps"></step-wizard>';
 
@@ -355,45 +334,44 @@ describe('The Step Wizard directive', function () {
       spyOn(directiveScope, 'nextTrigger').and.returnValue(undefined);
       spyOn(directiveScope, 'prevTrigger').and.returnValue(undefined);
     }));
-    it('should not step forward', function(){
+
+    it('should not step forward', function() {
       expect(directiveScope.wizardNext()).toBe(false);
     });
 
-    it('should not step backwards', function(){
+    it('should not step backwards', function() {
       expect(directiveScope.wizardPrev()).toBe(false);
     });
 
   });
 
-  describe('saveButtonText', function () {
-    beforeEach(inject(function(){
-      scope.wizardSteps = [
-        {
-          label: 'Test label 1',
-          uri: '/test-uri-1'
-        },
-        {
-          label: 'Test label 2',
-          uri: '/test-uri-2'
-        },
-        {
-          label: 'Test label 3',
-          uri: '/test-uri-3'
-        }
-      ];
+  describe('saveButtonText', function() {
+    beforeEach(inject(function() {
+      scope.wizardSteps = [{
+        label: 'Test label 1',
+        uri: '/test-uri-1'
+      }, {
+        label: 'Test label 2',
+        uri: '/test-uri-2'
+      }, {
+        label: 'Test label 3',
+        uri: '/test-uri-3'
+      }];
     }));
 
-    it('should default to Save&Exit when nothing is set', function () {
+    it('should default to Save&Exit when nothing is set', function() {
       template = '<step-wizard steps="wizardSteps"></step-wizard>';
       compileDirective();
       expect(directiveScope.saveButtonText).toEqual('Save & Exit');
     });
-    it('should be set to save-button-text attribute', function () {
+
+    it('should be set to save-button-text attribute', function() {
       scope.mockButtonText = 'fakeButtonText';
       template = '<step-wizard steps="wizardSteps" save-button-text="mockButtonText"></step-wizard>';
       compileDirective();
       expect(directiveScope.saveButtonText).toEqual(scope.mockButtonText);
     });
+
   });
 
 
