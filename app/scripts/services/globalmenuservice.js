61f526fcffca5e6fb9ccd16c3738a6012af95216
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-10-29 15:58:58 -0500

    TSVPORTAL-1039 - temporary commit so I can checkout master anf hotfix error

--- app/scripts/services/globalmenuservice.js
+++ app/scripts/services/globalmenuservice.js
@@ -8,135 +8,15 @@
  * Service in the ts5App.
  */
 angular.module('ts5App')
-  .service('GlobalMenuService', function ($localStorage, $rootScope, $location) {
+  .service('GlobalMenuService', function () {
 
-    //console.log('here');
-    $rootScope.$on("$locationChangeStart", function (event, next, current) {
-      //console.log(next, current);
-      if (!next.contains('login')) {
-        //event.preventDefault();
-      }
-      //$location.path('/login');
-    });
-
-    var userModel = {
-      '1': {
-        id: 1,
-        name: 'Ali Karbassi'
-      },
-      '2': {
-        id: 2,
-        name: 'Rodrigo Curiel'
-      },
-      '3': {
-        id: 3,
-        name: 'Max Felker'
-      }
-    };
-
-    var settingsModel = {
-      '1': {
-        id: 1,
-        name: 'English',
-        shortName: 'en'
-      },
-      '2': {
-        id: 2,
-        name: 'Spanish',
-        shortName: 'es'
-      },
-      '3': {
-        id: 3,
-        name: 'German',
-        shortName: 'de'
-      }
-    };
-
-    // TODO: Should pull from a factory at some point.
-    var companyModel = {
-      '403': {
-        id: 403,
-        name: 'British Airways'
-      },
-      '438': {
-        id: 438,
-        name: 'easyJet'
-      },
-      '413': {
-        id: 413,
-        name: 'Stock Owner'
-      },
-      '407': {
-        id: 407,
-        name: 'GRO 2'
-      }
-    };
-
-    var defaults = {};
-    defaults.user = userModel['1'];
-    defaults.settings = settingsModel['1'];
-
-    // TODO: Set company object instead of just number, need to refactor so ember will be happy
-    defaults.company = 403;
-
-    $localStorage.$default(defaults);
-
-    $rootScope.user = $localStorage.user;
-    $rootScope.settings = $localStorage.settings;
-    $rootScope.company = $localStorage.company;
-
-    // User
-    function getUser() {
-      return $localStorage.user;
-    }
-
-    function setUser(_user_) {
-      var userObj = userModel[_user_];
-
-      $localStorage.user = userObj;
-      $rootScope.user = $localStorage.user;
-
-    }
-
-    // settings
-    function getsettings() {
-      return $localStorage.settings;
-    }
-
-    function setsettings(_settings_) {
-      var settingsObj = settingsModel[_settings_];
-
-      $localStorage.settings = settingsObj;
-      $rootScope.settings = $localStorage.settings;
-
-    }
-
-    // Company
     function getCompany() {
-      return $localStorage.company;
+      return 403;
     }
 
-    function setCompany(companyId) {
-      $localStorage.company = companyId;
-      $rootScope.company = $localStorage.company;
-    }
-
-    // Public API here
     return {
-      'user': {
-        'get': getUser,
-        'set': setUser,
-        'getModel': userModel
-      },
-      'settings': {
-        'get': getsettings,
-        'set': setsettings,
-        'getModel': settingsModel
-      },
-      'company': {
-        'get': getCompany,
-        'set': setCompany,
-        'getModel': companyModel
+      company: {
+        get: getCompany
       }
     };
   });
--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -1,22 +1,69 @@
 'use strict';
 
-describe('Controller: LoginCtrl', function () {
+fdescribe('Controller: LoginCtrl', function () {
 
   // load the controller's module
   beforeEach(module('ts5App'));
 
-  var LoginCtrl,
-    scope;
+  var LoginCtrl;
+  var scope;
+  var identityAccessFactory;
+  var httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector, $q, $httpBackend) {
     scope = $rootScope.$new();
+    identityAccessFactory = $injector.get('identityAccessFactory');
+    httpBackend = $httpBackend;
+
+    spyOn(identityAccessFactory, 'login');
+
     LoginCtrl = $controller('LoginCtrl', {
       $scope: scope
     });
   }));
 
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should have a login function attached to scope', function () {
     expect(scope.login).toBeDefined();
   });
+
+  describe('login process', function () {
+
+    it('should not do anything if form is invalid', function () {
+      scope.loginForm = {
+        $invalid: true
+      };
+      expect(scope.login()).toBeUndefined();
+    });
+
+    it('should not do anything if form is invalid', function () {
+      scope.loginForm = {
+        $invalid: true
+      };
+      expect(scope.login()).toBeUndefined();
+    });
+
+    it('should not do anything if form is invalid', function () {
+      var expectedURL = /dispatch\/store-instances$/;
+      httpBackend.expectGET(expectedURL).respond(200, {});
+
+      scope.credentials = {
+        username: 'fakeUser',
+        password: 'fakePass'
+      };
+
+      scope.loginForm = {
+        $invalid: false
+      };
+
+      expect(scope.login()).toBeUndefined();
+    });
+
+  });
+
 });
