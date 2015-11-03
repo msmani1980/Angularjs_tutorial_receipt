a48031a69cc7165faa437bc7843bdfbb3516a2fd
Max Felker <mfelker@egate-solutions.com>
2015-11-03 13:42:07 -0500

    TSVPORTAL-4137 | Scroll to error dialog implementation

--- app/scripts/directives/error-dialog.js
+++ app/scripts/directives/error-dialog.js
@@ -9,7 +9,7 @@
 angular.module('ts5App')
   .directive('errorDialog', function() {
 
-    var errorDialogController = function($scope) {
+    var errorDialogController = function($scope,$document) {
 
       var $this = this;
       this.form = $scope.$parent[$scope.formObject.$name];
@@ -22,12 +22,70 @@ angular.module('ts5App')
         $this.internalServerError = true;
       };
 
+      this.scrollToDialog = function() {
+        var top = 25;
+        var duration = 2000;
+        $document.scrollTop(top, duration);
+      };
+
+      this.setScrollWatch = function() {
+        $scope.$watch('display',function(currentFlag){
+          if(currentFlag === true) {
+            $this.scrollToDialog();
+          }
+        });
+      };
+
       this.init = function() {
         $scope.errorPattern = [];
         $scope.errorRequired = [];
         $scope.$on('internal-server-error', this.internalServerErrorHandler);
-        $scope.watchForm();
+        this.setScrollWatch();
+        this.watchForm();
       };
+
+      this.validateRequiredFields = function() {
+        $scope.errorRequired = [];
+        angular.forEach(this.form.$error.required, function(field) {
+          if (field.$invalid) {
+            var fieldName = $this.formatErrorText(field.$name);
+            $scope.errorRequired.push(fieldName);
+          }
+        });
+      };
+
+      this.validatePatternFields = function() {
+        $scope.errorPattern = [];
+        angular.forEach(this.form.$error.pattern, function(field) {
+          if (field.$invalid && field.$viewValue) {
+            var fieldName = $this.formatErrorText(field.$name);
+            $scope.errorPattern.push(fieldName);
+          }
+        });
+      };
+
+      this.checkForErrors = function() {
+        this.validateRequiredFields();
+        this.validatePatternFields();
+      };
+
+      this.watchForm = function() {
+        if (this.form && this.form.$error) {
+          var formName = $scope.formObject.$name;
+          var watchGroup = formName + '.$error.pattern + ' + formName + '.$error.required';
+          $scope.$parent.$watchCollection(watchGroup, function() {
+            $this.checkForErrors();
+          });
+          $scope.$parent.$watchCollection(formName + '.$error', function() {
+            var error = $this.form.$error;
+            if (!error.pattern && !error.required) {
+              $scope.$parent.displayError = false;
+            }
+          });
+        }
+      };
+
+      this.init();
 
       $scope.showInternalServerError = function() {
         return $this.internalServerError;
@@ -40,49 +98,6 @@ angular.module('ts5App')
       $scope.showFailedRequest = function() {
         return ( $scope.errorResponse && !$scope.showValidationErrors() && !$scope.showInternalServerError() );
       };
-
-      $scope.validateRequiredFields = function() {
-        $scope.errorRequired = [];
-        angular.forEach($this.form.$error.required, function(field) {
-          if (field.$invalid) {
-            var fieldName = $this.formatErrorText(field.$name);
-            $scope.errorRequired.push(fieldName);
-          }
-        });
-      };
-
-      $scope.validatePatternFields = function() {
-        $scope.errorPattern = [];
-        angular.forEach($this.form.$error.pattern, function(field) {
-          if (field.$invalid && field.$viewValue) {
-            var fieldName = $this.formatErrorText(field.$name);
-            $scope.errorPattern.push(fieldName);
-          }
-        });
-      };
-
-      $scope.checkForErrors = function() {
-        $scope.validateRequiredFields();
-        $scope.validatePatternFields();
-      };
-
-      $scope.watchForm = function() {
-        if ($this.form && $this.form.$error) {
-          var formName = $scope.formObject.$name;
-          var watchGroup = formName + '.$error.pattern + ' + formName + '.$error.required';
-          $scope.$parent.$watchCollection(watchGroup, function() {
-            $scope.checkForErrors();
-          });
-          $scope.$parent.$watchCollection(formName + '.$error', function() {
-            var error = $this.form.$error;
-            if (!error.pattern && !error.required) {
-              $scope.$parent.displayError = false;
-            }
-          });
-        }
-      };
-
-      this.init();
 
     };
     return {
