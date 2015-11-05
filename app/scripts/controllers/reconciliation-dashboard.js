58d68c73b7eb0770bf52a05d99c36187b4c33d7d
Alyssa Reyes <areyes@egate-solutions.com>
2015-11-05 15:28:39 -0600

    TSVPORTAL-4257 | create mock data and add tests

--- app/scripts/controllers/reconciliation-dashboard.js
+++ app/scripts/controllers/reconciliation-dashboard.js
@@ -8,29 +8,19 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('ReconciliationDashboardCtrl', function ($scope, dateUtility) {
+  .controller('ReconciliationDashboardCtrl', function ($scope, dateUtility, reconciliationFactory) {
 
     $scope.viewName = 'Reconciliation Dashboard';
     $scope.search = {};
-    $scope.multiSelectedValues = {};
-
-    $scope.showCreatePopup = function () {
-      angular.element('#reconciliationModal').modal('show');
-    };
-
-    $scope.status = {
-      isopen: false
-    };
 
     $scope.toggleColumnView = function (columnName) {
-      if(angular.isDefined($scope.displayColumns[columnName])) {
+      if (angular.isDefined($scope.displayColumns[columnName])) {
         $scope.displayColumns[columnName] = !$scope.displayColumns[columnName];
       }
     };
 
     $scope.updateOrderBy = function (orderName) {
-      var titleToSet = ($scope.tableSortTitle === orderName) ? ('-' + $scope.tableSortTitle) : (orderName);
-      $scope.tableSortTitle = titleToSet;
+      $scope.tableSortTitle = ($scope.tableSortTitle === orderName) ? ('-' + $scope.tableSortTitle) : (orderName);
     };
 
     $scope.doesInstanceContainAction = function (instance, actionName) {
@@ -41,96 +31,37 @@ angular.module('ts5App')
     };
 
     $scope.getArrowType = function (orderName) {
-      if ( $scope.tableSortTitle === orderName) {
+      if ($scope.tableSortTitle === orderName) {
         return 'ascending';
-      } else if ( $scope.tableSortTitle === '-' + orderName) {
+      } else if ($scope.tableSortTitle === '-' + orderName) {
         return 'descending';
       }
       return 'none';
     };
 
-    function initColumns () {
+    function getReconciliationList() {
+      reconciliationFactory.getMockReconciliationDataList().then(function (dataFromAPI) {
+        $scope.reconciliationList = angular.copy(dataFromAPI);
+      });
+    }
+
+    function init() {
+      $scope.tableSortTitle = 'dispatchedStation';
       $scope.displayColumns = {
         receivedStation: false,
         storeInstanceId: false,
         updatedDate: false,
         updatedBy: false
-      }
-    }
-
-    function init() {
-      $scope.tableSortTitle = 'dispatchedStation';
-      initColumns();
+      };
 
       $scope.search = {
         scheduleStartDate: dateUtility.dateNumDaysBeforeTodayFormatted(10),
         scheduleEndDate: dateUtility.dateNumDaysBeforeTodayFormatted(2)
       };
 
-      $scope.reconciliationList = [
-        {
-          dispatchedStation: 'LGW',
-          receivedStation: 'LTN',
-          storeNumber: '7321',
-          storeInstanceId: 91,
-          scheduleDate: '10/08/2015',
-          ePOSStatus: 'No',
-          postTripStatus: '3/3',
-          cashHandlerStatus: '4/4',
-          ePOSCreatedStore: 'Yes',
-          status: 'Inbounded',
-          updatedDate: '9/10/2015 4:30',
-          updatedBy: 'rabraham',
-          actions: ['Validate', 'Report']
-        },
-        {
-          dispatchedStation: 'LTN',
-          receivedStation: 'LGW',
-          storeNumber: '123',
-          storeInstanceId: 54,
-          scheduleDate: '12/08/2015',
-          ePOSStatus: '3/3',
-          postTripStatus: '2/3',
-          cashHandlerStatus: '4/6',
-          ePOSCreatedStore: 'No',
-          status: 'Confirmed',
-          updatedDate: '11/10/2015 4:30',
-          updatedBy: 'rabraham',
-          actions: ['Review', 'Pay Commission', 'Unconfirm', 'Report']
-
-        },
-        {
-          dispatchedStation: 'STN',
-          receivedStation: 'ORD',
-          storeNumber: '7325',
-          storeInstanceId: 103,
-          scheduleDate: '11/21/2015',
-          ePOSStatus: 'No',
-          postTripStatus: '3/3',
-          cashHandlerStatus: '4/4',
-          ePOSCreatedStore: 'Yes',
-          status: 'Discrepancies',
-          updatedDate: '9/30/2015 4:30',
-          updatedBy: 'tgunderson',
-          actions: ['Review', 'Confirm', 'Report']
-        },
-        {
-          dispatchedStation: 'LHR',
-          receivedStation: 'GVA',
-          storeNumber: '1132456',
-          storeInstanceId: 91,
-          scheduleDate: '08/10/2015',
-          ePOSStatus: 'No',
-          postTripStatus: '1/4',
-          cashHandlerStatus: '4/5',
-          ePOSCreatedStore: 'No',
-          status: 'Commission Paid',
-          updatedDate: '7/13/2015 4:30',
-          updatedBy: 'tgunderson',
-          actions: ['Report']
-        }
-      ];
+      getReconciliationList();
     }
+
     init();
 
   });
--- test/spec/controllers/reconciliation-dashboard.js
+++ test/spec/controllers/reconciliation-dashboard.js
@@ -5,19 +5,108 @@ describe('Controller: ReconciliationDashboardCtrl', function () {
   // load the controller's module
   beforeEach(module('ts5App'));
 
-  var ReconciliationDashboardCtrl,
-    scope;
+  var ReconciliationDashboardCtrl;
+  var scope;
+  var location;
+  var reconciliationFactory;
+  var controller;
+  var reconciliationListDeferred;
+  var reconciliationListResponseJSON;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $q, $location, $injector) {
+    location = $location;
     scope = $rootScope.$new();
+
+    reconciliationFactory = $injector.get('reconciliationFactory');
+    controller = $controller;
+
+    reconciliationListResponseJSON = [{id: 1}]; // stub for now until API is complete
+    reconciliationListDeferred = $q.defer();
+    reconciliationListDeferred.resolve(reconciliationListResponseJSON);
+    spyOn(reconciliationFactory,  'getMockReconciliationDataList').and.returnValue(reconciliationListDeferred.promise);
+
+
     ReconciliationDashboardCtrl = $controller('ReconciliationDashboardCtrl', {
       $scope: scope
-      // place here mocked dependencies
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(ReconciliationDashboardCtrl.awesomeThings.length).toBe(3);
+
+  describe('init', function () {
+    it('should call get LMP stock data', function () {
+      expect(reconciliationFactory.getMockReconciliationDataList).toHaveBeenCalled();
+      scope.$digest();
+      expect(scope.reconciliationList).toBeDefined();
+    });
+
+    it('should init displayColumns with receivedStation, storeInstanceId, updatedDate, updatedBy columns to be hidden', function () {
+      expect(scope.displayColumns).toBeDefined();
+      expect(scope.displayColumns.receivedStation).toEqual(false);
+      expect(scope.displayColumns.storeInstanceId).toEqual(false);
+      expect(scope.displayColumns.updatedDate).toEqual(false);
+      expect(scope.displayColumns.updatedBy).toEqual(false);
+    });
+
   });
+
+  describe('table helper functions', function () {
+    describe('update orderBy', function () {
+      it('should change sort title to given title', function () {
+        scope.tableSortTitle = 'oldSortName';
+        scope.updateOrderBy('newSortName', true);
+        expect(scope.tableSortTitle).toEqual('newSortName');
+      });
+
+      it('should reverse sort direction if already sorting on given name', function () {
+        scope.tableSortTitle = 'sortName';
+        scope.updateOrderBy('sortName', true);
+        expect(scope.tableSortTitle).toEqual('-sortName');
+      });
+    });
+
+    describe('getArrowType', function () {
+      it('should return descending if sort title is negative', function () {
+        scope.tableSortTitle = '-sortName';
+        var arrowType = scope.getArrowType('sortName');
+        expect(arrowType).toEqual('descending');
+      });
+      it('should return ascending if sort title is not negative', function () {
+        scope.tableSortTitle = 'sortName';
+        var arrowType = scope.getArrowType('sortName');
+        expect(arrowType).toEqual('ascending');
+      });
+      it('should return none if sort title is not the given name', function () {
+        scope.tableSortTitle = 'sortName';
+        var arrowType = scope.getArrowType('otherName');
+        expect(arrowType).toEqual('none');
+      });
+    });
+
+    describe('toggleColumn', function () {
+      it('should reverse column visibility', function () {
+        scope.displayColumns = {testColumn: false};
+        scope.toggleColumnView('testColumn');
+        expect(scope.displayColumns.testColumn).toEqual(true);
+        scope.toggleColumnView('testColumn');
+        expect(scope.displayColumns.testColumn).toEqual(false);
+      });
+    });
+
+    describe('doesInstanceContainAction', function () {
+      it('should return true if instance contains an action', function () {
+        var testInstance = {actions: ['testAction']};
+        expect(scope.doesInstanceContainAction(testInstance, 'testAction')).toEqual(true);
+      });
+      it('should return false if instance contains an action', function () {
+        var testInstance = {actions: ['testAction']};
+        expect(scope.doesInstanceContainAction(testInstance, 'fakeAction')).toEqual(false);
+      });
+      it('should return false if instance does not contain actions', function () {
+        var testInstance = {};
+        expect(scope.doesInstanceContainAction(testInstance, 'testAction')).toEqual(false);
+      });
+    });
+  });
+
 });
