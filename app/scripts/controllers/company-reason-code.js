230a48e701c0b690d0b46548a063b58ea1b7f021
Max Felker <mfelker@egate-solutions.com>
2015-11-10 13:42:02 -0500

    Revert "TSVPORTAL-4153 | Removed company reason code controller and view"
    This reverts commit da15a1c97a0ee22f05b036256d0a84e4ba2f4cab.

---
+++ app/scripts/controllers/company-reason-code.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ts5App.controller:PostFlightDataCtrl
+ * @description
+ * # PostFlightDataCtrl
+ * Controller of the ts5App
+ */
+angular.module('ts5App')
+  .controller('CompanyReasonCodeCtrl', function ($scope) {
+    $scope.viewName = 'Company Reason Code';
+  });
