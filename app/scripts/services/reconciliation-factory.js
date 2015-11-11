85294de7ac0289b2e9d17fd16cccb5a2f999dcaf
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-11-11 10:31:02 -0600

    TSVPORTAL-3690 - added call to getStoreInstance in factory

--- app/scripts/services/reconciliation-factory.js
+++ app/scripts/services/reconciliation-factory.js
@@ -8,40 +8,41 @@
  * Factory in the ts5App.
  */
 angular.module('ts5App')
-  .factory('reconciliationFactory', function ($q) {
+  .factory('reconciliationFactory', function ($q, storeInstanceService) {
 
     var getLMPStockMockData = function () {
-      var mockLMPData = [{
-        itemName: 'Chocolate',
-        itemDescription: 'Food: Chocolate',
-        dispatchedCount: 50,
-        inboundCount: 50,
-        ePOSSales: 20,
-        varianceQuantity: 0,
-        retailValue: 5,
-        varianceValue: 7.0,
-        isDiscrepancy: true
-      }, {
-        itemName: 'Pepsi',
-        itemDescription: 'Drink: Pepsi',
-        dispatchedCount: 150,
-        inboundCount: 30,
-        ePOSSales: 25,
-        varianceQuantity: 11,
-        retailValue: 6,
-        varianceValue: 12.0,
-        isDiscrepancy: false
-      }, {
-        itemName: 'Coke',
-        itemDescription: 'Drink: Coke',
-        dispatchedCount: 20,
-        inboundCount: 35,
-        ePOSSales: 12,
-        varianceQuantity: 0,
-        retailValue: 5,
-        varianceValue: 14.0,
-        isDiscrepancy: false
-      }];
+      var mockLMPData = [
+        {
+          itemName: 'Chocolate',
+          itemDescription: 'Food: Chocolate',
+          dispatchedCount: 50,
+          inboundCount: 50,
+          ePOSSales: 20,
+          varianceQuantity: 0,
+          retailValue: 5,
+          varianceValue: 7.0,
+          isDiscrepancy: true
+        }, {
+          itemName: 'Pepsi',
+          itemDescription: 'Drink: Pepsi',
+          dispatchedCount: 150,
+          inboundCount: 30,
+          ePOSSales: 25,
+          varianceQuantity: 11,
+          retailValue: 6,
+          varianceValue: 12.0,
+          isDiscrepancy: false
+        }, {
+          itemName: 'Coke',
+          itemDescription: 'Drink: Coke',
+          dispatchedCount: 20,
+          inboundCount: 35,
+          ePOSSales: 12,
+          varianceQuantity: 0,
+          retailValue: 5,
+          varianceValue: 14.0,
+          isDiscrepancy: false
+        }];
 
       var LMPStockMockResponseDeferred = $q.defer();
       LMPStockMockResponseDeferred.resolve(mockLMPData);
@@ -49,29 +50,30 @@ angular.module('ts5App')
     };
 
     var getCashBagMockData = function () {
-      var mockCashBag = [{
-        cashBag: 'CB123',
-        currency: 'GBP',
-        exchangeAmount: 12,
-        crewAmount: 14,
-        paperAmount: 34,
-        coinAmount: 12.00,
-        varianceValue: 5,
-        bankExchangeRate: 7.0,
-        totalBand: 100,
-        isDiscrepancy: true
-      }, {
-        cashBag: 'CB345',
-        currency: 'GBP',
-        exchangeAmount: 15,
-        crewAmount: 34,
-        paperAmount: 23,
-        coinAmount: 15.00,
-        varianceValue: 12,
-        bankExchangeRate: 16.0,
-        totalBand: 200,
-        isDiscrepancy: false
-      }];
+      var mockCashBag = [
+        {
+          cashBag: 'CB123',
+          currency: 'GBP',
+          exchangeAmount: 12,
+          crewAmount: 14,
+          paperAmount: 34,
+          coinAmount: 12.00,
+          varianceValue: 5,
+          bankExchangeRate: 7.0,
+          totalBand: 100,
+          isDiscrepancy: true
+        }, {
+          cashBag: 'CB345',
+          currency: 'GBP',
+          exchangeAmount: 15,
+          crewAmount: 34,
+          paperAmount: 23,
+          coinAmount: 15.00,
+          varianceValue: 12,
+          bankExchangeRate: 16.0,
+          totalBand: 200,
+          isDiscrepancy: false
+        }];
 
       var cashBagMockResponseDeferred = $q.defer();
       cashBagMockResponseDeferred.resolve(mockCashBag);
@@ -147,7 +149,12 @@ angular.module('ts5App')
       return reconciliationListMockResponseDeferred.promise;
     };
 
+    function getStoreInstance(storeInstanceId) {
+      return storeInstanceService.getStoreInstance(storeInstanceId);
+    }
+
     return {
+      getStoreInstance: getStoreInstance,
       getLMPStockMockData: getLMPStockMockData,
       getCashBagMockData: getCashBagMockData,
       getMockReconciliationDataList: getMockReconciliationDataList
--- test/spec/services/reconciliation-factory.js
+++ test/spec/services/reconciliation-factory.js
@@ -1,21 +1,40 @@
 'use strict';
 
-describe('Factory: reconciliationFactory', function () {
+fdescribe('Factory: reconciliationFactory', function () {
 
   beforeEach(module('ts5App'));
+  beforeEach(module('served/store-instance.json'));
+
 
   var reconciliationFactory;
-  var rootScope;
-  var scope;
+  var storeInstanceService;
+  var getStoreInstanceDeferred;
+  var storeInstanceJSON;
 
-  beforeEach(inject(function ($rootScope, _reconciliationFactory_) {
-    rootScope = $rootScope;
-    scope = $rootScope.$new();
+  beforeEach(inject(function (_reconciliationFactory_, $injector, $q) {
+    inject(function (_servedStoreInstance_) {
+      storeInstanceJSON = _servedStoreInstance_;
+    });
+
+    storeInstanceService = $injector.get('storeInstanceService');
+
+    getStoreInstanceDeferred = $q.defer();
+    getStoreInstanceDeferred.resolve(storeInstanceJSON);
+    spyOn(storeInstanceService, 'getStoreInstance').and.returnValue(getStoreInstanceDeferred.promise);
+
     reconciliationFactory = _reconciliationFactory_;
   }));
 
   it('should be defined', function () {
     expect(!!reconciliationFactory).toBe(true);
+  });
+
+  describe('API calls', function () {
+    it('should call storeInstanceService on getStoreInstance', function () {
+      var storeInstanceId = 'fakeStoreInstanceId';
+      reconciliationFactory.getStoreInstance(storeInstanceId);
+      expect(storeInstanceService.getStoreInstance).toHaveBeenCalledWith(storeInstanceId);
+    });
   });
 
   describe('mock API calls', function () {
