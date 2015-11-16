ec6ca4c860286f4c580b2ffb8385757a8afe4050
Vinko Buble <vbuble@egate-solutions.com>
2015-11-16 10:05:50 -0500

    TSVPORTAL-382 PR changes - clean code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,25 +10,54 @@ angular.module('ts5App')
   .controller('MainCtrl', function ($rootScope, $scope, companiesFactory, mainMenuService, identityAccessService, lodash) {
 
     $scope.viewName = 'TS5 Dashboard';
+
+    function checkMenuItemMatchesFeaturePermission(featurePermission, menuItemPermission) {
+      return featurePermission.resource.apiName === menuItemPermission.apiName &&
+        lodash.intersection(featurePermission.permissionCode, menuItemPermission.permissionCodes).length === menuItemPermission.permissionCodes.length;
+    }
+    function checkMenuItemHasFeaturePermissions(permissions, featurePermission) {
+      return lodash.find(permissions, function(menuItemPermission) {
+        return checkMenuItemMatchesFeaturePermission(featurePermission, menuItemPermission);
+      });
+    }
+
+    function findPackageWithMatchingMatchingMenuItem(menuItem, featurePermissions) {
+      return lodash.find(featurePermissions, function (featurePermission) {
+        return checkMenuItemHasFeaturePermissions(menuItem.permissions, featurePermission);
+      });
+    }
+
+    function hasMenuItemMatchingPackageWithPermissions(menuItem, featurePermissions) {
+      if (menuItem.permissions) {
+        return findPackageWithMatchingMatchingMenuItem(menuItem, featurePermissions);
+      }
+      return true;
+    }
+
+    function hasResponseMatchingMenuItemWithPermissions(menuItem, response) {
+      if (response[menuItem.package] && response[menuItem.package][menuItem.role]) {
+        return hasMenuItemMatchingPackageWithPermissions(menuItem, response[menuItem.package][menuItem.role]);
+      }
+      return false;
+
+    }
+
+    function menuItemsWithFeaturePermissions(menuItems, response) {
+      return lodash.filter(menuItems, function(menuItem) {
+        return hasResponseMatchingMenuItemWithPermissions(menuItem, response);
+      });
+    }
+
+    function menuWithFeaturePermissions(menu, response) {
+      return lodash.filter(menu, function(item) {
+        item.menuItems = menuItemsWithFeaturePermissions(item.menuItems, response);
+        return item.menuItems.length !== 0;
+      });
+    }
+
     function updateNavigationPerUserFeatures() {
       identityAccessService.featuresInRole().then(function(response) {
-        $scope.dashboardMenu = lodash.filter(mainMenuService.getMenu(), function(item) {
-          item.menuItems = lodash.filter(item.menuItems, function(menuItem) {
-            if (response[menuItem.package] && response[menuItem.package][menuItem.role]) {
-              if (menuItem.permissions) {
-                return lodash.find(response[menuItem.package][menuItem.role], function (featurePermission) {
-                  return lodash.find(menuItem.permissions, function (menuItemPermission) {
-                    return featurePermission.resource.apiName === menuItemPermission.apiName &&
-                    lodash.intersection(featurePermission.permissionCode, menuItemPermission.permissionCodes).length === menuItemPermission.permissionCodes.length;
-                  });
-                });
-              }
-              return true;
-            }
-            return false;
-          });
-          return item.menuItems.length !== 0;
-        });
+        $scope.dashboardMenu = menuWithFeaturePermissions(mainMenuService.getMenu(), response);
       });
     }
 
