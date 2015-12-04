8484d0e3bfc70879ec8c79e4f4f71fb4b5b2bd96
Jared Donisvitch <jared@bigroomstudios.com>
2015-12-04 10:35:13 -0500

    TSVPORTAL-4525 | resolve delete and duplicate issues

--- app/scripts/controllers/company-exchange-rate-edit.js
+++ app/scripts/controllers/company-exchange-rate-edit.js
@@ -71,10 +71,10 @@ angular.module('ts5App')
         .join(', ');
     };
 
-    this.formatPayloadForSearch = function () {
+    this.formatPayloadForSearch = function() {
       var searchPayload = angular.copy($scope.search);
       searchPayload = payloadUtility.serializeDates(searchPayload);
-      if(searchPayload.acceptedCurrencies) {
+      if (searchPayload.acceptedCurrencies) {
         delete searchPayload.acceptedCurrencies;
       }
       return searchPayload;
@@ -285,14 +285,12 @@ angular.module('ts5App')
     $scope.showDeleteConfirmation = function(index, exchangeRate) {
       $scope.exchangeRateToDelete = exchangeRate;
       $scope.exchangeRateToDelete.rowIndex = index;
-
       angular.element('.delete-warning-modal').modal('show');
     };
 
     $scope.deleteCompanyExchangeRate = function() {
       angular.element('.delete-warning-modal').modal('hide');
-      angular.element('#exchange-rate-' + $scope.exchangeRateToDelete.rowIndex).remove();
-
+      $scope.companyExchangeRates.splice($scope.exchangeRateToDelete.rowIndex, 1);
       $this.deleteCompanyExchangeRate($scope.exchangeRateToDelete.id);
     };
 
@@ -312,7 +310,7 @@ angular.module('ts5App')
       var newExchangeRates = angular.copy($scope.companyExchangeRates);
       newExchangeRates.push(newExchangeRate);
       $scope.companyExchangeRates = $filter('orderBy')(newExchangeRates,
-        'acceptedCurrencyCode + exchangeRate + startDate');
+        'acceptedCurrencyCode +  exchangeRate + startDate');
     };
 
     this.init = function() {
