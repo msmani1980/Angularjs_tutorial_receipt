b3a656eb39a6a02d6f2315d2af15c932819fb4bb
Alyssa Reyes <areyes@egate-solutions.com>
2015-12-07 15:57:45 -0600

    TSVPORTAL-4536 | integrate mock data

---
+++ app/scripts/services/store-instance-amend-factory.js
@@ -0,0 +1,125 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ts5App.storeInstanceAmendFactory
+ * @description
+ * # storeInstanceAmendFactory
+ * Factory in the ts5App.
+ */
+angular.module('ts5App')
+  .factory('storeInstanceAmendFactory', function ($q) {
+
+    var getCashBagListMockData = function () {
+      var mockCashBag = [{
+        cashBagNumber: '123',
+        bankRefNumber: 'AB45',
+        isManual: true,
+        isDeleted: false,
+        isVerified: true,
+        regularItemSales: 20.00,
+        virtualItemSales: 10.00,
+        voucherItemSales: 15.00,
+        promotionDiscounts: 5.00,
+        cashRevenue: 10.00,
+        creditRevenue: 5.00,
+        discountRevenue: 11.50,
+        flightSectors: [{
+          scheduleDate: '10/20/2015',
+          scheduleNumber: '123',
+          departureStationCode: 'ORD',
+          arrivalStationCode: 'LAX',
+          passengerCount: 123,
+          tailNumber: 'ABC-123',
+          transactionsCount: 7,
+          transactionTotal: 15.00,
+          isManual: false,
+          crewData: [{
+            crewId: 1,
+            firstName: 'John',
+            lastName: 'Smith'
+          }, {
+            crewId: 2,
+            firstName: 'Mary',
+            lastName: 'Jane'
+          }]
+        }, {
+          scheduleDate: '10/24/2015',
+          scheduleNumber: '105',
+          departureStationCode: 'LAX',
+          arrivalStationCode: 'ORD',
+          passengerCount: 123,
+          tailNumber: 'GAD-123',
+          transactionsCount: 3,
+          transactionTotal: 11.00,
+          isManual: true,
+          crewData: [{
+            crewId: 1,
+            firstName: 'John',
+            lastName: 'Smith'
+          }, {
+            crewId: 2,
+            firstName: 'Mary',
+            lastName: 'Jane'
+          }]
+        }]
+      }, {
+        isManual: true,
+        isDeleted: false,
+        regularItemSales: 5.00,
+        virtualItemSales: 15.00,
+        voucherItemSales: 10.00,
+        promotionDiscounts: 5.00,
+        cashRevenue: 15.00,
+        creditRevenue: 10.00,
+        discountRevenue: 5.00,
+        flightSectors: [{
+          scheduleDate: '11/20/2015',
+          scheduleNumber: '145',
+          departureStationCode: 'SAN',
+          arrivalStationCode: 'GNV',
+          passengerCount: 123,
+          tailNumber: 'ABC-123',
+          transactionsCount: 7,
+          transactionTotal: 15.00,
+          isManual: false,
+          crewData: [{
+            crewId: 1,
+            firstName: 'John',
+            lastName: 'Smith'
+          }, {
+            crewId: 2,
+            firstName: 'Mary',
+            lastName: 'Jane'
+          }]
+        }, {
+          scheduleDate: '12/30/2015',
+          scheduleNumber: '105',
+          departureStationCode: 'CPH',
+          arrivalStationCode: 'ORD',
+          passengerCount: 123,
+          tailNumber: 'GAD-123',
+          transactionsCount: 3,
+          transactionTotal: 11.00,
+          isManual: true,
+          crewData: [{
+            crewId: 1,
+            firstName: 'John',
+            lastName: 'Smith'
+          }, {
+            crewId: 2,
+            firstName: 'Mary',
+            lastName: 'Jane'
+          }]
+        }]
+      }];
+
+      var cashBagMockResponseDeferred = $q.defer();
+      cashBagMockResponseDeferred.resolve(mockCashBag);
+      return cashBagMockResponseDeferred.promise;
+    };
+
+    return {
+      getCashBagListMockData: getCashBagListMockData
+    };
+  });
