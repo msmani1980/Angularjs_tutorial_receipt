4295feb6c830fd372f20635fe96a52c6978e3ee5
Alyssa Reyes <areyes@egate-solutions.com>
2015-12-22 13:19:42 -0600

    TSVPORTAL-4618 | fix filters and tests

--- app/scripts/controllers/stock-dashboard.js
+++ app/scripts/controllers/stock-dashboard.js
@@ -67,7 +67,7 @@ angular.module('ts5App').controller('StockDashboardCtrl',
     this.setExportURL = function(cateringStation) {
       $scope.exportURL = ENV.apiUrl + '/api/stock-management/dashboard/' + cateringStation.id;
       var sessionToken = identityAccessFactory.getSessionObject().sessionToken;
-      $scope.exportURL += '/file/export?sortBy=itemName&sessionToken=' + sessionToken;
+      $scope.exportURL += '/file/export?sortOn=itemName&sessionToken=' + sessionToken;
     };
 
     this.generateStockTakeQuery = function() {
