707d476f8e9700358c64540f8200425c1b4f981a
Rodrigo Curiel <rcuriel@egate-solutions.com>
2016-01-04 11:26:13 -0600

    TSVPORTAL-4711 - removed unused parameter

--- app/scripts/controllers/promotions.js
+++ app/scripts/controllers/promotions.js
@@ -8,7 +8,7 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('PromotionsCtrl', function ($scope, $location, $routeParams, $q, $filter, $timeout,
+  .controller('PromotionsCtrl', function ($scope, $location, $routeParams, $q, $filter,
                                           promotionsFactory, dateUtility) {
 
     $scope.readOnly = true;
