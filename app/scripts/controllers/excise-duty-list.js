e4b5c98ae598a487bbc9086ff4234996a72e7c31
Alyssa Reyes <areyes@egate-solutions.com>
2016-02-05 11:55:20 -0600

    TSVPORTAL-4982 | resolve country and volume

--- app/scripts/controllers/excise-duty-list.js
+++ app/scripts/controllers/excise-duty-list.js
@@ -7,7 +7,7 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('ExciseDutyListCtrl', function ($scope, exciseDutyFactory, lodash) {
+  .controller('ExciseDutyListCtrl', function ($scope, exciseDutyFactory, dateUtility, lodash, $q) {
     $scope.viewName = 'Excise Duty List';
     $scope.companyGlobalCurrency = 'GBP';
     var $this = this;
@@ -17,8 +17,12 @@ angular.module('ts5App')
       offset: 0
     };
 
-    function formatSearchPayloadForAPI () {
-      return {};
+    function showLoadingModal(text) {
+      angular.element('#loading').modal('show').find('p').text(text);
+    }
+
+    function hideLoadingModal() {
+      angular.element('#loading').modal('hide');
     }
 
     $scope.shouldShowSearchPrompt = function () {
@@ -48,6 +52,24 @@ angular.module('ts5App')
       $scope.getExciseDutyList();
     };
 
+    function formatSearchPayloadForAPI() {
+      return {};
+    }
+
+    function formatExciseDutyResponseForApp(dataFromAPI) {
+      var newExciseDutyList = angular.copy(dataFromAPI.response);
+      angular.forEach(newExciseDutyList, function (exciseDuty) {
+        exciseDuty.startDate = dateUtility.formatDateForApp(exciseDuty.startDate);
+        exciseDuty.endDate = dateUtility.formatDateForApp(exciseDuty.endDate);
+        var countryMatch = lodash.findWhere($scope.countryList, { id: exciseDuty.countryId });
+        var volumeMatch = lodash.findWhere($scope.volumeUnits, { id: exciseDuty.volumeUnitId });
+        exciseDuty.countryName = (angular.isDefined(countryMatch)) ? countryMatch.countryName : '';
+        exciseDuty.volumeUnit = (angular.isDefined(volumeMatch)) ? volumeMatch.unitName : '';
+      });
+
+      $scope.exciseDutyList = newExciseDutyList;
+    }
+
     $scope.getExciseDutyList = function () {
       if ($this.meta.offset >= $this.meta.count) {
         return;
@@ -65,14 +87,24 @@ angular.module('ts5App')
       payload.limit = $this.meta.limit;
       payload.offset = $this.meta.offset;
 
-      exciseDutyFactory.getExciseDutyList(payload).then(function (dataFromAPI) {
-        $scope.exciseDutyList = angular.copy(dataFromAPI.response);
-      });
+      exciseDutyFactory.getExciseDutyList(payload).then(formatExciseDutyResponseForApp);
 
       $this.meta.offset += $this.meta.limit;
     };
 
-    function init () {
+    function completeInit(responseArray) {
+      $scope.countryList = responseArray[0].countries;
+      $scope.volumeUnits = responseArray[1].units;
+      hideLoadingModal();
+    }
+
+    function init() {
+      showLoadingModal('initializing');
+      var promises = [
+        exciseDutyFactory.getCountriesList(),
+        exciseDutyFactory.getVolumeUnits()
+      ];
+      $q.all(promises).then(completeInit);
     }
 
     init();
