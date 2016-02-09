e9c0fe4d69b28013fe2d7a37dafe274710e374ea
Damir Palinic <damir@bigroomstudios.com>
2016-02-09 19:47:34 +0100

    TSVPORTAL-5024 - Added some tests

--- test/spec/controllers/transaction-list.js
+++ test/spec/controllers/transaction-list.js
@@ -12,6 +12,7 @@ describe('Controller: TransactionListCtrl', function () {
 
   var TransactionListCtrl,
     scope,
+    transactionFactory,
     transactionsJSON,
     transactionTypesJSON,
     companyCurrenciesJSON,
@@ -24,7 +25,7 @@ describe('Controller: TransactionListCtrl', function () {
     getCreditCardTypesDeferred;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $q, transactionFactory, recordsService, currencyFactory, stationsService, companyCcTypesService) {
+  beforeEach(inject(function ($controller, $rootScope, $q, _transactionFactory_, recordsService, currencyFactory, stationsService, companyCcTypesService) {
     inject(function (_servedTransactions_, _servedTransactionTypes_, _servedCompanyCurrencyGlobals_, _servedCompanyStationGlobals_, _servedCompanyCcTypes_) {
       transactionsJSON = _servedTransactions_;
       transactionTypesJSON = _servedTransactionTypes_;
@@ -32,6 +33,8 @@ describe('Controller: TransactionListCtrl', function () {
       companyStationsJSON = _servedCompanyStationGlobals_;
       companyCreditCardTypesJSON = _servedCompanyCcTypes_;
     });
+
+    transactionFactory = _transactionFactory_;
 
     scope = $rootScope.$new();
     getTransactionListDeferred = $q.defer();
@@ -63,5 +66,106 @@ describe('Controller: TransactionListCtrl', function () {
     expect(scope.viewName).toBe('Transactions');
   });
 
+  describe('search.paymentMethods change will ', function() {
+    it('set isCreditCardPaymentSelected to false in case credit card is not selected', function () {
+      scope.search = {
+        paymentMethods: ['Cash']
+      };
+      scope.$digest();
 
+      expect(scope.isCreditCardPaymentSelected).toBe(false);
+    });
+
+    it('set isCreditCardPaymentSelected to true in case credit card is selected', function () {
+      scope.search = {
+        paymentMethods: ['Cash']
+      };
+
+      scope.search.paymentMethods.push('Credit Card');
+      scope.$digest();
+
+      expect(scope.isCreditCardPaymentSelected).toBe(true);
+    });
+
+    it('reset credit card search fields in case credit card is deselected', function () {
+      scope.search = {
+        paymentMethods: ['Cash', 'Credit Card'],
+        cardHolderName: 'Card Holder',
+        cardTypes: 'Visa',
+        lastFour: '1234',
+        ccTransactionStatuses: 'New',
+        ccAuthorizationStatuses: 'Approved'
+      };
+      scope.$digest();
+
+      scope.search.paymentMethods.pop();
+      scope.$digest();
+
+      var expectedSearch = scope.search = {
+        paymentMethods: ['Cash'],
+        cardHolderName: null,
+        cardTypes: null,
+        lastFour: null,
+        ccTransactionStatuses: null,
+        ccAuthorizationStatuses: null
+      };
+
+      expect(scope.search).toBe(expectedSearch);
+    });
+  });
+
+  it('toggleColumnView will set field view value to true', function () {
+    scope.toggleColumnView('storeInstance');
+
+    expect(scope.displayColumns.storeInstance).toBe(true);
+  });
+
+  describe('getOverriddenTransactionTypeName will', function () {
+    it('return overridden name if overridden name is defined', function () {
+      expect(scope.getOverriddenTransactionTypeName('CLEARED')).toBe('Cleared');
+    });
+    it('return default name if overridden name is not defined', function () {
+      expect(scope.getOverriddenTransactionTypeName('UNKNOWN')).toBe('UNKNOWN');
+    });
+  });
+
+  describe('getTransactions will', function () {
+    it('call getTransactionList if offset is not greater than count', function () {
+      scope.getTransactions();
+
+      expect(transactionFactory.getTransactionList).toHaveBeenCalled();
+    });
+    it('update meta offset', function () {
+      var offset = TransactionListCtrl.meta.offset;
+      scope.getTransactions();
+
+      expect(TransactionListCtrl.meta.offset).toBe(offset + 100);
+    });
+  });
+
+  describe('clearSearch will', function () {
+    it('clear search object', function () {
+      scope.search = {
+        storeInstanceId: 1
+      };
+
+      scope.clearSearch();
+      expect(scope.search).toEqual({});
+    });
+    it('call getTransactionList method', function () {
+      scope.clearSearch();
+      expect(transactionFactory.getTransactionList).toHaveBeenCalled();
+    });
+  });
+
+  describe('searchTransactions will', function () {
+    it('set isSearch to true', function () {
+      scope.searchTransactions();
+      expect(TransactionListCtrl.isSearch).toBe(true);
+    });
+    it('call getTransactionList method', function () {
+      scope.clearSearch();
+      expect(transactionFactory.getTransactionList).toHaveBeenCalled();
+    });
+  });
 });
