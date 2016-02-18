34536c95af9a47e4858483e10e7a67c84b519f00
Alyssa Reyes <areyes@egate-solutions.com>
2016-02-18 11:32:27 -0600

    TSVPORTAL-5284 | fix test

--- test/spec/controllers/excise-duty-relationship-list.js
+++ test/spec/controllers/excise-duty-relationship-list.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: ExciseDutyRelationshipListCtrl', function () {
+fdescribe('Controller: ExciseDutyRelationshipListCtrl', function () {
 
   beforeEach(module('ts5App'));
   beforeEach(module('template-module'));
@@ -402,7 +402,7 @@ describe('Controller: ExciseDutyRelationshipListCtrl', function () {
           scope.exciseDutyList = [{id: 1}];
           scope.clearSearchForm();
           scope.$digest();
-          expect(scope.search).toEqual(null);
+          expect(scope.search).toEqual({});
           expect(scope.itemExciseDutyList).toEqual(null);
         });
       });
