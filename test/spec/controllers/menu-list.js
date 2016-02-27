9b0172bc37ae2744e27ea736e14f5a65eb9e5ba6
Jared Donisvitch <jared@bigroomstudios.com>
2016-02-26 19:16:01 -0500

    TSVPORTAL-4144 | fix for edit

--- test/spec/controllers/menu-list.js
+++ test/spec/controllers/menu-list.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*global moment*/
-describe('Controller: MenuListCtrl', function () {
+describe('Controller: MenuListCtrl', function() {
 
   // load the controller's module
   beforeEach(module('ts5App'));
@@ -15,16 +15,18 @@ describe('Controller: MenuListCtrl', function () {
   var location;
   var deleteMenuDeffered;
   var httpBackend;
+  var dateUtility;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($q, $controller, $rootScope, _menuService_, $httpBackend,
+  beforeEach(inject(function($q, $controller, $rootScope, $injector, _menuService_, $httpBackend,
     $location) {
-    inject(function (_servedMenus_) {
+    inject(function(_servedMenus_) {
       menuListJSON = _servedMenus_;
     });
 
     httpBackend = $httpBackend;
     location = $location;
+    dateUtility = $injector.get('dateUtility');
     scope = $rootScope.$new();
     getMenuListDeferred = $q.defer();
     deleteMenuDeffered = $q.defer();
@@ -39,70 +41,74 @@ describe('Controller: MenuListCtrl', function () {
     scope.$digest();
   }));
 
-  it('should attach a viewName to the scope', function () {
+  it('should attach a viewName to the scope', function() {
     expect(scope.viewName).toBe('Menu Management');
   });
 
-  it('should clear search model and make a API call', function () {
+  it('should clear search model and make a API call', function() {
     scope.search = {
       startDate: 'fakeDate'
     };
     scope.clearForm();
     expect(scope.search.startDate).toBe(undefined);
-    expect(menuService.getMenuList).toHaveBeenCalledWith({ limit: 100, offset: 0 });
+    expect(menuService.getMenuList).toHaveBeenCalledWith({
+      limit: 100,
+      offset: 0
+    });
   });
 
-  it('should clear search model and make a API call', function () {
+  it('should clear search model and make a API call', function() {
     scope.search = {
       startDate: '10/05/1979'
     };
     scope.searchMenus();
     expect(menuService.getMenuList).toHaveBeenCalledWith({
       startDate: '19791005',
-      limit: 100, offset: 0
+      limit: 100,
+      offset: 0
     });
   });
 
-  it('should get the menu list from API', function () {
+  it('should get the menu list from API', function() {
     scope.loadMenus();
     expect(menuService.getMenuList).toHaveBeenCalled();
   });
 
-  describe('menuList in scope', function () {
+  describe('menuList in scope', function() {
     beforeEach(function() {
-        scope.loadMenus();
-        scope.$digest();
-      });
+      scope.loadMenus();
+      scope.$digest();
+    });
 
     it('should attach a menuList after a API call to getMenuList',
-      function () {
+      function() {
         expect(scope.menuList.length).toBeGreaterThan(1);
       });
 
-    it('should have a menu name property', function () {
+    it('should have a menu name property', function() {
       expect(scope.menuList[0].menuCode).toBe('T1');
     });
 
-    it('should have an array of items', function () {
+    it('should have an array of items', function() {
       expect(scope.menuList[0].menuItems.length).toBeGreaterThan(0);
     });
 
-    it('should have a formatted start and date', function () {
+    it('should have a formatted start and date', function() {
       expect(scope.menuList[0].startDate).toBe('05/06/2015');
     });
   });
 
-  describe('Action buttons', function () {
+  describe('Action buttons', function() {
     var fakeMenuItem;
 
-    beforeEach(function () {
+    beforeEach(function() {
       fakeMenuItem = {
         endDate: moment().add(1, 'month').format('L').toString(),
         startDate: moment().format('L').toString()
       };
     });
 
-    it('should change the url based on the menu object', function () {
+    it('should change the url based on the menu object', function() {
       scope.showMenu({
         id: 1
       });
@@ -110,27 +116,32 @@ describe('Controller: MenuListCtrl', function () {
       expect(location.path()).toBe('/menu/view/1');
     });
 
-    describe('can user edit / delete menu', function () {
-      it('should have a isMenuEditable function', function () {
+    describe('can user edit / delete menu', function() {
+      it('should have a isMenuEditable function', function() {
         expect(!!scope.isMenuEditable).toBe(true);
       });
 
-      it('should return true if menu is editable', function () {
+      it('should return true if menu is editable', function() {
         expect(scope.isMenuEditable(fakeMenuItem)).toBe(true);
       });
 
-      it('should return false if menu is not editable', function () {
+      it('should return false if menu is not editable', function() {
         fakeMenuItem.endDate = moment().subtract(1, 'month').format(
           'L').toString();
         expect(scope.isMenuEditable(fakeMenuItem)).toBe(false);
       });
 
-      it('should have a isMenuReadOnly function', function () {
+      it('should return true if menus end date is today', function() {
+        fakeMenuItem.endDate = dateUtility.nowFormatted();
+        expect(scope.isMenuEditable(fakeMenuItem)).toBe(true);
+      });
+
+      it('should have a isMenuReadOnly function', function() {
         expect(!!scope.isMenuReadOnly).toBe(true);
       });
 
       it('should return true if startDate < today > endDate',
-        function () {
+        function() {
           fakeMenuItem.startDate = moment().subtract(1, 'month').format(
             'L').toString();
           fakeMenuItem.endDate = moment().subtract(2, 'month').format(
@@ -139,7 +150,7 @@ describe('Controller: MenuListCtrl', function () {
         });
 
       it('should return true if startDate < today < endDate',
-        function () {
+        function() {
           fakeMenuItem.startDate = moment().subtract(1, 'month').format(
             'L').toString();
           fakeMenuItem.endDate = moment().add(2, 'month').format(
@@ -148,7 +159,7 @@ describe('Controller: MenuListCtrl', function () {
         });
 
       it('should return false if startDate > today > endDate',
-        function () {
+        function() {
           fakeMenuItem.startDate = moment().add(1, 'month').format(
             'L').toString();
           fakeMenuItem.endDate = moment().add(2, 'month').format(
@@ -157,16 +168,16 @@ describe('Controller: MenuListCtrl', function () {
         });
 
       it('should return true if menu === null or undefined',
-        function () {
+        function() {
           expect(scope.isMenuReadOnly(fakeMenuItem)).toBe(true);
         });
     });
 
-    it('should have a confirmDelete function', function () {
+    it('should have a confirmDelete function', function() {
       expect(!!scope.showDeleteConfirmation).toBe(true);
     });
 
-    it('should attach menuToDelete to scope', function () {
+    it('should attach menuToDelete to scope', function() {
       scope.showDeleteConfirmation({
         name: 'menuToDelete'
       });
@@ -174,7 +185,7 @@ describe('Controller: MenuListCtrl', function () {
     });
 
     it('should do a DELETE request to menuService with menuToDelete',
-      function () {
+      function() {
         scope.showDeleteConfirmation({
           id: '1'
         });
@@ -182,7 +193,7 @@ describe('Controller: MenuListCtrl', function () {
         expect(menuService.deleteMenu).toHaveBeenCalled();
       });
 
-    describe('the error handler', function () {
+    describe('the error handler', function() {
 
       var mockError = {
         status: 400,
@@ -203,11 +214,11 @@ describe('Controller: MenuListCtrl', function () {
         scope.$apply();
       });
 
-      it('should set error data ', function () {
+      it('should set error data ', function() {
         expect(scope.errorResponse).toEqual(mockError);
       });
 
-      it('should return false', function () {
+      it('should return false', function() {
         expect(scope.displayError).toBeTruthy();
       });
 
