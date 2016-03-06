47913bec111beb5cffc09ad90efcd45564b7e02e
Damir Palinic <damir@bigroomstudios.com>
2016-03-06 20:36:52 +0100

    TSVPORTAL-4406 - Started adding stock totals

--- app/scripts/controllers/store-instance-amend.js
+++ app/scripts/controllers/store-instance-amend.js
@@ -8,8 +8,16 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('StoreInstanceAmendCtrl', function ($q, $scope, $routeParams, $filter, storeInstanceAmendFactory, dateUtility, lodash) {
+  .controller('StoreInstanceAmendCtrl', function ($q, $scope, $routeParams, $filter, storeInstanceAmendFactory, dateUtility, lodash, globalMenuService, reconciliationFactory) {
     var $this = this;
+
+    function formatAsCurrency(valueToFormat) {
+      if (angular.isDefined(valueToFormat)) {
+        return sprintf('%.2f', valueToFormat);
+      }
+
+      return valueToFormat;
+    }
 
     $scope.showAddOrEditScheduleModal = function (scheduleToEdit) {
       if (scheduleToEdit) {
@@ -169,28 +177,197 @@ angular.module('ts5App')
       });
     };
 
-    this.getCashBagListSuccess = function (dataFromAPI) {
+    function getCurrencyByBaseCurrencyId(currenciesArray, baseCurrencyId) {
+      return currenciesArray.filter(function (currencyItem) {
+        return currencyItem.id === baseCurrencyId;
+      })[0];
+    }
+
+    function getCashBagListSuccess (dataFromAPI) {
       $scope.cashBagList = angular.copy(dataFromAPI);
-    };
+    }
 
-    this.getCashBagList = function () {
-      storeInstanceAmendFactory.getCashBagListMockData().then($this.getCashBagListSuccess);
-    };
+    function getCashBagList () {
+      return storeInstanceAmendFactory.getCashBagListMockData().then(getCashBagListSuccess);
+    }
 
-    this.initViewDefaults = function () {
+    function setStoreInstance (storeInstanceDataFromAPI) {
+      var storeInstance = angular.copy(storeInstanceDataFromAPI);
+      storeInstance.scheduleDate = dateUtility.formatDateForApp(storeInstance.scheduleDate);
+
+      $scope.storeInstance = storeInstance;
+    }
+
+    function getStoreInstance () {
+      return reconciliationFactory.getStoreInstanceDetails($routeParams.storeInstanceId).then(setStoreInstance);
+    }
+
+    function setCompany (companyFromAPI) {
+      $scope.company = angular.copy(companyFromAPI);
+    }
+
+    function getCompany () {
+      var companyId = globalMenuService.company.get();
+      return reconciliationFactory.getCompany(companyId).then(setCompany);
+    }
+
+    function setCompanyGlobalCurrencies (companyGlobalCurrenciesFromAPI) {
+      $scope.companyGlobalCurrencies = angular.copy(companyGlobalCurrenciesFromAPI.response);
+    }
+
+    function getCompanyGlobalCurrencies () {
+      return reconciliationFactory.getCompanyGlobalCurrencies().then(setCompanyGlobalCurrencies);
+    }
+
+    function setItemTypes (itemTypesFromAPI) {
+      $scope.itemTypes = angular.copy(itemTypesFromAPI);
+    }
+
+    function getItemTypes () {
+      return reconciliationFactory.getItemTypesList().then(setItemTypes);
+    }
+
+    function setStockTotals (stockTotalsFromAPI) {
+      $scope.stockTotals = angular.copy(stockTotalsFromAPI.response);
+    }
+
+    function getStockTotals () {
+      return reconciliationFactory.getStockTotals($routeParams.storeInstanceId).then(setStockTotals);
+    }
+
+    function setPromotionTotals (promotionTotalsFromAPI) {
+      $scope.promotionTotals = $filter('filter')(angular.copy(promotionTotalsFromAPI.response), {
+        exchangeRateTypeId: 1
+      });
+    }
+
+    function getPromotionTotals () {
+      return reconciliationFactory.getPromotionTotals($routeParams.storeInstanceId).then(setPromotionTotals);
+    }
+
+    function showLoadingModal(text) {
+      $scope.displayError = false;
+      angular.element('#loading').modal('show').find('p').text(text);
+    }
+
+    function hideLoadingModal() {
+      angular.element('#loading').modal('hide');
+    }
+
+    function handleResponseError(responseFromAPI) {
+      hideLoadingModal();
+      $scope.errorResponse = angular.copy(responseFromAPI);
+      $scope.displayError = true;
+    }
+
+    function setupCompanyBaseCurrency () {
+      $scope.companyBaseCurrency = getCurrencyByBaseCurrencyId($scope.companyGlobalCurrencies, $scope.company.baseCurrencyId);
+    }
+
+    function getTotalsFor(stockTotals, itemTypeName) {
+      var stockItem = $filter('filter')(stockTotals, {
+        itemTypeName: itemTypeName
+      });
+      var totalLMP = 0;
+      var totalEPOS = 0;
+      angular.forEach(stockItem, function (item) {
+        totalLMP += item.lmpTotal || 0;
+        totalEPOS += item.eposTotal || 0;
+      });
+
+      return {
+        parsedLMP: totalLMP,
+        parsedEPOS: totalEPOS,
+        totalLMP: formatAsCurrency(totalLMP),
+        totalEPOS: formatAsCurrency(totalEPOS)
+      };
+    }
+
+    function getTotalsForPromotions(promotionTotals) {
+      var total = 0;
+      angular.forEach(promotionTotals, function (promotionItem) {
+        total += promotionItem.convertedAmount;
+      });
+
+      return {
+        parsedLMP: total,
+        parsedEPOS: total,
+        totalLMP: formatAsCurrency(total),
+        totalEPOS: formatAsCurrency(total)
+      };
+    }
+
+    function setupNetTotals () {
+      angular.forEach($scope.stockTotals, function (stockItem) {
+        stockItem.itemTypeName = lodash.findWhere($scope.itemTypes, {
+          id: stockItem.itemTypeId
+        }).name;
+      });
+
+      var totalItems = getTotalsFor($scope.stockTotals, 'Regular');
+      var totalVirtual = getTotalsFor($scope.stockTotals, 'Virtual');
+      var totalVoucher = getTotalsFor($scope.stockTotals, 'Voucher');
+      var totalPromotion = getTotalsForPromotions($scope.promotionTotals);
+
+      var stockTotals = {
+        totalRetail: totalItems,
+        totalVirtual: totalVirtual,
+        totalVoucher: totalVoucher,
+        totalPromotion: totalPromotion
+      };
+
+      var netLMP = stockTotals.totalRetail.parsedLMP + stockTotals.totalVirtual.parsedEPOS + stockTotals.totalVoucher
+          .parsedEPOS - stockTotals.totalPromotion.parsedLMP;
+      var netEPOS = stockTotals.totalRetail.parsedEPOS + stockTotals.totalVirtual.parsedEPOS + stockTotals.totalVoucher
+          .parsedEPOS - stockTotals.totalPromotion.parsedEPOS;
+
+      var netTotals = {
+        netLMP: formatAsCurrency(netLMP),
+        netEPOS: formatAsCurrency(netEPOS)
+      };
+
+      //var stockItems = $this.stockTotals.concat($this.promotionTotals);
+      $scope.stockTotals = angular.extend(stockTotals, {
+        totalNet: netTotals
+      });
+    }
+
+    function handleInitDataSuccess() {
+      setupCompanyBaseCurrency();
+      setupNetTotals();
+
+      hideLoadingModal();
+    }
+
+    function initData() {
+      var promiseArray = [
+        getCashBagList(),
+        getStoreInstance(),
+        getCompany(),
+        getCompanyGlobalCurrencies(),
+        getItemTypes(),
+        getStockTotals(),
+        getPromotionTotals()
+      ];
+
+      $q.all(promiseArray).then(handleInitDataSuccess, handleResponseError);
+    }
+
+    function initViewDefaults () {
       $scope.moveCashBagAction = 'none';
       $scope.showDeletedCashBags = false;
       $scope.sectorsToMove = [];
       $scope.cashBagFilter = {};
       $scope.scheduleSearch = {};
       angular.element('#checkbox').bootstrapSwitch();
-    };
+    }
 
-    this.init = function () {
-      $this.initViewDefaults();
-      $this.getCashBagList();
-    };
+    function init () {
+      showLoadingModal('Loading Store Instance Amend Details');
+      initViewDefaults();
+      initData();
+    }
 
-    this.init();
+    init();
 
   });
