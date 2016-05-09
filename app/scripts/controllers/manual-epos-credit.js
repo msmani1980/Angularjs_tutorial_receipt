e5e67838d7ad52c2c08e8165420177816fc60cfc
Alyssa Reyes <areyes@egate-solutions.com>
2016-05-09 11:14:02 -0500

    TSVPORTAL-3108 | add new route

---
+++ app/scripts/controllers/manual-epos-credit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ts5App.controller:ManualEposCreditCtrl
+ * @description
+ * # ManualEposCreditCtrl
+ * Controller of the ts5App
+ */
+angular.module('ts5App')
+  .controller('ManualEposCreditCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
