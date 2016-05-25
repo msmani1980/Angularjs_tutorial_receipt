296234528d39053f1fbd17ea36508e4339053138
Alyssa Reyes <areyes@egate-solutions.com>
2016-05-25 14:37:19 -0500

    TSVPORTAL-3101 | integrate checks for unverified changes

--- app/scripts/controllers/manual-epos-entry.js
+++ app/scripts/controllers/manual-epos-entry.js
@@ -8,7 +8,7 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('ManualEposEntryCtrl', function ($scope, $q, manualEposFactory, $location, $routeParams) {
+  .controller('ManualEposEntryCtrl', function ($scope, $q, manualEposFactory, $location, $routeParams, lodash) {
 
     function showLoadingModal(text) {
       angular.element('#loading').modal('show').find('p').text(text);
@@ -26,6 +26,25 @@ angular.module('ts5App')
       return (angular.isDefined($scope.isVerified) ? $scope.isVerified[formName] : false);
     };
 
+    $scope.doesFormHaveChanges = function (formName) {
+      return angular.isDefined($scope.containsChanges) ? !$scope.isFormVerified(formName) && $scope.containsChanges[formName] : false;
+    };
+
+    $scope.shouldDisableForm = function (formName) {
+      return ($scope.isConfirmed) ? !$scope.isFormVerified(formName) : false;
+    };
+
+    function areAllChangesVerified() {
+      var formList = ['cash', 'credit', 'virtual', 'voucher', 'discount', 'promotion'];
+      var unverifiedChangesExist = false;
+      angular.forEach(formList, function (formName) {
+        var isFormInvalid = ($scope.doesFormHaveChanges(formName)) ? !$scope.isFormVerified(formName) : false;
+        unverifiedChangesExist = unverifiedChangesExist || isFormInvalid;
+      });
+
+      return unverifiedChangesExist;
+    }
+
     function setVerification(dataFromAPI) {
       var cashBagVerification = angular.copy(dataFromAPI);
       $scope.isVerified = {
@@ -36,21 +55,37 @@ angular.module('ts5App')
         virtual: !!cashBagVerification.virtualItemVerifiedOn,
         voucher: !!cashBagVerification.voucherItemsVerifiedOn
       };
+      $scope.isConfirmed = !!cashBagVerification.confirmedOn;
     }
 
-    function getCashBagVerification() {
-      manualEposFactory.checkCashBagVerification($routeParams.cashBagId).then(setVerification);
+    function groupItemList(itemListFromAPI, itemTypeList) {
+      var virtualItemTypeId = lodash.findWhere(itemTypeList, { name: 'Virtual' }).id;
+      var voucherItemTypeId = lodash.findWhere(itemTypeList, { name: 'Voucher' }).id;
+      var groupedItemList = lodash.groupBy(angular.copy(itemListFromAPI.response), function (item) {
+        return item.itemTypeId;
+      });
+
+      return {
+        virtual: groupedItemList[virtualItemTypeId],
+        voucher: groupedItemList[voucherItemTypeId]
+      };
     }
 
-    function setCashBag(dataFromAPI) {
-      $scope.cashBag = angular.copy(dataFromAPI);
-    }
+    function completeInit(responseCollection) {
+      $scope.cashBag = angular.copy(responseCollection[0]);
+      setVerification(responseCollection[1]);
+      var groupedItemList = groupItemList(responseCollection[5], responseCollection[6]);
 
-    function getCashBag() {
-      manualEposFactory.getCashBag($routeParams.cashBagId).then(setCashBag);
-    }
+      $scope.containsChanges = {
+        cash: responseCollection[2].meta.count > 0,
+        credit: responseCollection[3].meta.count > 0,
+        discount: responseCollection[4].meta.count > 0,
+        voucher: groupedItemList.voucher.length > 0,
+        virtual: groupedItemList.virtual.length > 0,
+        promotion: false // TODO: getPromotionList
+      };
 
-    function completeInit() {
+      $scope.readyToConfirm = areAllChangesVerified();
       hideLoadingModal();
     }
 
@@ -60,9 +95,17 @@ angular.module('ts5App')
       $scope.cashBagId = $routeParams.cashBagId;
 
       var initPromises = [
-        getCashBag(),
-        getCashBagVerification()
+        manualEposFactory.getCashBag($routeParams.cashBagId),
+        manualEposFactory.checkCashBagVerification($routeParams.cashBagId),
+        manualEposFactory.getCashBagCashList($routeParams.cashBagId),
+        manualEposFactory.getCashBagCreditList($routeParams.cashBagId),
+        manualEposFactory.getCashBagDiscountList($routeParams.cashBagId),
+        manualEposFactory.getCashBagItemList($routeParams.cashBagId),
+        manualEposFactory.getItemTypes()
+        
+        // TODO: getPromotionList
       ];
+
       $q.all(initPromises).then(completeInit);
     }
 
