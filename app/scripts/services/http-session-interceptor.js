02ea27fefdeff3e944693a41481336aabeb4d0ec
Sujith Vellat <sujith.vellat@marlabs.com>
2016-06-02 08:31:10 -0400

    TSVPORTAL-6497| suffixed the page urls with wild char match for child pages

--- app/scripts/services/http-session-interceptor.js
+++ app/scripts/services/http-session-interceptor.js
@@ -17,98 +17,99 @@ angular.module('ts5App')
     };
 
     var notrsvrPages = [
-        '/cash-bag-list',
-        '/cash-bag-submission',
+        '/cash-bag-list/*',
+        '/cash-bag-submission/*',
         '/cash-bag/*',
-        '/category-list',
+        '/category-list/*',
         '/category/*',
-        '/change-password', 
-        '/commission-data-table',
+        '/change-password/*', 
+        '/commission-data-table/*',
         '/commission-data/*',
-        '/company-create',
+        '/company-create/*',
         '/company-edit/*',
-        '/company-exchange-rate-edit',
-        '/company-list',
-        '/company-reason-code',
-        '/company-reason-type-subscribe',
+        '/company-exchange-rate-edit/*',
+        '/company-list/*',
+        '/company-reason-code/*',
+        '/company-reason-type-subscribe/*',
         '/company-relationship-list/*',
         '/company-view/*',
         '/company/*',
-        '/currency-edit',
-        '/discounts',
+        '/currency-edit/*',
         '/discounts/*',
-        '/employee-commission-list',
+        '/employee-commission-list/*',
         '/employee-commission/*',
         '/employee-message/*',
-        '/employee-messages',
-        '/exchange-rates',
-        '/excise-duty-list',
-        '/excise-duty-relationship-list',
-        '/forgot-username-password', 
-        '/global-reason-code',
+        '/employee-messages/*',
+        '/exchange-rates/*',
+        '/excise-duty-list/*',
+        '/excise-duty-relationship-list/*',
+        '/forgot-username-password/*', 
+        '/global-reason-code/*',
         '/item-copy/*',
-        '/item-create',
+        '/item-create/*',
         '/item-edit/*',
-        '/item-import',
-        '/item-list',
+        '/item-import/*',
+        '/item-list/*',
         '/item-view/*',
         '/lmp-delivery-note/*',
-        '/lmp-locations-list',
+        '/lmp-locations-list/*',
         '/login',
-        '/manage-goods-received',
-        '/manual-ecs',
+        '/manage-goods-received/*',
+        '/manual-ecs/*',
         '/manual-epos-cash/*',
         '/manual-epos-credit/*',
         '/manual-epos-dashboard/*',
         '/manual-epos-discount/*',
         '/manual-epos-items/*',
-        '/manual-store-instance', 
-        '/menu-list',
-        '/menu-relationship-create',
+        '/manual-store-instance/*', 
+        '/menu-list/*',
+        '/menu-relationship-create/*',
         '/menu-relationship-edit/*',
-        '/menu-relationship-list',
+        '/menu-relationship-list/*',
         '/menu-relationship-view/*',
-        '/menu/:state/*',
-        '/post-trip-data-list',
+        '/menu/*',
+        '/post-trip-data-list/*',
         '/post-trip-data/*',
-        '/promotions',
         '/promotions/*',
-        '/retail-company-exchange-rate-setup',
-        '/station-create',
+        '/retail-company-exchange-rate-setup/*',
+        '/station-create/*',
         '/station-edit/*',
-        '/station-list',
+        '/station-list/*',
         '/station-view/*',
-        '/stock-dashboard',
-        '/stock-owner-item-create',
+        '/stock-dashboard/*',
+        '/stock-owner-item-create/*',
         '/stock-owner-item-edit/*',
-        '/stock-owner-item-list',
+        '/stock-owner-item-list/*',
         '/stock-owner-item-view/*',
-        '/stock-take-report',
-        '/stock-take-review',
+        '/stock-take-report/*',
+        '/stock-take-review/*',
         '/stock-take/*',
         '/store-instance-amend/*',
         '/store-instance-create/*',
-        '/store-instance-dashboard',
+        '/store-instance-dashboard/*',
         '/store-instance-inbound-seals/*',
         '/store-instance-packing/*',
         '/store-instance-seals/*',
         '/store-instance-review/*',
-        '/store-instance-step-1',
-        '/store-number',
-        '/tax-rates',
-        '/transactions',
-        '/ember/#/schedules',
-        '/ember/#/menu-assignments',
-        '/ember/#/menu-rules',
-        '/ember/#/menu-rules/create',
-        '/ember/#/promotion-categories',
-        '/ember/#/promotion-catalogs',
-        '/ember/#/receipt-rules',
-        '/ember/#/receipt-rules/create'
+        '/store-instance-step-1/*',
+        '/store-number/*',
+        '/tax-rates/*',
+        '/transactions/*',
+        '/ember/#/schedules/*',
+        '/ember/#/menu-assignments/*',
+        '/ember/#/menu-rules/*',
+        '/ember/#/promotion-categories/*',
+        '/ember/#/promotion-catalogs/*',
+        '/ember/#/receipt-rules/*'
     ];
 
     var legacyApis = [
-        '/rsvr/api/company-preferences'
+        '/rsvr/api/company-preferences',
+        '/rsvr/api/companies/*/relationships'
+    ];
+
+    var onlyRsvrApis = [
+      '/rsvr/api/dailyexchangerate'
     ];
 
     function responseError(response) {
@@ -132,24 +133,28 @@ angular.module('ts5App')
     }
 
     var isPageWithLegacyAPIs = function() {
-      var pageMatches = Array.prototype.filter.call(notrsvrPages, function (page) {
-        return $location.absUrl().match(page);
-      });
-
-      return pageMatches.length > 0;
+      return isMatching($location.absUrl(), notrsvrPages);
     };
 
     var isLegacyAPI = function(config) {
-      var apiMatches =  Array.prototype.filter.call(legacyApis, function (api) {
-        return config.url.match(api);
+      return isMatching(config.url, legacyApis);
+    };
+
+    var isOnlyRsvrAPI = function(url) {
+      return isMatching(url, onlyRsvrApis);
+    };
+
+    var isMatching = function(url, list) {
+      var matches = Array.prototype.filter.call(list, function (item) {
+        return url.match(item);
       });
 
-      return apiMatches.length !== 0;
+      return matches.length !== 0;
     };
 
     var shouldReplaceUrl = function(config) {
       var hasRestParam = $location.absUrl().indexOf('api=rest') > 0;
-      if (!hasRestParam && isPageWithLegacyAPIs() || isLegacyAPI(config)) {
+      if (!hasRestParam && !isOnlyRsvrAPI(config.url) && isPageWithLegacyAPIs() || isLegacyAPI(config)) {
         return true;
       }
 
