9ca5a6771cf4b9110a54b315dc1549a82e27f197
Rodrigo Curiel <rcuriel@egate-solutions.com>
2016-06-13 11:44:20 -0500

    TSVPORTAL-6455 | Added CRUD service for company-formats

--- app/scripts/services/company-format-service.js
+++ app/scripts/services/company-format-service.js
@@ -8,6 +8,61 @@
  * Service in the ts5App.
  */
 angular.module('ts5App')
-  .service('companyFormatService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('companyFormatService', function ($resource, ENV) {
+
+    var companyFormatRequestURL = ENV.apiUrl + '/rsvr/api/company-formats/:id';
+    var requestParameters = {
+      id: '@id'
+    };
+    var actions = {
+      getCompanyFormat: {
+        method: 'GET'
+      },
+      createCompanyFormat: {
+        method: 'POST'
+      },
+      updateCompanyFormat: {
+        method: 'PUT'
+      },
+      deleteCompanyFormat: {
+        method: 'DELETE'
+      }
+    };
+
+    var companyFormatRequestResources = $resource(companyFormatRequestURL, requestParameters, actions);
+
+    var getCompanyFormatList = function (payload) {
+      requestParameters.id = '';
+      var requestPayload = payload || {};
+      return companyFormatRequestResources.getCompanyFormat(requestPayload).$promise;
+    };
+
+    var getCompanyFormat = function (id) {
+      requestParameters.id = id;
+      return companyFormatRequestResources.getCompanyFormat().$promise;
+    };
+
+    var createCompanyFormat = function (payload) {
+      requestParameters.id = '';
+      return companyFormatRequestResources.createCompanyFormat(payload).$promise;
+    };
+
+    var updateCompanyFormat = function (id, payload) {
+      requestParameters.id = id;
+      return companyFormatRequestResources.updateCompanyFormat(payload).$promise;
+    };
+
+    var deleteCompanyFormat = function (id) {
+      requestParameters.id = id;
+      return companyFormatRequestResources.deleteCompanyFormat().$promise;
+    };
+
+    return {
+      getCompanyFormatList: getCompanyFormatList,
+      getCompanyFormat: getCompanyFormat,
+      createCompanyFormat: createCompanyFormat,
+      updateCompanyFormat: updateCompanyFormat,
+      deleteCompanyFormat: deleteCompanyFormat
+    };
+
   });
