bc17ccfc5b720a5411fc3ee3b6238e39b1019f88
Alyssa Reyes <areyes@egate-solutions.com>
2016-06-28 16:42:11 -0500

    TSVPORTAL-6890 | fix currency id mapping and small cleanup

--- app/scripts/services/http-session-interceptor.js
+++ app/scripts/services/http-session-interceptor.js
@@ -94,7 +94,7 @@ angular.module('ts5App')
     ];
 
     var legacyApis = [
-	    '/rsvr/api/companies/[0-9]*/relationships',
+        '/rsvr/api/companies/[0-9]*/relationships',
         '/rsvr/api/companies/[0-9]*$',
         '/rsvr/api/retail-items/master/*',
         '/rsvr/api/daily-exchange-rates/[0-9]*',
