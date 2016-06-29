9b231f24d2bd7fadeeba9beb4a13b2b0332f5398
Mani.Murugan <MAlagarsamy@egate-solutions.com>
2016-06-29 13:30:34 -0500

    REPO-18 : Code Review

--- app/scripts/filters/filesize.js
+++ app/scripts/filters/filesize.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//jshint maxcomplexity:8
+
 /**
  * @ngdoc filter
  * @name ts5App.filter:filesize
--- app/scripts/filters/timeinterval.js
+++ app/scripts/filters/timeinterval.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//jshint maxcomplexity:11
+
 /**
  * @ngdoc filter
  * @name ts5App.filter:timeinterval
