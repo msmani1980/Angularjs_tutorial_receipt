0c8d827e1fe01e7e250ec097bdbbccf2b901dd4e
Bala Balaji Reddy <breddy@egate-solutions.com>
2016-06-29 23:50:03 -0400

    TSVPORTAL-6051 : Updated based on latest code.

--- app/scripts/controllers/item-create.js
+++ app/scripts/controllers/item-create.js
@@ -866,12 +866,12 @@ angular.module('ts5App').controller('ItemCreateCtrl',
       }
 
       var priceCurrencies = [];
-      var priceGroup = $scope.formData.prices[priceIndex];
-
+      
       angular.forEach(currenciesList, function (currency) {
         var newCurrency = $this.generateCurrency(currency);
+        var priceGroup = $scope.formData.prices[priceIndex];
         var existingCurrency = lodash.findWhere(priceGroup.priceCurrencies, { companyCurrencyId: newCurrency.companyCurrencyId });
-        newCurrency.id = existingCurrency.id; 
+        newCurrency.id = (existingCurrency) ? existingCurrency.id : newCurrency.id; 
         newCurrency.price = (existingCurrency) ? existingCurrency.price : newCurrency.price;
         priceCurrencies.push(newCurrency);
       });
