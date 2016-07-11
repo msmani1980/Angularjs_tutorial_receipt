6b5135703921643078407424d7ce15c049b250ec
Bala Balaji Reddy <breddy@egate-solutions.com>
2016-07-10 22:20:39 -0400

    TSVPORTAL-6954 : Added missing function for displaying Image dates in Stock Owner Item.

--- app/scripts/controllers/stock-owner-item-create.js
+++ app/scripts/controllers/stock-owner-item-create.js
@@ -397,9 +397,27 @@ angular.module('ts5App').controller('StockOwnerItemCreateCtrl',
     };
 
     this.setUIReady = function() {
+      $scope.minDate = $this.determineMinDate();	
       $scope.uiSelectTemplateReady = true;
       $this.hideLoadingModal();
     };
+    
+    this.determineMinDate = function() {
+        var diff = 1;
+        if ($scope.editingItem && !dateUtility.isTomorrowOrLater($scope.formData.startDate)) {
+          diff = dateUtility.diff(
+            dateUtility.nowFormatted(),
+            $scope.formData.startDate
+          );
+        }
+
+        var dateString = diff.toString() + 'd';
+        if (diff >= 0) {
+          dateString = '+' + dateString;
+        }
+        
+        return dateString;
+      };
 
     this.getDependencies = function() {
       $this.showLoadingModal('We are loading the Items data!');
