a2b4775bd01e1f5b037026d0db1162d640c8776a
Mani.Murugan <MAlagarsamy@egate-solutions.com>
2016-08-09 13:27:30 -0500

    REPO-174 : Test Case updated

--- test/spec/controllers/schedule-report.js
+++ test/spec/controllers/schedule-report.js
@@ -1,5 +1,25 @@
 'use strict';
 
 describe('Controller: ScheduleReportCtrl', function () {
+	
+	  beforeEach(module('ts5App'));
+	
+	  var scope, $compile;
+	  var controller;
+	  	  
+	  beforeEach(inject(function($controller, $rootScope,  _$compile_) {
+	  
+		    scope = $rootScope.$new();
+		    $compile = _$compile_;
+		    
+	  }));
 
+	  describe('controller init', function() {
+
+		    it('should define Scope Report emailMe', function() {
+		      scope.emailMe = true;
+		      scope.$digest();
+		      expect(scope.emailMe).toBe(true);
+		    });
+	 });
 });
