9fdc9e5b143f13c3f530725e618328de945a22cd
Mani.Murugan <MAlagarsamy@egate-solutions.com>
2016-08-10 12:37:51 -0500

    REPO-174 : PR Comments update

--- test/spec/controllers/report-options.js
+++ test/spec/controllers/report-options.js
@@ -2,23 +2,36 @@
 
 describe('Controller: ReportOptionsCtrl', function () {
 	
-	  beforeEach(module('ts5App'));
-	
-	  var scope, $compile;
-	  	  
-	  beforeEach(inject(function($rootScope,  _$compile_) {
-	  
-		    scope = $rootScope.$new();
-		    $compile = _$compile_;
-		    
-	  }));
-
-	  describe('controller init', function() {
-
-		    it('should define Scope Report emailMe', function() {
-		      scope.emailMe = true;
-		      scope.$digest();
-		      expect(scope.emailMe).toBe(true);
-		    });
-	 });
+  beforeEach(module('ts5App'));
+  var scope, $compile;
+  var ReportOptionsCtrl;
+  var jobService;
+  var jobServiceRunDeferred;
+  var jobServiceRunResponseJSON;
+  
+  beforeEach(inject(function($rootScope,  _$compile_, $injector, $q, $controller) {
+    scope = $rootScope.$new();
+    $compile = _$compile_;
+    jobService = $injector.get('jobService');
+    jobServiceRunResponseJSON = {};
+    jobServiceRunDeferred = $q.defer();
+    jobServiceRunDeferred.resolve(jobServiceRunResponseJSON);
+    spyOn(jobService, 'run').and.returnValue(jobServiceRunDeferred.promise);
+    ReportOptionsCtrl = $controller('ReportOptionsCtrl', {
+      $scope: scope
+    });
+    scope.$digest();
+  }));  
+ 
+ describe('run', function () {
+    it('should call with params with emailMe var', function () {
+      scope.emailMe = true;
+      scope.run();
+      var expectedParams = jasmine.objectContaining({
+        emailMe: true
+      });
+      expect(jobService.run).toHaveBeenCalledWith(expectedParams);
+    });
+  });
+ 
 });
--- test/spec/controllers/schedule-report.js
+++ test/spec/controllers/schedule-report.js
@@ -2,23 +2,28 @@
 
 describe('Controller: ScheduleReportCtrl', function () {
 	
-	  beforeEach(module('ts5App'));
-	
-	  var scope, $compile;
-	  	  
-	  beforeEach(inject(function($rootScope,  _$compile_) {
-	  
-		    scope = $rootScope.$new();
-		    $compile = _$compile_;
-		    
-	  }));
-
-	  describe('controller init', function() {
-
-		    it('should define Scope Report DateRange', function() {
-		      scope.dateRange = [{ name: 'Previous Day', value: 'Previous Day' }, { name: 'Previous 7 days', value: 'Previous 7 days' }, { name: 'Previous Month', value: 'Previous Month' }];
-		      scope.$digest();
-		      expect(scope.dateRange[0].name).toEqual('Previous Day');
-		    });
-	 });
+  beforeEach(module('ts5App'));
+  
+  var scope, $compile;
+  var ScheduleReportCtrl;
+  
+  beforeEach(inject(function($rootScope,  _$compile_, $injector, $q, $controller) {
+    scope = $rootScope.$new();
+    $compile = _$compile_;
+    
+    ScheduleReportCtrl = $controller('ScheduleReportCtrl', {
+      $scope: scope
+    });
+    
+    scope.$digest();
+    
+  }));
+  
+  describe('controller init', function() {	  
+    it(' dateRange should be object ', function() {
+      expect(Object.prototype.toString.call(scope.dateRange)).toBe('[object Object]');
+    });
+    
+  });
+  
 });
