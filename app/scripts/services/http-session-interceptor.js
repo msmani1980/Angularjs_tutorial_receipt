84386bd2a183fd810c7e364535913e33dc2b1684
Bala Balaji Reddy <breddy@egate-solutions.com>
2016-08-25 10:54:01 -0500

    TSVPORTAL-7350 | Revert back the api changes - QA non approved.

--- app/scripts/services/http-session-interceptor.js
+++ app/scripts/services/http-session-interceptor.js
@@ -63,6 +63,7 @@ angular.module('ts5App')
         '/stock-take-report/*',
         '/stock-take-review/*',
         '/stock-take/*',
+        '/store-instance-dashboard/*',
         '/store-instance-create/*',
         '/store-instance-inbound-seals/*',
         '/store-instance-packing/*',
@@ -80,6 +81,7 @@ angular.module('ts5App')
     ];
 
     var legacyApis = [
+        '/rsvr/api/retail-items/master/*',              
         '/rsvr/api/promotions/[0-9]*$',
         '/rsvr/api/dispatch/store-instances/[0-9]*/status/[5-9][^11]*$',
         '/rsvr/api/promotions',
