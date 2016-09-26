d11380b56a7d6264cb09b68eb6e288bafb70d1b9
Alyssa Reyes <areyes@egate-solutions.com>
2016-09-26 15:51:16 -0500

    TSVPORTAL-727 | filter item by date and category

--- app/scripts/controllers/promotion-category.js
+++ app/scripts/controllers/promotion-category.js
@@ -8,18 +8,34 @@
  * Controller of the ts5App
  */
 angular.module('ts5App')
-  .controller('PromotionCategoryCtrl', function ($scope, $routeParams, promotionCategoryFactory, $q, lodash) {
+  .controller('PromotionCategoryCtrl', function ($scope, $routeParams, promotionCategoryFactory, $q, lodash, messageService, dateUtility) {
 
     $scope.itemList = [];
     $scope.promotionCategory = {};
+
+    function showLoadingModal(message) {
+      angular.element('#loading').modal('show').find('p').text(message);
+    }
+
+    function hideLoadingModal() {
+      angular.element('#loading').modal('hide');
+    }
 
     function showErrors() {
 
     }
 
     $scope.addItem = function () {
+      if (!$scope.promotionCategory.startDate && !$scope.promotionCategory.endDate) {
+        messageService.display('warning', 'Please select a date range first', 'Add Promotion Category Item');
+        return;
+      }
+
       var newIndex = $scope.itemList.length;
-      $scope.itemList.push({ itemIndex: newIndex });
+      $scope.itemList.push({
+        itemIndex: newIndex,
+        masterItemList: angular.copy($scope.masterItemList)
+      });
     };
 
     $scope.removeItem = function (item) {
@@ -30,14 +46,40 @@ angular.module('ts5App')
     };
 
     $scope.filterItemListFromCategory = function (item) {
+      if (!item.selectedCategory) {
+        item.masterItemList = [];
+        return;
+      }
+
       var category = item.selectedCategory;
-      var itemPayload = { categoryId: category.id };
+      var itemPayload = {
+        categoryId: category.id,
+        startDate: dateUtility.formatDateForAPI($scope.promotionCategory.startDate),
+        endDate: dateUtility.formatDateForAPI($scope.promotionCategory.endDate)
+      };
 
       promotionCategoryFactory.getMasterItemList(itemPayload).then(function (response) {
         item.masterItemList = angular.copy(response.masterItems);
-      }, showErrors());
+      }, showErrors);
 
     };
+
+    $scope.shouldDisableItemDropDown = function (item) {
+      return item.masterItemList.length <= 0;
+    };
+
+    function getMasterItemList() {
+      showLoadingModal();
+      var payload = {
+        startDate: dateUtility.formatDateForAPI($scope.promotionCategory.startDate),
+        endDate: dateUtility.formatDateForAPI($scope.promotionCategory.endDate)
+      };
+
+      promotionCategoryFactory.getMasterItemList(payload).then(function (response) {
+        $scope.masterItemList = angular.copy(response.masterItems);
+        hideLoadingModal();
+      }, showErrors);
+    }
 
     function completeInit(responseCollection) {
       $scope.categoryList = angular.copy(responseCollection[0].salesCategories);
@@ -46,9 +88,11 @@ angular.module('ts5App')
         $scope.promotionCategory = angular.copy(responseCollection[1]);
       }
 
+      hideLoadingModal();
     }
 
-    function getDependencies() {
+    function init() {
+      showLoadingModal('Loading Data');
       var initPromises = [
         promotionCategoryFactory.getCategoryList()
       ];
@@ -60,11 +104,13 @@ angular.module('ts5App')
       $q.all(initPromises).then(completeInit, showErrors);
     }
 
-    function init() {
-      getDependencies();
-    }
-
     init();
+
+    $scope.$watchGroup(['promotionCategory.startDate', 'promotionCategory.endDate'], function () {
+      if ($scope.promotionCategory && $scope.promotionCategory.startDate && $scope.promotionCategory.endDate) {
+        getMasterItemList();
+      }
+    });
 
   }
 )
