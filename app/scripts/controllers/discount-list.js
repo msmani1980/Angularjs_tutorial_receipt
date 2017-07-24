1fae3a7e87bc25aca4d351df225eddc046000480
Josip Medic <josip@bigroomstudios.com>
2017-07-24 06:51:48 +0200

    TSVPORTAL-731: TSVPORTAL-9457 Active records that end today do not have Edit icons fixed

--- app/scripts/controllers/discount-list.js
+++ app/scripts/controllers/discount-list.js
@@ -108,7 +108,7 @@ angular.module('ts5App')
         return false;
       }
 
-      return dateUtility.isAfterToday(discount.endDate);
+      return dateUtility.isAfterTodayOrEqual(discount.endDate);
     };
 
     $scope.isDiscountReadOnly = function(exchangeRate) {
