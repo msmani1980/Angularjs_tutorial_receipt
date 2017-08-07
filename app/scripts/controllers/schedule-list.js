eecae9cffc800213cc9b3fc460d28ecd02c44408
Damir Palinic <damir@bigroomstudios.com>
2017-08-07 15:02:12 +0200

    TSVPORTAL-9345 - Moved schedule to new date utility functions

--- app/scripts/controllers/schedule-list.js
+++ app/scripts/controllers/schedule-list.js
@@ -64,7 +64,7 @@ angular.module('ts5App')
     };
 
     $scope.showDeleteButton = function(dateString) {
-      return dateUtility.isAfterToday(dateString);
+      return dateUtility.isAfterTodayDatePicker(dateString);
     };
 
     $scope.clearSearchForm = function() {
