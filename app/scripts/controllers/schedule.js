1be31b2b4ba38b176608a5cc985e3ac794dd801d
Damir Palinic <damir@bigroomstudios.com>
2017-08-08 12:50:38 +0200

    TSVPORTAL-9345 - Changed calendar html

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -30,19 +30,7 @@ angular.module('ts5App')
     ];
     $scope.getScheduleSucceded = false;
     $scope.isCreate = false;
-
-    this.determineMinForStartDate = function() {
-      $scope.minForStartDate = '+1d';
-    };
-
-    this.determineMinForEndDate = function() {
-      $scope.minForEndDate = ($scope.editingItem && $scope.shouldDisableStartDate) ? '0d' : '+1d';
-    };
-
-    this.determineMinDate = function() {
-      $this.determineMinForStartDate();
-      $this.determineMinForEndDate();
-    };
+    $scope.calendarsReady = false;
 
     this.showLoadingModal = function(message) {
       angular.element('#loading').modal('show').find('p').text(message);
@@ -56,7 +44,6 @@ angular.module('ts5App')
       $scope.readOnly = false;
       $scope.isCreate = true;
       $scope.viewName = 'Create Schedule';
-      $this.determineMinDate();
     };
 
     this.viewInit = function() {
@@ -235,8 +222,7 @@ angular.module('ts5App')
 
       $scope.shouldDisableStartDate = !(dateUtility.isAfterTodayDatePicker(startDate));
       $scope.shouldDisableEndDate = !(dateUtility.isAfterTodayDatePicker(endDate));
-
-      $this.determineMinDate();
+      $scope.calendarsReady = true;
 
       $scope.schedule = {
         departureTime: response.departureTime,
