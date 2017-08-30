fa7ac24b716f135aef260851506906548acf5b8d
Damir Palinic <damir@bigroomstudios.com>
2017-08-30 10:33:30 +0200

    TSVPORTAL-9805 - Fix conflict with master

--- app/scripts/controllers/discount-list.js
+++ app/scripts/controllers/discount-list.js
@@ -12,6 +12,7 @@ angular.module('ts5App')
 
     var $this = this;
     $scope.viewName = 'Discount';
+    $scope.isSearchClicked = false;
     $scope.search = {};
     $scope.discountList = [];
     $scope.discountToDelete = {};
@@ -38,6 +39,9 @@ angular.module('ts5App')
 
     this.attachDiscountListToScope = function(discountListFromAPI) {
       $this.meta.count = $this.meta.count || discountListFromAPI.meta.count;
+      if (!$scope.discountList.length) {
+        $scope.discountList = angular.copy($this.formatDates(discountListFromAPI.companyDiscounts));
+      }
 
       if ($scope.discountList.length !== discountListFromAPI.meta.count) {
         $scope.discountList = $scope.discountList.concat($this.formatDates(discountListFromAPI.companyDiscounts));
@@ -57,15 +61,16 @@ angular.module('ts5App')
         offset: $this.meta.offset
       });
 
-      if ($scope.search.startDate === undefined || $scope.search.startDate === null || $scope.search.startDate === '') {
+      if (!$scope.isSearchClicked && ($scope.search.startDate === undefined || $scope.search.startDate === null || $scope.search.startDate === '')) {
         query.startDate = dateUtility.formatDateForAPI(dateUtility.nowFormattedDatePicker());
       }
-      
+
       discountFactory.getDiscountList(query).then($this.attachDiscountListToScope, errorHandler);
       $this.meta.offset += $this.meta.limit;
     };
 
     $scope.searchDiscounts = function() {
+      $scope.isSearchClicked = true;
       $scope.discountList = [];
       $this.meta = {
         count: undefined,
