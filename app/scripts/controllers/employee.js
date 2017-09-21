bca2cd817c4e24502f9889cf46fcc7574b87679c
Sujit Panda <sujit.panda@marlabs.com>
2017-09-21 21:27:25 +0530

    TSVPORTAL-9902 | Create Employee Front end changes

--- app/scripts/controllers/employee.js
+++ app/scripts/controllers/employee.js
@@ -7,11 +7,38 @@
  * # EmployeeCtrl
  * Controller of the ts5App
  */
-angular.module('ts5App')
-  .controller('EmployeeCtrl', function () {
+angular.module('ts5App').controller('EmployeeCtrl', function($scope) {
+	$scope.viewName = 'Add Employee';
+	$scope.employeeList = {
+		employeeID : "",
+		employeeFirstName : "",
+		startDate : "",
+		passcode : "",
+		employeeLastName : "",
+		endDate : "",
+		country : "",
+		employeeTitle : "",
+		baseStation : ""
+	};
+	$scope.formSave = function() {
+		if (!formValid()) {
+			$scope.displayError = true;
+			return false;
+		}
+	};
+	function formValid() {
+		resetErrors();
+		return $scope.employeeDataForm.$valid;
+	}
+	function resetErrors() {
+		$scope.formErrors = [];
+		$scope.errorCustom = [];
+		$scope.displayError = false;
+	}
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+	
+});
