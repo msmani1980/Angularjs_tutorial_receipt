31f2d3fd875c9ca7b61384d06ecb16f5c026f82a
Vlad Krinitsyn <VKrinitsyn@egate-solutions.com>
2017-10-09 16:02:21 -0500

    feature/TSVPORTAL-9646-employee-commission

--- app/scripts/services/employees-service.js
+++ app/scripts/services/employees-service.js
@@ -11,6 +11,7 @@ angular.module('ts5App')
   .service('employeesService', function ($resource, ENV) {
 
     var employeeRequestURL = ENV.apiUrl + '/rsvr/api/companies/:id/employees/:empId';
+    var employeeBSRequestURL = ENV.apiUrl + '/rsvr/api/companies/:id/employees/basestations';
 
     var employeeActions = {
       getEmployees: {
@@ -18,6 +19,7 @@ angular.module('ts5App')
       }
     };
     var empoyeeRequestResource = $resource(employeeRequestURL, null, employeeActions);
+    var empoyeeBSRequestResource = $resource(employeeBSRequestURL, null, employeeActions);
 
     var getEmployees = function (companyId, additionalPayload) {
       var payload = { id:companyId };
@@ -29,8 +31,20 @@ angular.module('ts5App')
       return empoyeeRequestResource.getEmployees(payload).$promise;
     };
 
+
+    var getBaseStations = function (companyId, additionalPayload) {
+      var payload = { id:companyId };
+
+      if (additionalPayload) {
+        angular.extend(payload, additionalPayload);
+      }
+
+      return empoyeeBSRequestResource.getEmployees(payload).$promise;
+    };
+    
     return {
-      getEmployees: getEmployees
+      getEmployees: getEmployees,
+      getBaseStations: getBaseStations
     };
 
   });
