fc7ab689c1167334c90a76b027abdbcb32362158
Josip Medic <josip@bigroomstudios.com>
2017-10-20 13:09:05 +0200

    TSVPORTAL-10132: fixing code style error

--- app/scripts/services/records-service.js
+++ app/scripts/services/records-service.js
@@ -154,6 +154,7 @@ angular.module('ts5App')
       getPromotionTypes: getPromotionTypes,
       getDiscountApplyTypes: getDiscountApplyTypes,
       getItemTypes: getItemTypes,
+      getVoucherDurations: getVoucherDurations,
       getCharacteristics: getCharacteristics,
       getFeatures: getFeatures,
       getCountTypes: getCountTypes,
