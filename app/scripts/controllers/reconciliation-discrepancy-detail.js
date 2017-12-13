7bd06e0591e1dd58156847cae82ced37abd34e16
Igor Melnychenko <imelnychenko@egate-solutions.com>
2017-12-13 15:13:25 -0600

    TSVPORTAL-8466

--- app/scripts/controllers/reconciliation-discrepancy-detail.js
+++ app/scripts/controllers/reconciliation-discrepancy-detail.js
@@ -10,9 +10,22 @@
 angular.module('ts5App')
   .controller('ReconciliationDiscrepancyDetail', function ($q, $scope, $routeParams, $filter, $route, messageService,
                                                            reconciliationFactory, currencyFactory, storeInstanceFactory, globalMenuService, dateUtility, lodash,
-                                                           $location) {
+                                                           $location, ENV, identityAccessFactory) {
 
     var $this = this;
+    var STATUS_TO_BUTTONS_MAP = {
+      1: ['Pack'],
+      2: ['Seal'],
+      3: ['Dispatch', 'Offload', 'Checkbox', 'Inbounded', 'On Floor'],
+      4: ['Receive', 'Get Flight Docs', 'Replenish', 'Un-dispatch', 'Checkbox'],
+      5: ['End Instance', 'Redispatch', 'Get Flight Docs', 'Checkbox'],
+      6: ['Start Inbound Seals', 'Get Flight Docs', 'Checkbox'],
+      7: ['Start Offload', 'Get Flight Docs', 'Checkbox'],
+      8: ['Get Flight Docs', 'Checkbox'],
+      9: ['Get Flight Docs'],
+      10: ['Get Flight Docs'],
+      11: ['Get Flight Docs']
+    };
 
     function formatAsCurrency(valueToFormat, optionalNumDigits) {
       var precision = (optionalNumDigits) ? '%.' + optionalNumDigits + 'f' : '%.2f';
@@ -694,7 +707,25 @@ angular.module('ts5App')
       getStockItemData();
       setDiscrepancy();
 
+      var storeStatusName = getStoreInstanceStatusName($scope.storeInstance.statusId);
+      var actionButtons = STATUS_TO_BUTTONS_MAP[storeStatusName];
+      $scope.showGenerateDocsButton = false;
+      if (lodash.find(actionButtons, lodash.matches('Get Flight Docs')) || $scope.storeInstance.statusName === 'On Floor') {
+        $scope.showGenerateDocsButton = true;
+        var sessionToken = identityAccessFactory.getSessionObject().sessionToken;
+        $scope.exportURL = ENV.apiUrl + '/rsvr-pdf/api/dispatch/store-instances/documents/C208-' + $routeParams.storeInstanceId + '.pdf?sessionToken=' + sessionToken;
+      }
+
       hideLoadingModal();
+    }
+
+    function getStoreInstanceStatusName(id) {
+      var matchedObject = lodash.findWhere($scope.statusList, { id: id });
+      if (matchedObject) {
+        return matchedObject.name;
+      }
+
+      return '';
     }
 
     function initData() {
