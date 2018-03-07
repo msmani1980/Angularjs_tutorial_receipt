53b4ebf243953c9baf41c0e73cec6bf8e7d5dc15
Damir Palinic <damir@bigroomstudios.com>
2018-03-07 13:13:21 +0100

    TSVPORTAL-6929 - Added user info on menu assignment

--- app/scripts/controllers/menu-assignment-list.js
+++ app/scripts/controllers/menu-assignment-list.js
@@ -247,6 +247,22 @@ angular.module('ts5App')
       $scope.loadMenuAssignmentData();
     };
 
+    $scope.getUpdateBy = function (menu) {
+      if (menu.updatedByPerson) {
+        return menu.updatedByPerson.userName;
+      }
+
+      if (menu.createdByPerson) {
+        return menu.createdByPerson.userName;
+      }
+
+      return '';
+    };
+
+    $scope.getUpdatedOn = function (menu) {
+      return menu.updatedOn ? dateUtility.formatTimestampForApp(menu.updatedOn) : dateUtility.formatTimestampForApp(menu.createdOn);
+    };
+
     this.makeInitPromises = function() {
       companyId = scheduleFactory.getCompanyId();
 
