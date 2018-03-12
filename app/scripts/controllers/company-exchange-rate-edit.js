daf0e5c3800ab79f4003872bb659dd9f1d88dca4
Damir Palinic <damir@bigroomstudios.com>
2018-03-12 10:18:08 +0100

    TSVPORTAL-6930 - Modified identity access

--- app/scripts/controllers/company-exchange-rate-edit.js
+++ app/scripts/controllers/company-exchange-rate-edit.js
@@ -9,7 +9,7 @@
  */
 angular.module('ts5App')
   .controller('CompanyExchangeRateEditCtrl', function($scope, globalMenuService, currencyFactory, dateUtility,
-    payloadUtility, messageService, $filter, lodash, accessService) {
+    payloadUtility, messageService, $filter, lodash, accessService, $http) {
 
     var $this = this;
 
@@ -231,6 +231,8 @@ angular.module('ts5App')
       $scope.errorResponse = null;
       $this.showToast('success', 'Company Exchange Rate', 'exchange rate successfully saved!');
       $this.hideLoadingModal();
+
+      $scope.searchCompanyExchangeRates();
     };
 
     this.showSaveErrors = function(dataFromAPI) {
@@ -479,12 +481,16 @@ angular.module('ts5App')
         return row.createdByPerson.userName;
       }
 
+      if (row.mode === 'create') {
+        return $http.defaults.headers.common.username;
+      }
+
       return 'Unknown';
     };
 
     $scope.getUpdatedOn = function (row) {
       if (!row.createdOn) {
-        return 'Unknown';
+        return '';
       }
 
       return row.updatedOn ? dateUtility.formatTimestampForApp(row.updatedOn) : dateUtility.formatTimestampForApp(row.createdOn);
