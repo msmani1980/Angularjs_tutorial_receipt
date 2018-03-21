5f699e0d56719e2a04be950db177026b9c69b8af
Josip Medic <josip@bigroomstudios.com>
2018-03-21 13:06:15 +0100

    TSVPORTAL-6932: TSVPORTAL-11095:  get duty by id

--- app/scripts/controllers/excise-duty-relationship-list.js
+++ app/scripts/controllers/excise-duty-relationship-list.js
@@ -271,7 +271,11 @@ angular.module('ts5App')
       hideLoadingModal();
       $scope.clearCreateForm(false);
       var formattedNewRecord = formatRecordForApp(newRecordFromAPI);
-      exciseDutyRelationshipFactory.getRelationship(newRecordFromAPI.id).then(formatResponseForApp, showErrors);
+      exciseDutyRelationshipFactory.getRelationship(newRecordFromAPI.id).then(
+        new function (response) {
+
+        },
+        showErrors);
       $scope.itemExciseDutyList = $scope.itemExciseDutyList || [];
       $scope.itemExciseDutyList.push(formattedNewRecord);
     }
