7ebfb52e9afdc40177b0731c76171dc2cd82ee2d
Damir Palinic <damir@bigroomstudios.com>
2018-03-28 17:50:03 +0200

    TSVPORTAL-11118 - Fix variable names

--- app/scripts/controllers/schedule-list.js
+++ app/scripts/controllers/schedule-list.js
@@ -250,11 +250,11 @@ angular.module('ts5App')
       });
     };
 
-    $scope.$watchGroup(['schedule.startDate', 'schedule.endDate'], function() {
-      if ($scope.schedule.startDate && $scope.schedule.endDate) {
+    $scope.$watchGroup(['search.startDate', 'search.endDate'], function() {
+      if ($scope.search.startDate && $scope.search.endDate) {
         var payload = {
-          startDate: dateUtility.formatDateForAPI($scope.schedule.startDate),
-          endDate: dateUtility.formatDateForAPI($scope.schedule.endDate)
+          startDate: dateUtility.formatDateForAPI($scope.search.startDate),
+          endDate: dateUtility.formatDateForAPI($scope.search.endDate)
         };
 
         scheduleFactory.getStationList(companyId, undefined, payload).then($this.getStationsSuccess);
