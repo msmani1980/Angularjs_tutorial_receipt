1a504ca06d8f99679a5176309adacc82467c8adc
Damir Palinic <damir@bigroomstudios.com>
2018-04-04 15:43:27 +0200

    TSVPORTAL-11059 - Adding custom validation

--- app/scripts/controllers/station-create.js
+++ app/scripts/controllers/station-create.js
@@ -142,7 +142,7 @@ angular.module('ts5App')
     this.validateForm = function() {
       $scope.displayError = $scope.stationCreateForm.$invalid;
 
-      if (this.checkReplenishOptionalValues()) {
+      if (this.checkDatesValidation()) {
         $scope.errorCustom = [{
           field: 'Required Fields',
           value: 'Outbound Seal or Inbound Seal or Cart Quantity or Canister Quantity, any one of these are required'
