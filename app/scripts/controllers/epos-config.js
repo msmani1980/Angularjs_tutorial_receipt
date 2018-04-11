a9146c674751e556c0cea72198ab65c35e771025
Damir Palinic <damir@bigroomstudios.com>
2018-04-11 14:34:47 +0200

    TSVPORTAL-11155 - Fix styles

--- app/scripts/controllers/epos-config.js
+++ app/scripts/controllers/epos-config.js
@@ -98,7 +98,7 @@ angular.module('ts5App')
         return productVersion;
       });
 
-      $scope.productVersions = lodash.orderBy(versions, ['build'],['desc']);
+      $scope.productVersions = _.orderBy(versions, ['build'], ['desc']);
 
       if ($scope.productVersions && $scope.productVersions.length > 0) {
         $scope.selectedProductVersion = $scope.productVersions[0];
