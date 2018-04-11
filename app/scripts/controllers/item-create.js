71be9ae31746575f19103016abd14434b773f4ce
Josip Medic <josip@bigroomstudios.com>
2018-04-11 11:24:48 +0200

    TSVPORTAL-7863: scroll changes

--- app/scripts/controllers/item-create.js
+++ app/scripts/controllers/item-create.js
@@ -1407,7 +1407,7 @@ angular.module('ts5App').controller('ItemCreateCtrl',
       $scope.activeBtn = id;
       var elm = angular.element('#' + id);
       var body = angular.element('html, body');
-      var navBar = angular.element('.navbar-header').height();
+      var navBar = angular.element('.navbar-ts5').height();
       var topBar = angular.element('.top-header').height();
       body.animate({
         scrollTop: elm.offset().top - (navBar + topBar + 100)
