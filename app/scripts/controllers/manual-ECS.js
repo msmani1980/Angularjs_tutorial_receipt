1a9a0c31573a3cb3683d72d9ef9b004ed6730a4c
Josip Medic <josip@bigroomstudios.com>
2018-04-19 08:28:49 +0200

    TSVPORTAL-11171: fixed missing line, build failed

--- app/scripts/controllers/manual-ECS.js
+++ app/scripts/controllers/manual-ECS.js
@@ -138,9 +138,10 @@ angular.module('ts5App')
       ];
 
       $this.meta.count = $this.meta.count || dataFromAPI.meta.count;
-      if($scope.storeInstances === undefined) {
+      if ($scope.storeInstances === undefined) {
         $scope.storeInstances = [];
       }
+      
       var paginatedList = $scope.storeInstances.concat(storeInstancesResponse);
       $scope.storeInstances = lodash.filter(paginatedList, function (storeInstance) {
         formatStoreInstanceForApp(storeInstance);
