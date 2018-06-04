aec2b8f8285e7649428d2305141b33de6d809e8e
Damir Palinic <damir@bigroomstudios.com>
2018-06-04 13:45:08 +0200

    TSVPORTAL-11319 - Adding service

--- app/scripts/services/company-receipt-service.js
+++ app/scripts/services/company-receipt-service.js
@@ -9,5 +9,56 @@
  */
 angular.module('ts5App')
   .service('companyReceiptService', function (ENV, $resource) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+    var requestURL = ENV.apiUrl + '/rsvr/api/company-receipts/:id';
+    var requestParameters = {
+      id: '@id'
+    };
+
+    var actions = {
+      getCompanyReceipts: {
+        method: 'GET'
+      },
+      getSurveyQuestion: {
+        method: 'GET'
+      },
+      createSurveyQuestion: {
+        method: 'POST'
+      },
+      updateSurveyQuestion: {
+        method: 'PUT'
+      },
+      removeSurveyQuestion: {
+        method: 'DELETE'
+      }
+    };
+
+    var requestResource = $resource(requestURL, requestParameters, actions);
+
+    var getCompanyReceipts = function (payload) {
+      return requestResource.getCompanyReceipts(payload).$promise;
+    };
+
+    var getSurveyQuestion = function (id) {
+      return requestResource.getSurveyQuestion({ id: id }).$promise;
+    };
+
+    var createSurveyQuestion = function (payload) {
+      return requestResource.createSurveyQuestion(payload).$promise;
+    };
+
+    var updateSurveyQuestion = function (id, payload) {
+      return requestResource.updateSurveyQuestion({ id: id }, payload).$promise;
+    };
+
+    var removeSurveyQuestion = function (id) {
+      return requestResource.removeSurveyQuestion({ id: id }).$promise;
+    };
+
+    return {
+      getCompanyReceipts: getCompanyReceipts,
+      getSurveyQuestion: getSurveyQuestion,
+      createSurveyQuestion: createSurveyQuestion,
+      updateSurveyQuestion: updateSurveyQuestion,
+      removeSurveyQuestion: removeSurveyQuestion
+    };
   });
