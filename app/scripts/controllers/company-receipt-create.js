9f6ea0152ee8a555588e69d3013e20efd1930e74
Damir Palinic <damir@bigroomstudios.com>
2018-06-06 13:38:32 +0200

    TSVPORTAL-11319 - Working on create page

--- app/scripts/controllers/company-receipt-create.js
+++ app/scripts/controllers/company-receipt-create.js
@@ -9,5 +9,12 @@
  */
 angular.module('ts5App')
   .controller('CompanyReceiptCreateCtrl', function ($scope, $q, $location, dateUtility, $routeParams, messageService, companyReceiptFactory, $filter) {
+    var $this = this;
+
+    $scope.viewName = 'Company Receipts';
+    $scope.shouldDisableEndDate = false;
+    $scope.displayError = false;
+    $scope.companyReceipt = {};
+
 
   });
