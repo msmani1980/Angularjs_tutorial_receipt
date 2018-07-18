54bafe58ec20f190a4e2d35bd7e68c0a266b6ad3
Damir Palinic <damir@bigroomstudios.com>
2018-07-18 13:30:00 +0200

    TSVPORTAL-11471 - Started working on view

---
+++ app/scripts/controllers/route-tax-rates.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ts5App.controller:RouteTaxRatesCtrl
+ * @description
+ * # RouteTaxRatesCtrl
+ * Controller of the ts5App
+ */
+angular.module('ts5App')
+  .controller('RouteTaxRatesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
