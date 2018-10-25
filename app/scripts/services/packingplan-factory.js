06382b7b31ac60a4cf5aa3e93a5bd860a80c881e
Bala Balaji Reddy <breddy@egate-solutions.com>
2018-10-25 13:25:20 -0500

    TSVPORTAL-11797 | Updated factory and service for packing plan

--- app/scripts/services/packingplan-factory.js
+++ app/scripts/services/packingplan-factory.js
@@ -8,16 +8,44 @@
  * Factory in the ts5App.
  */
 angular.module('ts5App')
-  .factory('packingplanFactory', function () {
+  .factory('packingplanFactory', function (globalMenuService, packingplanService, menuMasterService) {
     // Service logic
-    // ...
+  var getCompanyId = function () {
+    return globalMenuService.company.get();
+  };
 
-    var meaningOfLife = 42;
+  var getPackingPlansList = function (payload) {
+    return packingplanService.getPackingPlansList(payload);
+  };
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+  var getPackingPlanById = function(planId) {
+    return packingplanService.getPackingPlanById(planId);
+  };
+
+  var createPackingPlan = function (payload) {
+    return packingplanService.createPackingPlan(payload);
+  };
+
+  var updatePackingPlan = function (payload) {
+    return packingplanService.updatePackingPlan(payload.id, payload);
+  };
+    
+  var deletePackingPlan = function (planId) {
+    return packingplanService.deletePriceUpdaterRule(planId);
+  };
+
+  var getMenuMasterList = function (payload) {
+    return menuMasterService.getMenuMasterList(payload);
+  }
+
+  return {
+    getPackingPlansList: getPackingPlansList,
+    getPackingPlanById: getPackingPlanById,
+    createPackingPlan: createPackingPlan,
+    updatePackingPlan: updatePackingPlan,
+    deletePackingPlan: deletePackingPlan,
+    getCompanyId: getCompanyId,
+    getMenuMasterList: getMenuMasterList
+  };
+
+});
