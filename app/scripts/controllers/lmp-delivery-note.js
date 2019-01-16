a5c6b893108e1bcefcabc264ef37dc7bf04b9db4
Bala Balaji Reddy <breddy@egate-solutions.com>
2019-01-16 11:20:42 -0600

    TSVPORTAL-12130 | Fixed issue when station list is 1

--- app/scripts/controllers/lmp-delivery-note.js
+++ app/scripts/controllers/lmp-delivery-note.js
@@ -576,7 +576,7 @@ angular.module('ts5App')
 
     $scope.isLMPStationIsDisabled = function() {
       if ($scope.readOnly ||
-        Array.isArray($scope.catererStationList) && $scope.catererStationList.length === 1) {
+        Array.isArray($scope.catererStationList) && $scope.catererStationList.length === 0) {
         return true;
       }
 
