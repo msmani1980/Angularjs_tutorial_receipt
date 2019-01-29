a4e83ef06ff2c7a363b8dab644fdb053722a16f1
mani.murugan <MAlagarsamy@egate-solutions.com>
2019-01-29 11:37:52 -0600

    REPO-446| 447 longer paramters tooltip alignment fix

--- app/scripts/controllers/queue.js
+++ app/scripts/controllers/queue.js
@@ -54,6 +54,7 @@ angular.module('ts5App')
     $scope.requestParamTooltip = function (params) {
       var paramString = '<div class="param-header"><i class="fa fa-tags"></i><span class="hidden-xs">Report Parameters</span>';
       angular.forEach(params, function(value, key) {
+        value = (value === 'No filter' ? 'All' : value);
         paramString += '<li><span><strong>' + key + ' : </strong>' + value + '</span></li>';
       });
 
