5a06f15055dd229b00811fd8b7fb9e623b702310
Igor Melnychenko <imelnychenko@egate-solutions.com>
2019-04-04 11:42:54 -0500

    TS5PORTAL-414

--- app/scripts/controllers/store-instance-packing.js
+++ app/scripts/controllers/store-instance-packing.js
@@ -9,7 +9,7 @@
  */
 angular.module('ts5App').controller('StoreInstancePackingCtrl',
   function($scope, storeInstancePackingFactory, $routeParams, lodash, storeInstanceWizardConfig,
-           $location, $q, dateUtility, socketIO, $localStorage, $filter) {
+           $location, $q, dateUtility, socketIO, $filter, $localStorage) {
 
     var $this = this;
 
@@ -250,18 +250,12 @@ angular.module('ts5App').controller('StoreInstancePackingCtrl',
         return false;
       }
 
-      var requiredQuantity = parseInt(angular.copy(item.menuQuantity)) || 0;
+      var requiredQuantity = parseInt(angular.copy(item.menuQuantity)) || 1;
       var dispatchedQuantity = parseInt(angular.copy(item.pickedQuantity)) || 0;
 
       var threshold;
       threshold = ((dispatchedQuantity / requiredQuantity) - 1) * 100;
-
-      if (threshold === Infinity || threshold === -Infinity || threshold === undefined) {
-        item.exceedsVariance = true;
-      } else {
-        item.exceedsVariance = (threshold > $scope.variance);
-      }
-
+      item.exceedsVariance = (threshold > $scope.variance);
     };
 
     this.checkVarianceOnAllItems = function() {
