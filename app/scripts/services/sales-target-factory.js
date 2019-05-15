1ba67e461102642b5e51b370a24b1b12dfccc11f
Damir Palinic <damir@bigroomstudios.com>
2019-05-15 11:46:26 +0200

    TS5PORTAL-522 - Sales targets services

---
+++ app/scripts/services/sales-target-factory.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ts5App.salesTargetFactory
+ * @description
+ * # salesTargetFactory
+ * Factory in the ts5App.
+ */
+angular.module('ts5App')
+  .factory('salesTargetFactory', function (salesTargetService) {
+    function getSalesTargetList(searchParams) {
+      return salesTargetService.getSalesTargetList(searchParams);
+    }
+
+    function getSalesTargetById(id) {
+      return salesTargetService.getSalesTargetById(id);
+    }
+
+    function createSalesTarget(payload) {
+      return salesTargetService.createSalesTarget(payload);
+    }
+
+    function updateSalesTarget(payload) {
+      return salesTargetService.updateSalesTarget(payload);
+    }
+
+    function deleteSalesTarget(id) {
+      return salesTargetService.deleteSalesTarget(id);
+    }
+
+    return {
+      getSalesTargetList: getSalesTargetList,
+      getSalesTargetById: getSalesTargetById,
+      createSalesTarget: createSalesTarget,
+      updateSalesTarget: updateSalesTarget,
+      deleteSalesTarget: deleteSalesTarget
+    }
+  });
